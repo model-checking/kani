ea4f531bf73759cd834070658e8e706ecde64f42
Jason Lingle <jason@lin.gl>
2017-06-11 19:17:49 -0600

    Add `Union` combinator.

--- src/strategy.rs
+++ src/strategy.rs
@@ -10,6 +10,9 @@
 use std::fmt;
 use std::sync::Arc;
 
+use rand;
+use rand::distributions::IndependentSample;
+
 use test_runner::*;
 
 /// A strategy for producing arbitrary values of a given type.
@@ -62,6 +65,25 @@ pub trait Strategy {
         (self, whence: String, fun: F) -> Filter<Self, F>
     where Self : Sized {
         Filter { source: self, whence: whence, fun: Arc::new(fun) }
+    }
+
+    /// Returns a strategy which picks uniformly from `self` and `other`.
+    ///
+    /// When shrinking, if a value from `other` was originally chosen but that
+    /// value can be shrunken no further, it switches to a value from `self`
+    /// and starts shrinking that.
+    ///
+    /// Be aware that chaining `prop_union` calls will result in a very
+    /// right-skewed distribution. If this is not what you want, you can call
+    /// the `.or()` method on the `Union` to add more values to the same union,
+    /// or directly call `Union::new()`.
+    ///
+    /// Both `self` and `other` must be of the same type. To combine
+    /// heterogeneous strategies, call the `boxed()` method on both `self` and
+    /// `other` to erase the type differences before calling `prop_union()`.
+    fn prop_union(self, other: Self) -> Union<Self>
+    where Self : Sized {
+        Union::new(vec![self, other])
     }
 
     /// Erases the type of this `Strategy` so it can be passed around as a
@@ -268,6 +290,101 @@ ValueTree for Filter<S, F> {
     }
 }
 
+/// A `Strategy` which picks from one of several delegate `Stragegy`s.
+///
+/// See `Strategy::prop_union()`.
+#[derive(Clone, Debug)]
+pub struct Union<T : Strategy> {
+    options: Vec<T>,
+}
+
+impl<T : Strategy> Union<T> {
+    /// Create a strategy which selects uniformly from the given delegate
+    /// strategies.
+    ///
+    /// When shrinking, after maximal simplification of the chosen element, the
+    /// strategy will move to earlier options and continue simplification with
+    /// those.
+    ///
+    /// ## Panics
+    ///
+    /// Panics if `options` is empty.
+    pub fn new(options: Vec<T>) -> Self {
+        assert!(options.len() > 0);
+
+        Union { options: options }
+    }
+
+    /// Add `other` as an additional alternate strategy.
+    pub fn or(mut self, other: T) -> Self {
+        self.options.push(other);
+        self
+    }
+}
+
+impl<T : Strategy> Strategy for Union<T> {
+    type Value = UnionValueTree<T::Value>;
+
+    fn new_value(&self, runner: &mut TestRunner)
+                 -> Result<Self::Value, String> {
+        let pick = rand::distributions::Range::new(0, self.options.len())
+            .ind_sample(runner.rng());
+
+        let mut options = Vec::with_capacity(pick);
+        for option in &self.options {
+            options.push(option.new_value(runner)?);
+        }
+
+        Ok(UnionValueTree {
+            options: options,
+            pick: pick,
+            min_pick: 0,
+            prev_pick: None,
+        })
+    }
+}
+
+/// `ValueTree` corresponding to `Union`.
+#[derive(Clone, Debug)]
+pub struct UnionValueTree<T : ValueTree> {
+    options: Vec<T>,
+    pick: usize,
+    min_pick: usize,
+    prev_pick: Option<usize>,
+}
+
+impl<T : ValueTree> ValueTree for UnionValueTree<T> {
+    type Value = T::Value;
+
+    fn current(&self) -> T::Value {
+        self.options[self.pick].current()
+    }
+
+    fn simplify(&mut self) -> bool {
+        if self.options[self.pick].simplify() {
+            self.prev_pick = None;
+            true
+        } else if self.pick > self.min_pick {
+            self.prev_pick = Some(self.pick);
+            self.pick -= 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn complicate(&mut self) -> bool {
+        if let Some(pick) = self.prev_pick {
+            self.pick = pick;
+            self.min_pick = pick;
+            self.prev_pick = None;
+            true
+        } else {
+            self.options[self.pick].complicate()
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -296,5 +413,40 @@ mod test {
             }
             assert!(0 == case.current() % 3);
         }
+    }
+
+    #[test]
+    fn test_union() {
+        let input = (10u32..20u32).prop_union(30u32..40u32);
+        // Expect that 25% of cases pass (left input happens to be < 15, and
+        // left is chosen as initial value). Of the 75% that fail, 50% should
+        // converge to 15 and 50% to 30 (the latter because the left is beneath
+        // the passing threshold).
+        let mut passed = 0;
+        let mut converged_low = 0;
+        let mut converged_high = 0;
+        for _ in 0..256 {
+            let mut runner = TestRunner::new(Config::default());
+            let case = input.new_value(&mut runner).unwrap();
+            let result = runner.run_one(case, |&v| if v < 15 {
+                Ok(())
+            } else {
+                Err(TestCaseError::Fail("fail".to_owned()))
+            });
+
+            match result {
+                Ok(true) => passed += 1,
+                Err(TestError::Fail(_, 15)) => converged_low += 1,
+                Err(TestError::Fail(_, 30)) => converged_high += 1,
+                e => panic!("Unexpected result: {:?}", e),
+            }
+        }
+
+        assert!(passed >= 32 && passed <= 96,
+                "Bad passed count: {}", passed);
+        assert!(converged_low >= 32 && converged_low <= 160,
+                "Bad converged_low count: {}", converged_low);
+        assert!(converged_high >= 32 && converged_high <= 160,
+                "Bad converged_high count: {}", converged_high);
     }
 }
