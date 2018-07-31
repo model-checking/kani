06d27bfd831624811024ec572a5676a0be104106
Mazdak Farrokhzad <twingoow@gmail.com>
2018-07-31 22:59:42 +0200

    adjust copyright headers.

--- proptest-derive2/src/interp.rs
+++ proptest-derive2/src/interp.rs
@@ -1,3 +1,11 @@
+// Copyright 2018 The proptest developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use syn::{self, Expr as E, Lit as L, IntSuffix as IS, BinOp as B, UnOp as U};
 
 /// Adapted from https://docs.rs/syn/0.14.2/src/syn/lit.rs.html#943 to accept
