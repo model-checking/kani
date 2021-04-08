3f2e09296b5e64dba6b2b6df532c50f0c187a52a
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2021-04-08 17:06:31 -0400

    Add regression test for https://github.com/model-checking/rmc/issues/21 (#22)

---
+++ rust-tests/cbmc-reg/Cast/fixme_path.rs
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("./foo/bar.txt");
+}
