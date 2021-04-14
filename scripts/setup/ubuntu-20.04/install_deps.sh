27db8e6a8d35174067a39499e7c1d413f9de0ae6
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-04-14 21:33:55 +0200

    Add RMC workflow file for GitHub Actions (#31)
    * Add RMC workflow file for GitHub Actions
    
    * Add multiple setup scripts used by workflow
    
    * Improvements for Actions worflow and setup scripts

---
+++ scripts/setup/ubuntu-20.04/install_deps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -eux
+
+# Install tools in Ubuntu 20.04 via `apt-get`
+sudo apt-get --yes update \
+  && sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes \
+  bison \
+  cmake \
+  curl \
+  flex \
+  g++ \
+  gcc \
+  git \
+  gpg-agent \
+  libssl-dev \
+  lsb-release \
+  make \
+  ninja-build \
+  patch \
+  pkg-config \
+  python-is-python3 \
+  software-properties-common \
+  wget \
+  zlib1g \
+  zlib1g-dev
