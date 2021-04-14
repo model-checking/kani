dc7206e3f3ee1f2619c1873e343151f51448d37f
Adrian Palacios <accorell@amazon.com>
2021-04-14 18:19:24 +0000

    Improvements for Actions worflow and setup scripts

--- scripts/setup/install_llvm.sh
+++
@@ -1,4 +0,0 @@
-# Install LLVM 11
-wget https://apt.llvm.org/llvm.sh \
-  && chmod a+x llvm.sh \
-  && sudo ./llvm.sh 11
--- scripts/setup/ubuntu-20.04/install_deps.sh
+++ scripts/setup/ubuntu-20.04/install_deps.sh
@@ -1,4 +1,10 @@
-# Install tools via `apt-get`
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -eux
+
+# Install tools in Ubuntu 20.04 via `apt-get`
 sudo apt-get --yes update \
   && sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes \
   bison \
