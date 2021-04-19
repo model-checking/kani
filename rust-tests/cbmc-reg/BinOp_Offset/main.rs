3fc2cf99c2aaaa101f2193da4f4faa45b9a617a7
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-04-19 23:54:58 +0200

    Add a couple of missing tests (nondet. vectors, offset) (#79)
    * Add a couple of missing tests (nondet. vectors, offset)
    
    * Add `test_offset_str` to BinOp_Offset test
    
    * Add assertions in `NondetVectors` test
    
    Co-authored-by: Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>

--- rust-tests/cbmc-reg/BinOp_Offset/main.rs
+++ rust-tests/cbmc-reg/BinOp_Offset/main.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-pub fn test_offset() {
+pub fn test_offset_array() {
     let s = ['a', 'b', 'c'];
     let ptr = s.as_ptr();
 
@@ -11,6 +11,18 @@ pub fn test_offset() {
     }
 }
 
+pub fn test_offset_str() {
+    let s: &str = "123";
+    let ptr: *const u8 = s.as_ptr();
+
+    unsafe {
+        assert!(*ptr.offset(0) == b'1');
+        assert!(*ptr.offset(1) == b'2');
+        assert!(*ptr.offset(2) == b'3');
+    }
+}
+
 pub fn main() {
-    test_offset();
+    test_offset_array();
+    test_offset_str()
 }
