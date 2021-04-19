84496daed81029fb6920be567e2808aa7466a715
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-04-19 23:54:58 +0200

    Add a couple of missing tests (nondet. vectors, offset) (#79)
    * Add a couple of missing tests (nondet. vectors, offset)
    
    * Add `test_offset_str` to BinOp_Offset test
    
    * Add assertions in `NondetVectors` test
    
    Co-authored-by: Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>

---
+++ rust-tests/cbmc-reg/NondetVectors/fixme_main.rs
@@ -0,0 +1,21 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+const FIFO_SIZE: usize = 2;
+fn __nondet<T>() -> T {
+    unimplemented!()
+}
+
+fn main() {
+    let len: usize = __nondet();
+    if !(len <= FIFO_SIZE) {
+        return;
+    }
+    let _buf1: Vec<u8> = vec![0u8; len]; //< this works
+    let elt: u8 = __nondet();
+    let _buf2: Vec<u8> = vec![elt; len]; //< this fails: "memset destination region writeable"
+    let idx: usize = __nondet();
+    if idx < len {
+        assert!(_buf1[idx] == 0u8);
+        assert!(_buf2[idx] == elt);
+    }
+}
