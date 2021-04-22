22b84ef7de3e5f2b13d851614a1a5c5644cd547a
Mark Tuttle <tuttle@acm.org>
2021-04-21 21:23:42 -0400

    Add unsized pointer cast regression tests (#89)
    Add fat-pointer regression tests

---
+++ rust-tests/cbmc-reg/FatPointers/boxtrait.rs
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// This is handled by the box to box case of unsized pointers
+
+pub trait Trait {
+    fn increment(&mut self);
+    fn get(&self) -> u32;
+}
+
+struct Concrete {
+    pub index: u32,
+}
+
+impl Concrete {
+    fn new() -> Self {
+        Concrete { index: 0 }
+    }
+}
+
+impl Trait for Concrete {
+    fn increment(&mut self) {
+        self.index = self.index + 1;
+    }
+    fn get(&self) -> u32 {
+        self.index
+    }
+}
+
+fn main() {
+    let mut x: Box<dyn Trait> = Box::new(Concrete::new());
+    x.increment();
+    assert!(x.get() == 1);
+}
