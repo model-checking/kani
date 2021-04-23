d47452726825577312a22924b3f0a9721271776c
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-04-23 16:04:56 +0200

    Enable regression workflow for macOS (#97)

---
+++ scripts/setup/macos-10.15/install_deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -eux
+
+# Update tools in macOS 10.15 via `brew`
+brew update
