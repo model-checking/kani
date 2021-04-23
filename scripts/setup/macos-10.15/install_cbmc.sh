bc69023d3604dacf6bd8356b236de1eab6d75cf2
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-04-23 16:04:56 +0200

    Enable regression workflow for macOS (#97)

---
+++ scripts/setup/macos-10.15/install_cbmc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -eux
+
+# Install CBMC for macOS 10.15
+brew install cbmc
