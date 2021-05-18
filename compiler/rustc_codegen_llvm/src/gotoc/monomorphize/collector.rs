1ccff64fa9d670177fcf36b31f511dc54470c424
Adrian Palacios <accorell@amazon.com>
2021-05-18 13:11:33 +0000

    Conform with latest changes in crate handling

--- compiler/rustc_codegen_llvm/src/gotoc/monomorphize/collector.rs
+++ compiler/rustc_codegen_llvm/src/gotoc/monomorphize/collector.rs
@@ -7,7 +7,7 @@ use rustc_data_structures::fx::{FxHashMap, FxHashSet};
 use rustc_data_structures::sync::{par_iter, MTLock, MTRef, ParallelIterator};
 use rustc_errors::{ErrorReported, FatalError};
 use rustc_hir as hir;
-use rustc_hir::def_id::{DefId, DefIdMap, LocalDefId, LOCAL_CRATE};
+use rustc_hir::def_id::{DefId, DefIdMap, LocalDefId};
 use rustc_hir::itemlikevisit::ItemLikeVisitor;
 use rustc_hir::lang_items::LangItem;
 use rustc_index::bit_set::GrowableBitSet;
@@ -171,7 +171,7 @@ fn collect_roots(tcx: TyCtxt<'_>, mode: MonoItemCollectionMode) -> Vec<MonoItem<
     let mut roots = Vec::new();
 
     {
-        let entry_fn = tcx.entry_fn(LOCAL_CRATE).and_then(|(id, ty)| Some((id.expect_local(), ty)));
+        let entry_fn = tcx.entry_fn(()).and_then(|(id, ty)| Some((id.expect_local(), ty)));
 
         debug!("collect_roots: entry_fn = {:?}", entry_fn);
 
