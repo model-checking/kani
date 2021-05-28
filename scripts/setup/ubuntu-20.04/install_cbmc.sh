f15151467d170c2fd9f7628ea544431ff9828774
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2021-05-28 13:08:25 +0200

    Complete count intrinsics codegen and testing (#155)

--- scripts/setup/ubuntu-20.04/install_cbmc.sh
+++ scripts/setup/ubuntu-20.04/install_cbmc.sh
@@ -4,7 +4,7 @@
 
 set -eux
 
-# Install CBMC 5.27 for Ubuntu 20.04
-wget https://github.com/diffblue/cbmc/releases/download/cbmc-5.27.0/ubuntu-20.04-cbmc-5.27.0-Linux.deb \
-  && sudo dpkg -i ubuntu-20.04-cbmc-5.27.0-Linux.deb \
+# Install CBMC 5.30.1 for Ubuntu 20.04
+wget https://github.com/diffblue/cbmc/releases/download/cbmc-5.30.1/ubuntu-20.04-cbmc-5.30.1-Linux.deb \
+  && sudo dpkg -i ubuntu-20.04-cbmc-5.30.1-Linux.deb \
   && cbmc --version
