eb29943146ef0ed3131da9bc802a7081c3c34a81
Thomas Del Vecchio <84409848+vecchiot-aws@users.noreply.github.com>
2021-05-31 11:16:13 -0400

    Added fixme tests for 'Unable to find vtable symbol' bug. (#159)
    Co-authored-by: Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>

---
+++ rust-tests/cbmc-reg/FatPointers/fixme_boxmuttrait_fail.rs
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use std::io::{sink, Write};
+
+fn main() {
+    let mut log: Box<dyn Write + Send> = Box::new(sink());
+    let dest: Box<dyn Write + Send> = Box::new(log.as_mut());
+
+    let mut log2: Box<dyn Write + Send> = Box::new(sink());
+    let buffer = vec![1, 2, 3, 5, 8];
+    let num_bytes = log2.write(&buffer).unwrap();
+    assert!(num_bytes == 8); // Should be == 5
+}
