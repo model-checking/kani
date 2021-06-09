e5f93e13c025e1bbd01b10041bad17b272e8ea6a
Alexa VanHattum <alexavanhattum@gmail.com>
2021-06-09 13:48:56 -0400

    Add test for duplicates across crates

---
+++ src/test/cbmc/DynTrait/different_crates_fail.rs
@@ -0,0 +1,51 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+struct Counter {
+    count: usize,
+}
+
+impl std::iter::Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Increment our count. 
+        self.count += 1;
+        Some(self.count)
+    }
+}
+
+trait Iterator {
+    fn next(&mut self) -> Option<usize>;
+}
+
+impl Iterator for Counter {
+    fn next(&mut self) -> Option<usize> {
+        Some(42)
+    }
+}
+
+trait Combined : Iterator + std::iter::Iterator<Item = usize> {}
+
+impl Combined for Counter {
+
+}
+
+fn std_count(c : &mut dyn std::iter::Iterator<Item = usize>) -> usize {
+    c.next().unwrap()
+}
+
+fn weird_count(c : &mut dyn Iterator) -> usize {
+    c.next().unwrap()
+}
+
+
+fn main() {
+    let counter : &mut Counter = &mut Counter { count: 0 };
+    assert!(std_count(counter as &mut dyn std::iter::Iterator<Item = usize>) == 0); // Should be 1
+    assert!(weird_count(counter as &mut dyn Iterator) == 0); // Should be 42
+    
+    let counter_combined = counter as &mut dyn Combined;
+    assert!(std::iter::Iterator::next(counter_combined).unwrap() == 0); // Should be 2
+    assert!(Iterator::next(counter_combined).unwrap() == 0); // Should be 42
+}
