ff4b095baf2843ede6e7f990b34211d0d286dde7
Alexa VanHattum <alexavanhattum@gmail.com>
2021-06-09 16:48:24 -0400

    Fix for duplicate vtable field names (#188)
    Handle duplicate dynamic trait names correctly, for now keeping the string-matching based vtable implementation. In the generated vtable itself, use the function name scoped to the specific trait (A::foo vs B::foo) instead of just the function name. Merge some vtable lookup handling. Add tests for more potential naming conflicts.

---
+++ src/test/cbmc/DynTrait/different_crates.rs
@@ -0,0 +1,53 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// This test uses a trait defined in a different crate (the standard library)
+// and a test defined in the local crate. The goal is to test vtable resolution
+// of duplicate names across different crates.
+struct Counter {
+    count: usize,
+}
+
+// A custom impl for the standard library trait.
+impl std::iter::Iterator for Counter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Increment our count. 
+        self.count += 1;
+        Some(self.count)
+    }
+}
+
+// An impl for our local trait, with an indentical name to the standard library
+trait Iterator {
+    fn next(&mut self) -> Option<usize>;
+}
+
+impl Iterator for Counter {
+    fn next(&mut self) -> Option<usize> {
+        Some(42)
+    }
+}
+
+trait Combined : Iterator + std::iter::Iterator<Item = usize> {}
+
+impl Combined for Counter {}
+
+fn std_count(c : &mut dyn std::iter::Iterator<Item = usize>) -> usize {
+    c.next().unwrap()
+}
+
+fn weird_count(c : &mut dyn Iterator) -> usize {
+    c.next().unwrap()
+}
+
+fn main() {
+    let counter : &mut Counter = &mut Counter { count: 0 };
+    assert!(std_count(counter as &mut dyn std::iter::Iterator<Item = usize>) == 1);
+    assert!(weird_count(counter as &mut dyn Iterator) == 42);
+    
+    let counter_combined = counter as &mut dyn Combined;
+    assert!(std::iter::Iterator::next(counter_combined).unwrap() == 2);
+    assert!(Iterator::next(counter_combined).unwrap() == 42);
+}
