099b8ffd3999afb8745380a11b2e2e264a1bae1d
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2021-06-18 07:30:38 -0400

    Add rmc-prelude.rs, and use it in tests (#226)

--- src/test/cbmc/Assume/main.rs
+++ src/test/cbmc/Assume/main.rs
@@ -1,13 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-fn __VERIFIER_assume(cond: bool) {
-    unimplemented!()
-}
-
-fn __nondet<T>() -> T {
-    unimplemented!()
-}
+include!("../../rmc-prelude.rs");
 
 fn main() {
     let i: i32 = __nondet();
