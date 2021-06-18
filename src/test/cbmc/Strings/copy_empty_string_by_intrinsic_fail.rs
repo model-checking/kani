9b3d31e34b993d42516a7023b5e8b0361073f89f
Alexa VanHattum <alexavanhattum@gmail.com>
2021-06-18 17:07:32 -0400

    Add more tests

--- src/test/cbmc/Strings/copy_empty_string_by_intrinsic_fail.rs
+++ src/test/cbmc/Strings/copy_empty_string_by_intrinsic_fail.rs
@@ -3,6 +3,12 @@
 
 // Make sure we can handle explicit copy_nonoverlapping on empty string
 
+// The copy_nonoverlapping succeeds, but the final copy back to a slice
+// fails:
+// [...copy_empty_string_by_intrinsic.assertion.2] line 1035 unreachable code: FAILURE
+// [...copy_empty_string_by_intrinsic.assertion.1] line 1037 a panicking function std::result::unwrap_failed is invoked: FAILURE
+// [...copy_string.assertion.2] line 28 assertion failed: dest_as_str.len() == l: FAILURE
+
 #![feature(rustc_private)]
 
 extern crate libc;
@@ -21,6 +27,12 @@ fn copy_string(s: &str, l: usize) {
         // Copy
         let src = from_raw_parts(s.as_ptr(), l).as_ptr();
         copy_nonoverlapping(src, dest, l);
+        
+        // THIS CHUNK causes 3 failures
+        // Back to str, check length
+        let dest_slice : &[u8] = from_raw_parts(dest, l);
+        let dest_as_str : &str= str::from_utf8(dest_slice).unwrap();
+        assert!(dest_as_str.len() == l);
     }
 }
 
