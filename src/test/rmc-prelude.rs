8a6212e5f0bde28e4a2a5595dfb5167f2cd0dddf
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2021-06-18 07:30:38 -0400

    Add rmc-prelude.rs, and use it in tests (#226)

---
+++ src/test/rmc-prelude.rs
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+fn __VERIFIER_assume(cond: bool) {
+    unimplemented!()
+}
+
+fn __nondet<T>() -> T {
+    unimplemented!()
+}
