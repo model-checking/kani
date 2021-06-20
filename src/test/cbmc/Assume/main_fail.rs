d39a195c76765a9992d4aa0ded479045fc03b7bb
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2021-06-20 13:23:43 -0400

    Add an __VERIFIER_expected_fail() intrinsic, and use it in a test (#229)
    * Add an __VERIFIER_expected_fail() intrinsic, and use it in a test
    
    * PR comments

--- src/test/cbmc/Assume/main_fail.rs
+++ src/test/cbmc/Assume/main_fail.rs
@@ -6,5 +6,5 @@ include!("../../rmc-prelude.rs");
 fn main() {
     let i: i32 = __nondet();
     __VERIFIER_assume(i < 10);
-    assert!(i > 20);
+    __VERIFIER_expect_fail(i > 20, "Blocked by assumption above.");
 }
