de21549e3d5a27d2ee2c5475675f8c872610fc9f
Abdalrhman M Mohamed <84409648+bdalrhm@users.noreply.github.com>
2021-06-23 12:39:55 -0400

    Enforce formatting of RMC tests. (#258)
    * Enable formatting of rmc tests.
    
    * Change expected line numbers for reformatted tests.

--- src/test/cbmc/Helpers/vtable_utils_ignore.rs
+++ src/test/cbmc/Helpers/vtable_utils_ignore.rs
@@ -1,25 +1,19 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Because each regression test does not share a crate, we just use 
+// Because each regression test does not share a crate, we just use
 // an import! to share this code across test directories.
-fn drop_from_vtrable(vtable_ptr : *mut usize) -> *mut () {
+fn drop_from_vtrable(vtable_ptr: *mut usize) -> *mut () {
     // 1st pointer-sized position
-    unsafe {
-        *vtable_ptr as *mut ()
-    }
+    unsafe { *vtable_ptr as *mut () }
 }
 
-fn size_from_vtable(vtable_ptr : *mut usize) -> usize {
+fn size_from_vtable(vtable_ptr: *mut usize) -> usize {
     // 2nd usize-sized position
-    unsafe {
-        *(vtable_ptr.offset(1))
-    }
+    unsafe { *(vtable_ptr.offset(1)) }
 }
 
-fn align_from_vtable(vtable_ptr : *mut usize) -> usize {
+fn align_from_vtable(vtable_ptr: *mut usize) -> usize {
     // 3rd usize-sized position
-    unsafe {
-        *(vtable_ptr.offset(2))
-    }
+    unsafe { *(vtable_ptr.offset(2)) }
 }
