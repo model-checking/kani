def550b6eb495ce1136e4135c69e098c9abd5d29
Abdalrhman M Mohamed <84409648+bdalrhm@users.noreply.github.com>
2021-06-23 12:39:55 -0400

    Enforce formatting of RMC tests. (#258)
    * Enable formatting of rmc tests.
    
    * Change expected line numbers for reformatted tests.

--- src/test/cbmc/DynTrait/different_crates_fail.rs
+++ src/test/cbmc/DynTrait/different_crates_fail.rs
@@ -13,7 +13,7 @@ impl std::iter::Iterator for Counter {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // Increment our count. 
+        // Increment our count.
         self.count += 1;
         Some(self.count)
     }
@@ -30,23 +30,23 @@ impl Iterator for Counter {
     }
 }
 
-trait Combined : Iterator + std::iter::Iterator<Item = usize> {}
+trait Combined: Iterator + std::iter::Iterator<Item = usize> {}
 
 impl Combined for Counter {}
 
-fn std_count(c : &mut dyn std::iter::Iterator<Item = usize>) -> usize {
+fn std_count(c: &mut dyn std::iter::Iterator<Item = usize>) -> usize {
     c.next().unwrap()
 }
 
-fn weird_count(c : &mut dyn Iterator) -> usize {
+fn weird_count(c: &mut dyn Iterator) -> usize {
     c.next().unwrap()
 }
 
 fn main() {
-    let counter : &mut Counter = &mut Counter { count: 0 };
+    let counter: &mut Counter = &mut Counter { count: 0 };
     assert!(std_count(counter as &mut dyn std::iter::Iterator<Item = usize>) == 0); // Should be 1
     assert!(weird_count(counter as &mut dyn Iterator) == 0); // Should be 42
-    
+
     let counter_combined = counter as &mut dyn Combined;
     assert!(std::iter::Iterator::next(counter_combined).unwrap() == 0); // Should be 2
     assert!(Iterator::next(counter_combined).unwrap() == 0); // Should be 42
--- src/test/expected/assert-eq/main.rs
+++ src/test/expected/assert-eq/main.rs
@@ -5,14 +5,14 @@
 /// This leads us to the land of foreign types, ReifyFnPointer, and transmute.
 /// The "C" output from RMC is about 1KLOC, vs 80LOC for the same version with straight `assert!`.
 ///     https://github.com/model-checking/rmc/issues/14
-/// The assertion message printed to the user on success is uninformative: 
+/// The assertion message printed to the user on success is uninformative:
 ///     "library/std/src/macros.rs line 17 a panicking function core::panicking::panic_fmt is invoked: SUCCESS"
 ///     https://github.com/model-checking/rmc/issues/13
 
 fn main() {
     let x = 1;
     let y = 2;
-    assert_eq!(x+1, y);
-    assert_eq!(x,y);//Expected failure
+    assert_eq!(x + 1, y);
+    assert_eq!(x, y); //Expected failure
     assert_ne!(x, y);
 }
