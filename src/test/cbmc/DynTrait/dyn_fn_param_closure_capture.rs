f81cf78b7431f9a801f1ce6616a55c42856b7448
Alexa VanHattum <alexavanhattum@gmail.com>
2021-06-25 16:09:59 -0400

    Implement sizeof for Code for vtable generation (fix crash) (#261)

---
+++ src/test/cbmc/DynTrait/dyn_fn_param_closure_capture.rs
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we can pass a dyn function pointer to a closure that captures
+// some data
+#![feature(raw)]
+#![allow(deprecated)]
+
+include!("../Helpers/vtable_utils_ignore.rs");
+
+fn takes_dyn_fun(fun: &dyn Fn() -> i32) {
+    let x = fun();
+    assert!(x == 5);
+    /* The closure captures `a` and thus has nonzero size */
+    assert!(size_from_vtable(vtable!(fun)) == 8);
+}
+
+fn main() {
+    let a = vec![3];
+    let closure = || a[0] + 2;
+    takes_dyn_fun(&closure)
+}
---
+++ src/test/cbmc/DynTrait/dyn_fn_param_closure_capture_fail.rs
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we can pass a dyn function pointer to a closure that captures
+// some data
+#![feature(raw)]
+#![allow(deprecated)]
+
+include!("../Helpers/vtable_utils_ignore.rs");
+include!("../../rmc-prelude.rs");
+
+fn takes_dyn_fun(fun: &dyn Fn() -> i32) {
+    let x = fun();
+    __VERIFIER_expect_fail(x != 5, "Wrong return");
+    /* The closure captures `a` and thus is sized */
+    __VERIFIER_expect_fail(size_from_vtable(vtable!(fun)) == 8, "Wrong size");
+}
+
+fn main() {
+    let a = vec![3];
+    let closure = || a[0] + 2;
+    takes_dyn_fun(&closure)
+}
