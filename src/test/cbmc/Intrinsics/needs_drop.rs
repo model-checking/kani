a3c3017c82b0d21a0d897235d91a6adc22f530d7
Alexa VanHattum <alexavanhattum@gmail.com>
2021-06-25 20:02:09 +0000

    Fix types for codegen of intrinsic const

---
+++ src/test/cbmc/Intrinsics/needs_drop.rs
@@ -0,0 +1,26 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we get the expected results for needs_drop intrinsic
+
+use std::mem;
+
+pub struct Foo<T> {
+    _foo: T,
+}
+
+impl<T> Foo<T> {
+    fn call_needs_drop(&self) -> bool {
+        return mem::needs_drop::<T>();   
+    }
+}
+
+fn main() {
+    // Integers don't need to be dropped
+    let int_foo = Foo::<i32>{_foo : 0};
+    assert!(!int_foo.call_needs_drop());
+
+    // But strings do need to be dropped
+    let string_foo = Foo::<String>{_foo: "".to_string() };
+    assert!(string_foo.call_needs_drop());
+}
