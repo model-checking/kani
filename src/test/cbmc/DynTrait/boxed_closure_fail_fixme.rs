f21c873988038ea5381f83df9245dc56b753f6dc
Alexa VanHattum <alexavanhattum@gmail.com>
2021-07-01 12:58:50 -0400

    Handle boxed closures as arguments (fix RMC std lib crash) (#278)
    * Support boxed closures
    
    * More comments
    
    * Review comments
    
    * Whitespace and test typo
    
    * Cleanup

---
+++ src/test/cbmc/DynTrait/boxed_closure_fail_fixme.rs
@@ -0,0 +1,18 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we can codegen a boxed dyn closure and fail an inner assertion
+
+// This current verifies "successfully" because the closure is not actually
+// called in the resulting CotoC code.
+// https://github.com/model-checking/rmc/issues/240
+
+fn main() {
+    // Create a boxed once-callable closure
+    let f: Box<dyn FnOnce()> = Box::new(|x| {
+        __VERIFIER_expect_fail(x == 2, "Wrong int");
+    });
+
+    // Call it
+    f(1);
+}
