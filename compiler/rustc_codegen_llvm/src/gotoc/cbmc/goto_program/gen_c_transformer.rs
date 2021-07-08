a0e4260d2df23887998c0ca04521d3f20bd40d26
Thomas Del Vecchio <vecchiot@amazon.com>
2021-07-08 17:00:19 -0400

    Add copyright message.

--- compiler/rustc_codegen_llvm/src/gotoc/cbmc/goto_program/gen_c_transformer.rs
+++ compiler/rustc_codegen_llvm/src/gotoc/cbmc/goto_program/gen_c_transformer.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 use super::{
     CIntType, DatatypeComponent, Expr, Location, Parameter, Stmt, StmtBody, Symbol, SymbolTable,
     SymbolValues, Transformer, Type,
