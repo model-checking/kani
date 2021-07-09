81bba4c82e84b64ff762addc9f4e168f75f69271
Alexa VanHattum <alexavanhattum@gmail.com>
2021-07-09 10:13:48 -0400

    Fix codegen local for Fn::call and FnMut::call_mut

---
+++ src/test/cbmc/DynTrait/dyn_fn_once_fail.rs
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we can pass a dyn FnOnce pointer to a stand alone
+// function definition. Fail case checks wrong int.
+
+include!("../../rmc-prelude.rs");
+
+fn takes_dyn_fun(fun: Box<dyn FnOnce() -> u32>) -> u32 {
+    fun()
+}
+
+pub fn unit_to_u32() -> u32 {
+    5
+}
+
+fn main() {
+    __VERIFIER_expect_fail(takes_dyn_fun(Box::new(&unit_to_u32)) == 3, "Wrong u32")
+}
