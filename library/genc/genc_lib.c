7dc2a5561798433db90af5b99c9c9825596b81b3
Thomas Del Vecchio <vecchiot@amazon.com>
2021-07-09 10:12:34 -0400

    Added more genc_lib code.

--- library/genc/genc_lib.c
+++ library/genc/genc_lib.c
@@ -14,28 +14,30 @@ void __CPROVER_assume(int condition) {
 
 
 
-// #define overflow(op, typ, var1, var2) \
-//     (strcmp(op, "+") == 0) ? ( \
-//         ((var1 < 0) && (var2 < 0) && (var1 + var2 > 0)) || \
-//         ((var1 > 0) && (var2 > 0) && (var1 + var2 < 0)) \
-//     ) : (strcmp(op, "-") == 0) ? ( \
-//         ((var1 < 0) && (var2 > 0) && (var1 - var2 > 0)) || \
-//         ((var1 > 0) && (var2 < 0) && (var1 - var2 < 0)) \
-//     ) : (strcmp(op, "*") == 0) ? ( \
-//         (var1 != 0) && ((var1 * var2) / var1 != var2) \
-//     ) : ( \
-//         1 \
-//     )
+#define overflow(op, typ, var1, var2) \
+    (strcmp(op, "+") == 0) ? ( \
+        ((var1 < 0) && (var2 < 0) && (var1 + var2 > 0)) || \
+        ((var1 > 0) && (var2 > 0) && (var1 + var2 < 0)) \
+    ) : (strcmp(op, "-") == 0) ? ( \
+        ((var1 < 0) && (var2 > 0) && (var1 - var2 > 0)) || \
+        ((var1 > 0) && (var2 < 0) && (var1 - var2 < 0)) \
+    ) : (strcmp(op, "*") == 0) ? ( \
+        (var1 != 0) && ((var1 * var2) / var1 != var2) \
+    ) : ( \
+        1 \
+    )
 
 // nondet_<k>
 #define byte_extract_little_endian(from_val, offset, to_type) \
-    ((to_type) from_val)
+    *((to_type*) (((void*) &from_val) + offset))
 
 typedef struct X {
     int y;
+    int z;
 } X;
 
 int main() {
-    X x = { 5 };
-    int y = byte_extract_little_endian(x, 0, int);
+    X x = { 5, 6 };
+    int y = byte_extract_little_endian(x, 4, int);
+    printf("Num: %d", y);
 }
