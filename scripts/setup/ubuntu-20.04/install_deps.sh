05a287a9fdd331980692529589137832aa233ea8
Ted Kaminski <tedinski@amazon.com>
2021-07-12 21:43:51 +0000

    Define dependencies with an array
    Among other things, this will allow us to comment on them in the future.

--- scripts/setup/ubuntu-20.04/install_deps.sh
+++ scripts/setup/ubuntu-20.04/install_deps.sh
@@ -2,28 +2,32 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-set -eux
+set -eu
 
-# Install tools in Ubuntu 20.04 via `apt-get`
-sudo apt-get --yes update \
-  && sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes \
-  bison \
-  cmake \
-  ctags \
-  curl \
-  flex \
-  g++ \
-  gcc \
-  git \
-  gpg-agent \
-  libssl-dev \
-  lsb-release \
-  make \
-  ninja-build \
-  patch \
-  pkg-config \
-  python-is-python3 \
-  software-properties-common \
-  wget \
-  zlib1g \
+DEPS=(
+  bison
+  cmake
+  ctags
+  curl
+  flex
+  g++
+  gcc
+  git
+  gpg-agent
+  libssl-dev
+  lsb-release
+  make
+  ninja-build
+  patch
+  pkg-config
+  python-is-python3
+  software-properties-common
+  wget
+  zlib1g
   zlib1g-dev
+)
+
+set -x
+
+sudo apt-get --yes update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes "${DEPS[@]}"
