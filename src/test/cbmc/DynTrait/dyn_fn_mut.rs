a783d7157d21d00ffd96ffbc8f593e4c0dace168
Alexa VanHattum <alexavanhattum@gmail.com>
2021-07-12 14:36:43 +0000

    Whitesapce

--- src/test/cbmc/DynTrait/dyn_fn_mut.rs
+++ src/test/cbmc/DynTrait/dyn_fn_mut.rs
@@ -1,8 +1,9 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+git// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 // Check that we can pass a dyn FnMut pointer to a stand alone
-// function definition
+// function definition.
+
 fn takes_dyn_fun(mut fun: Box<dyn FnMut(&mut i32)>, x_ptr: &mut i32) {
     fun(x_ptr)
 }
