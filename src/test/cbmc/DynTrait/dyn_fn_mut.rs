c1555d69a315da0bdae511e7716656d8355a450c
Alexa VanHattum <alexavanhattum@gmail.com>
2021-07-12 15:17:45 +0000

    Typo

--- src/test/cbmc/DynTrait/dyn_fn_mut.rs
+++ src/test/cbmc/DynTrait/dyn_fn_mut.rs
@@ -1,4 +1,4 @@
-git// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 // Check that we can pass a dyn FnMut pointer to a stand alone
