27ac894f7257694261daab72c5452f3fbe5aeb4c
Ted Kaminski <tuxted+github@gmail.com>
2021-07-13 12:17:53 -0500

    Improve setup scripts for development (#322)
    * Define dependencies with an array
    
    Among other things, this will allow us to comment on them in the future.
    
    * Add missing (non-CI) dependency
    
    Running setup scripts on the default Ubuntu AMI results in this error message:
    
    ```
    + sudo python3 -m pip install --upgrade cbmc_viewer-2.5-py3-none-any.whl
    /usr/bin/python3: No module named pip
    ```
    
    This fixes the issue.
    
    * Use the downloaded file, if already downloaded
    
    wget has a default behavior that causes downloads like this to get new
    unexpected filenames if the file already exists.
    
    The switch to curl is simply personal preference.
    
    * swap back to wget
    
    * keep deps in alpha order for now

--- scripts/setup/ubuntu-20.04/install_cbmc.sh
+++ scripts/setup/ubuntu-20.04/install_cbmc.sh
@@ -2,9 +2,16 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
-set -eux
+set -eu
 
 # Install CBMC 5.30.1 for Ubuntu 20.04
-wget https://github.com/diffblue/cbmc/releases/download/cbmc-5.30.1/ubuntu-20.04-cbmc-5.30.1-Linux.deb \
-  && sudo dpkg -i ubuntu-20.04-cbmc-5.30.1-Linux.deb \
-  && cbmc --version
+
+FILE="ubuntu-20.04-cbmc-5.30.1-Linux.deb"
+URL="https://github.com/diffblue/cbmc/releases/download/cbmc-5.30.1/$FILE"
+
+set -x
+
+wget -O "$FILE" "$URL"
+sudo dpkg -i "$FILE"
+
+cbmc --version
