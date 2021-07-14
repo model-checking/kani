9c90b22afdbc30e16b85e72c5a55a23447566305
Thomas Del Vecchio <84409848+vecchiot-aws@users.noreply.github.com>
2021-07-14 08:20:10 -0400

    C lib flag (#260)
    * Added --c-lib flag to rmc and cargo-rmc.
    
    * Added rmc test for extern C files.
    
    * Added cargo-rmc example for extern C files.
    
    * PR fixes.
    
    * Fixups after rebase.
    
    * PR fixes.

--- scripts/rmc.py
+++ scripts/rmc.py
@@ -79,9 +79,11 @@ def add_selected_default_cbmc_flags(args):
     if not args.no_unwinding_checks:
         add_set_cbmc_flags(args, UNWINDING_CHECKS)
 
+# Updates environment to use gotoc backend debugging
 def add_rmc_rustc_debug_to_env(env):
     env["RUSTC_LOG"] = env.get("RUSTC_LOG", "rustc_codegen_llvm::gotoc")
 
+# Prints info about the RMC process
 def print_rmc_step_status(step_name, completed_process, verbose=False):
     status = "PASS"
     if completed_process.returncode != EXIT_CODE_SUCCESS:
@@ -90,6 +92,7 @@ def print_rmc_step_status(step_name, completed_process, verbose=False):
         print(f"[RMC] stage: {step_name} ({status})")
         print(f"[RMC] cmd: {' '.join(completed_process.args)}")
 
+# Handler for running arbitrary command-line commands
 def run_cmd(cmd, label=None, cwd=None, env=None, output_to=None, quiet=False, verbose=False, debug=False, scanners=[], dry_run=False):
     # If this a dry run, we emulate running a successful process whose output is the command itself
     # We set `output_to` to `stdout` so that the output is not omitted below
@@ -123,6 +126,7 @@ def run_cmd(cmd, label=None, cwd=None, env=None, output_to=None, quiet=False, ve
     
     return process.returncode
 
+# Generates a symbol table from a rust file
 def compile_single_rust_file(input_filename, output_filename, verbose=False, debug=False, keep_temps=False, mangler="v0", dry_run=False):
     if not keep_temps:
         atexit.register(delete_file, output_filename)
@@ -134,7 +138,7 @@ def compile_single_rust_file(input_filename, output_filename, verbose=False, deb
 
     return run_cmd(build_cmd, env=build_env, label="compile", verbose=verbose, debug=debug, dry_run=dry_run)
 
-
+# Generates a symbol table (and some other artifacts) from a rust crate
 def cargo_build(crate, target_dir="target", verbose=False, debug=False, mangler="v0", dry_run=False):
     rustflags = ["-Z", "codegen-backend=gotoc", "-Z", f"symbol-mangling-version={mangler}", f"--cfg={RMC_CFG}"]
     rustflags = " ".join(rustflags)
@@ -150,17 +154,25 @@ def cargo_build(crate, target_dir="target", verbose=False, debug=False, mangler=
         add_rmc_rustc_debug_to_env(build_env)
     return run_cmd(build_cmd, env=build_env, cwd=crate, label="build", verbose=verbose, debug=debug, dry_run=dry_run)
 
+# Adds information about unwinding to the RMC output
 def append_unwind_tip(text):
     unwind_tip = ("[RMC] info: Verification output shows one or more unwinding failures.\n"
                   "[RMC] tip: Consider increasing the unwinding value or disabling `--unwinding-assertions`.\n")
     return text + unwind_tip
 
+# Generates a goto program from a symbol table
 def symbol_table_to_gotoc(json_filename, cbmc_filename, verbose=False, keep_temps=False, dry_run=False):
     if not keep_temps:
         atexit.register(delete_file, cbmc_filename)
     cmd = ["symtab2gb", json_filename, "--out", cbmc_filename]
     return run_cmd(cmd, label="to-gotoc", verbose=verbose, dry_run=dry_run)
 
+# Links in external C programs into a goto program
+def link_c_lib(src, dst, c_lib, verbose=False, quiet=False, function="main", dry_run=False):
+    cmd = ["goto-cc"] + ["--function", function] + [src] + c_lib + ["-o", dst]
+    return run_cmd(cmd, label="goto-cc", verbose=verbose, quiet=quiet, dry_run=dry_run)
+
+# Runs CBMC on a goto program
 def run_cbmc(cbmc_filename, cbmc_args, verbose=False, quiet=False, dry_run=False):
     cbmc_cmd = ["cbmc"] + cbmc_args + [cbmc_filename]
     scanners = []
@@ -171,25 +183,22 @@ def run_cbmc(cbmc_filename, cbmc_args, verbose=False, quiet=False, dry_run=False
         scanners.append(unwind_asserts_scanner)
     return run_cmd(cbmc_cmd, label="cbmc", output_to="stdout", verbose=verbose, quiet=quiet, scanners=scanners, dry_run=dry_run)
 
+# Generates a viewer report from a goto program
 def run_visualize(cbmc_filename, prop_args, cover_args, verbose=False, quiet=False, keep_temps=False, function="main", srcdir=".", wkdir=".", outdir=".", dry_run=False):
     results_filename = os.path.join(outdir, "results.xml")
     coverage_filename = os.path.join(outdir, "coverage.xml")
     property_filename = os.path.join(outdir, "property.xml")
-    temp_goto_filename = os.path.join(outdir, "temp.goto")
     if not keep_temps:
-        for filename in [results_filename, coverage_filename, property_filename, temp_goto_filename]:
+        for filename in [results_filename, coverage_filename, property_filename]:
             atexit.register(delete_file, filename)
 
-    # 1) goto-cc --function main <cbmc_filename> -o temp.goto
-    # 2) cbmc --xml-ui --trace ~/rmc/library/rmc/rmc_lib.c temp.goto > results.xml
-    # 3) cbmc --xml-ui --cover location ~/rmc/library/rmc/rmc_lib.c temp.goto > coverage.xml
-    # 4) cbmc --xml-ui --show-properties ~/rmc/library/rmc/rmc_lib.c temp.goto > property.xml
-    # 5) cbmc-viewer --result results.xml --coverage coverage.xml --property property.xml --srcdir . --goto temp.goto --reportdir report
-
-    run_goto_cc(cbmc_filename, temp_goto_filename, verbose, quiet, function=function, dry_run=dry_run)
+    # 1) cbmc --xml-ui --trace ~/rmc/library/rmc/rmc_lib.c <cbmc_filename> > results.xml
+    # 2) cbmc --xml-ui --cover location ~/rmc/library/rmc/rmc_lib.c <cbmc_filename> > coverage.xml
+    # 3) cbmc --xml-ui --show-properties ~/rmc/library/rmc/rmc_lib.c <cbmc_filename> > property.xml
+    # 4) cbmc-viewer --result results.xml --coverage coverage.xml --property property.xml --srcdir . --goto <cbmc_filename> --reportdir report
     
     def run_cbmc_local(cbmc_args, output_to, dry_run=False):
-        cbmc_cmd = ["cbmc"] + cbmc_args + [temp_goto_filename]
+        cbmc_cmd = ["cbmc"] + cbmc_args + [cbmc_filename]
         return run_cmd(cbmc_cmd, label="cbmc", output_to=output_to, verbose=verbose, quiet=quiet, dry_run=dry_run)
 
     cbmc_prop_args = prop_args + ["--xml-ui"]
@@ -199,15 +208,12 @@ def run_visualize(cbmc_filename, prop_args, cover_args, verbose=False, quiet=Fal
     run_cbmc_local(cbmc_cover_args + ["--cover", "location"], coverage_filename, dry_run=dry_run)
     run_cbmc_local(cbmc_prop_args + ["--show-properties"], property_filename, dry_run=dry_run)
 
-    run_cbmc_viewer(temp_goto_filename, results_filename, coverage_filename,
+    run_cbmc_viewer(cbmc_filename, results_filename, coverage_filename,
                     property_filename, verbose, quiet, srcdir, wkdir, os.path.join(outdir, "report"), dry_run=dry_run)
 
     return retcode
 
-def run_goto_cc(src, dst, verbose=False, quiet=False, function="main", dry_run=False):
-    cmd = ["goto-cc"] + ["--function", function] + [src] + ["-o", dst]
-    return run_cmd(cmd, label="goto-cc", verbose=verbose, quiet=quiet, dry_run=dry_run)
-
+# Handler for calling cbmc-viewer
 def run_cbmc_viewer(goto_filename, results_filename, coverage_filename, property_filename, verbose=False, quiet=False, srcdir=".", wkdir=".", reportdir="report", dry_run=False):
     cmd = ["cbmc-viewer"] + \
           ["--result", results_filename] + \
@@ -219,15 +225,15 @@ def run_cbmc_viewer(goto_filename, results_filename, coverage_filename, property
           ["--reportdir", reportdir]
     return run_cmd(cmd, label="cbmc-viewer", verbose=verbose, quiet=quiet, dry_run=dry_run)
 
-
+# Handler for calling goto-instrument
 def run_goto_instrument(input_filename, output_filename, args, verbose=False, dry_run=False):
     cmd = ["goto-instrument"] + args + [input_filename]
     return run_cmd(cmd, label="goto-instrument", verbose=verbose, output_to=output_filename, dry_run=dry_run)
 
-
+# Generates a C program from a goto program
 def goto_to_c(goto_filename, c_filename, verbose=False, dry_run=False):
     return run_goto_instrument(goto_filename, c_filename, ["--dump-c"], verbose, dry_run=dry_run)
 
-
+# Generates the CMBC symbol table from a goto program
 def goto_to_symbols(goto_filename, symbols_filename, verbose=False, dry_run=False):
     return run_goto_instrument(goto_filename, symbols_filename, ["--show-symbol-table"], verbose, dry_run=dry_run)
--- src/test/cbmc/ForeignItems/main.rs
+++ src/test/cbmc/ForeignItems/main.rs
@@ -1,20 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //! To run this test, do
-//! rmc fixme_main.rs -- lib.c
+//! rmc main.rs -- lib.c
 
-// rmc-flags: --c-lib lib.c
+// rmc-flags: --c-lib src/test/cbmc/ForeignItems/lib.c
 
-// TODO, we should also test packed and transparent representations
-// https://doc.rust-lang.org/reference/type-layout.html
 #[repr(C)]
 pub struct Foo {
     i: u32,
     c: u8,
 }
 
-// TODO:
-//#[repr(packed)]
 #[repr(C)]
 pub struct Foo2 {
     i: u32,
@@ -25,19 +21,11 @@ pub struct Foo2 {
 // https://doc.rust-lang.org/reference/items/external-blocks.html
 // https://doc.rust-lang.org/nomicon/ffi.html
 extern "C" {
-    // NOTE: this currently works even if I don't make S static, but is UB.
-    // We should have a check for that.
     static mut S: u32;
 
     fn update_static();
     fn takes_int(i: u32) -> u32;
     fn takes_ptr(p: &u32) -> u32;
-    // In rust, you say nullable pointer by using option of reference.
-    // Rust guarantees that this has the bitwise represntation
-    // Some(&x) => &x;
-    // None => NULL;
-    // FIXME: we need to notice when this happens and do a bitcast, or C is unhappy
-    // https://github.com/model-checking/rmc/issues/3
     fn takes_ptr_option(p: Option<&u32>) -> u32;
     fn mutates_ptr(p: &mut u32);
     #[link_name = "name_in_c"]
@@ -56,8 +44,7 @@ fn main() {
 
         assert!(takes_int(1) == 3);
         assert!(takes_ptr(&5) == 7);
-        //assert!(takes_ptr_option(Some(&5)) == 4);
-        //assert!(takes_ptr_option(None) == 0);
+
         let mut p = 17;
         mutates_ptr(&mut p);
         assert!(p == 16);
@@ -70,6 +57,6 @@ fn main() {
 
         let f2 = Foo2 { i: 12, c: 7, i2: 8 };
         assert!(takes_struct_ptr2(&f2) == 19);
-        assert!(takes_struct2(f2) == 19);
+        assert!(takes_struct2(f2) == 20);
     }
 }
