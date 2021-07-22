02dbbad643d26ceab64f6ea9ad9a368f5e22eb89
Thomas Del Vecchio <vecchiot@amazon.com>
2021-07-22 16:10:35 -0400

    Add documentation and rearrange error handling.

--- scripts/rmc_flags.py
+++ scripts/rmc_flags.py
@@ -4,6 +4,7 @@
 
 import argparse
 
+# Add flags related to debugging output.
 def add_loudness_flags(make_group, add_flag):
     group = make_group(
         "Loudness flags", "Determine how much textual output to produce.")
@@ -14,7 +15,7 @@ def add_loudness_flags(make_group, add_flag):
     add_flag(group, "--quiet", "-q", action="store_true",
              help="Produces no output, just an exit code and requested artifacts. Overrides --verbose")
 
-
+# Add flags which specify configurations for the proof.
 def add_proof_harness_flags(make_group, add_flag):
     group = make_group("Proof harness flags",
                        "Provide information about the proof harness for RMC.")
@@ -23,7 +24,7 @@ def add_proof_harness_flags(make_group, add_flag):
     add_flag(group, "--function", default="main",
              help="Entry point for verification")
 
-
+# Add flags that produce extra artifacts.
 def add_artifact_flags(make_group, add_flag):
     group = make_group(
         "Artifact flags", "Produce artifacts in addition to a basic RMC report.")
@@ -36,7 +37,7 @@ def add_artifact_flags(make_group, add_flag):
     add_flag(group, "--gen-symbols", action="store_true",
              help="Generate a symbol table")
 
-
+# Add flags to turn off default checks.
 def add_check_flags(make_group, add_flag):
     group = make_group("Check flags", "Disable some or all default checks.")
     add_flag(group, "--no-default-checks", action="store_true",
@@ -48,13 +49,13 @@ def add_check_flags(make_group, add_flag):
     add_flag(group, "--no-unwinding-checks", action="store_true",
              help="Disable default unwinding checks")
 
-
+# Add flags needed only for visualizer.
 def add_visualizer_flags(make_group, add_flag):
     group = make_group(
         "Visualizer flags", "Generate an HTML-based UI for the generated RMC report.\nSee https://github.com/awslabs/aws-viewer-for-cbmc.")
-    add_flag(group, "--srcdir", default="."
+    add_flag(group, "--srcdir", default=".",
              help="The source directory. The root of the source tree.")
-    add_flag(group, "--wkdir", default="."
+    add_flag(group, "--wkdir", default=".",
              help="""
                   The working directory. Used to determine source locations in output.
                   This is generally the location from which rmc is currently being invoked.
@@ -62,7 +63,7 @@ def add_visualizer_flags(make_group, add_flag):
     add_flag(group, "--visualize", action="store_true",
              help="Generate visualizer report; open report/html/index.html")
 
-
+# Add flags for ad-hoc features.
 def add_other_flags(make_group, add_flag):
     group = make_group("Other flags")
     add_flag(group, "--allow-cbmc-verification-failure", action="store_true",
@@ -72,24 +73,33 @@ def add_other_flags(make_group, add_flag):
     add_flag(group, "--dry-run", action="store_true",
              help="Print commands instead of running them")
 
-
+# Add flags we don't expect end-users to use.
 def add_developer_flags(make_group, add_flag):
     group = make_group(
         "Developer flags", "These are generally meant for use by RMC developers, and are not stable.")
     add_flag(group, "--cbmc-args", nargs=argparse.REMAINDER, default=[],
              help="Pass through directly to CBMC; must be the last flag")
 
-
+# Adds the flags common to both rmc and cargo-rmc.
+# Allows you to specify flags/groups of flags to not add.
+# This does not return the parser, but mutates the one provided.
 def add_flags(parser, exclude_flags=[], exclude_groups=[]):
+    # Keep track of what excluded flags and groups we've seen
+    # so we can warn for possibly incorrect names passed in.
     excluded_flags = set()
     excluded_groups = set()
 
+    # Add a group to the parser with title/description, and get a handler for it.
     def make_group(title=None, description=None):
+        if title in exclude_groups:
+            excluded_groups.add(group.title)
+            return None
+
         return parser.add_argument_group(title, description)
 
+    # Add the flag to the group, 
     def add_flag(group, flag, *args, **kwargs):
-        if group.title in exclude_groups:
-            excluded_groups.add(group.title)
+        if group == None:
             return
 
         if flag in exclude_flags:
@@ -111,10 +121,16 @@ def add_flags(parser, exclude_flags=[], exclude_groups=[]):
     for add_group in add_groups:
         add_group(make_group, add_flag)
 
+    # Error if any excluded flags/groups don't exist.
     extra_groups = set(exclude_groups) - excluded_groups
     if extra_groups:
-        print(f"WARNING: Attempt to exclude groups which don't exist: {extra_groups}")
+        print(f"ERROR: Attempt to exclude groups which don't exist: {extra_groups}")
 
     extra_flags = set(exclude_flags) - excluded_flags
     if extra_flags:
-        print(f"WARNING: Attempt to exclude flags which don't exist: {extra_flags}")
+        print(f"ERROR: Attempt to exclude flags which don't exist: {extra_flags}")
+
+    if extra_groups or extra_flags:
+        return False
+
+    return True
