4e6020c08353619626c7d9d913f5a7ac30964bbb
Thomas Del Vecchio <vecchiot@amazon.com>
2021-07-26 10:14:00 -0400

    Reorganized.

--- scripts/rmc_flags.py
+++ scripts/rmc_flags.py
@@ -8,11 +8,11 @@ import argparse
 def add_loudness_flags(make_group, add_flag, config):
     group = make_group(
         "Loudness flags", "Determine how much textual output to produce.")
-    add_flag(group, "--debug", action="store_true",
+    add_flag(group, "--debug", default=False, action=argparse.BooleanOptionalAction,
              help="Produce full debug information")
-    add_flag(group, "--quiet", "-q", action="store_true",
+    add_flag(group, "--quiet", "-q", default=False, action=argparse.BooleanOptionalAction,
              help="Produces no output, just an exit code and requested artifacts; overrides --verbose")
-    add_flag(group, "--verbose", "-v", action="store_true",
+    add_flag(group, "--verbose", "-v", default=False, action=argparse.BooleanOptionalAction,
              help="Output processing stages and commands, along with minor debug information")
 
 # Add flags which specify configurations for the proof.
@@ -33,11 +33,11 @@ def add_artifact_flags(make_group, add_flag, config):
 
     group = make_group(
         "Artifact flags", "Produce artifacts in addition to a basic RMC report.")
-    add_flag(group, "--gen-c", action="store_true",
+    add_flag(group, "--gen-c", default=False, action=argparse.BooleanOptionalAction,
              help="Generate C file equivalent to inputted program")
-    add_flag(group, "--gen-symbols", action="store_true",
+    add_flag(group, "--gen-symbols", default=False, action=argparse.BooleanOptionalAction,
              help="Generate a goto symbol table")
-    add_flag(group, "--keep-temps", action="store_true",
+    add_flag(group, "--keep-temps", default=False, action=argparse.BooleanOptionalAction,
              help="Keep temporary files generated throughout RMC process")
     add_flag(group, "--target-dir", default=default_target, metavar="DIR",
              help=f"Directory for all generated artifacts; defaults to \"{default_target}\"")
@@ -45,14 +45,14 @@ def add_artifact_flags(make_group, add_flag, config):
 # Add flags to turn off default checks.
 def add_check_flags(make_group, add_flag, config):
     group = make_group("Check flags", "Disable some or all default checks.")
-    add_flag(group, "--no-default-checks", action="store_true",
-             help="Disable all default checks")
-    add_flag(group, "--no-memory-safety-checks", action="store_true",
-             help="Disable default memory safety checks")
-    add_flag(group, "--no-overflow-checks", action="store_true",
-             help="Disable default overflow checks")
-    add_flag(group, "--no-unwinding-checks", action="store_true",
-             help="Disable default unwinding checks")
+    add_flag(group, "--default-checks", default=True, action=argparse.BooleanOptionalAction,
+             help="Turn on all default checks")
+    add_flag(group, "--memory-safety-checks", default=True, action=argparse.BooleanOptionalAction,
+             help="Turn on default memory safety checks")
+    add_flag(group, "--overflow-checks", default=True, action=argparse.BooleanOptionalAction,
+             help="Turn on default overflow checks")
+    add_flag(group, "--unwinding-checks", default=True, action=argparse.BooleanOptionalAction,
+             help="Turn on default unwinding checks")
 
 # Add flags needed only for visualizer.
 def add_visualizer_flags(make_group, add_flag, config):
@@ -60,7 +60,7 @@ def add_visualizer_flags(make_group, add_flag, config):
         "Visualizer flags", "Generate an HTML-based UI for the generated RMC report.\nSee https://github.com/awslabs/aws-viewer-for-cbmc.")
     add_flag(group, "--srcdir", default=".",
              help="The source directory: the root of the source tree")
-    add_flag(group, "--visualize", action="store_true",
+    add_flag(group, "--visualize", default=True, action=argparse.BooleanOptionalAction,
              help="Generate visualizer report to <target-dir>/report/html/index.html")
     add_flag(group, "--wkdir", default=".",
              help="""
@@ -71,9 +71,9 @@ def add_visualizer_flags(make_group, add_flag, config):
 # Add flags for ad-hoc features.
 def add_other_flags(make_group, add_flag, config):
     group = make_group("Other flags")
-    add_flag(group, "--allow-cbmc-verification-failure", action="store_true",
+    add_flag(group, "--allow-cbmc-verification-failure", default=False, action=argparse.BooleanOptionalAction,
              help="Do not produce error return code on CBMC verification failure")
-    add_flag(group, "--dry-run", action="store_true",
+    add_flag(group, "--dry-run", default=False, action=argparse.BooleanOptionalAction,
              help="Print commands instead of running them")
 
 # Add flags we don't expect end-users to use.
