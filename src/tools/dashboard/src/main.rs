4342aa8dd9d741dd09ac3af3e4631f436347ec83
Abdalrhman M Mohamed <84409648+bdalrhm@users.noreply.github.com>
2021-08-09 09:41:39 -0500

    Add initial support for Litani build and dashboard. (#389)
    * Add initial support for Litani build and dashboard.
    
    * Add panic message and issue numbers.
    
    * Add a timeout for Litani jobs.
    
    * Add depencies between jobs.
    
    * Add links to issues.
    
    Co-authored-by: Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>

--- src/tools/dashboard/src/main.rs
+++ src/tools/dashboard/src/main.rs
@@ -1,8 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
+#![feature(command_access)]
 
 mod dashboard;
+mod litani;
 mod reference;
+mod util;
 
 fn main() {
     reference::display_reference_dashboard();
