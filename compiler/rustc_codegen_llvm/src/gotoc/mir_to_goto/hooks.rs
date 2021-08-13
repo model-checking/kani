cc1b6c0708437c6879f208351f753af57ac188c8
Daniel Schwartz-Narbonne <dsn@amazon.com>
2021-08-12 21:08:48 -0400

    removed pub mod from stubs

--- compiler/rustc_codegen_llvm/src/gotoc/mir_to_goto/hooks.rs
+++ compiler/rustc_codegen_llvm/src/gotoc/mir_to_goto/hooks.rs
@@ -9,8 +9,7 @@
 //! this module addresses this issue.
 
 use crate::gotoc::cbmc::goto_program::{BuiltinFn, Expr, Location, Stmt, Symbol, Type};
-use crate::gotoc::mir_to_goto::stubs::hash_map_stub::HashMapStub;
-use crate::gotoc::mir_to_goto::stubs::vec_stub::VecStub;
+use crate::gotoc::mir_to_goto::stubs::{HashMapStub, VecStub};
 use crate::gotoc::mir_to_goto::GotocCtx;
 use rustc_hir::definitions::DefPathDataName;
 use rustc_middle::mir::{BasicBlock, Place};
