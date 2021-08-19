396a4ed772b6996d1c47947d7d5ea0442c0dcd6e
Thomas Del Vecchio <vecchiot@amazon.com>
2021-08-19 12:58:26 -0400

    Update type_to_string to be injective.

--- compiler/rustc_codegen_llvm/src/gotoc/cbmc/goto_program/symtab_transformer/gen_c_transformer/common.rs
+++ compiler/rustc_codegen_llvm/src/gotoc/cbmc/goto_program/symtab_transformer/gen_c_transformer/common.rs
@@ -5,9 +5,17 @@ pub fn type_to_string(typ: &Type) -> String {
     match typ {
         Type::Array { typ, size } => format!("array_of_{}_{}", size, type_to_string(typ.as_ref())),
         Type::Bool => format!("bool"),
-        Type::CBitField { typ, .. } => format!("cbitfield_of_{}", type_to_string(typ.as_ref())),
-        Type::CInteger(_) => format!("c_int"),
-        Type::Code { .. } => format!("code"),
+        Type::CBitField { width, typ } => format!("cbitfield_of_{}_{}", width, type_to_string(typ.as_ref())),
+        Type::CInteger(int_kind) => format!("c_int_{:?}", int_kind),
+        Type::Code { parameters, return_type } => {
+            let parameter_string = parameters.iter()
+                .map(|param| param.typ())
+                .map(type_to_string)
+                .collect::<Vec<_>>()
+                .join("_");
+            let return_string = type_to_string(ret_type.as_ref());
+            format!("code_from_{}_to_{}", parameter_string, return_string)
+        }
         Type::Constructor => format!("constructor"),
         Type::Double => format!("double"),
         Type::Empty => format!("empty"),
@@ -21,11 +29,19 @@ pub fn type_to_string(typ: &Type) -> String {
         Type::Pointer { typ } => format!("pointer_to_{}", type_to_string(typ.as_ref())),
         Type::Signedbv { width } => format!("signed_bv_{}", width),
         Type::Struct { tag, .. } => format!("struct_{}", tag),
-        Type::StructTag(tag) => format!("struct_{}", tag),
+        Type::StructTag(tag) => format!("struct_tag_{}", tag),
         Type::Union { tag, .. } => format!("union_{}", tag),
-        Type::UnionTag(tag) => format!("union_{}", tag),
+        Type::UnionTag(tag) => format!("union_tag_{}", tag),
         Type::Unsignedbv { width } => format!("unsigned_bv_{}", width),
-        Type::VariadicCode { .. } => format!("variadic_code"),
-        Type::Vector { typ, .. } => format!("vec_of_{}", type_to_string(typ.as_ref())),
+        Type::VariadicCode { parameters, return_type } => {
+            let parameter_string = parameters.iter()
+                .map(|param| param.typ())
+                .map(type_to_string)
+                .collect::<Vec<_>>()
+                .join("_");
+            let return_string = type_to_string(ret_type.as_ref());
+            format!("variadic_code_from_{}_to_{}", parameter_string, return_string)
+        },
+        Type::Vector { size, typ } => format!("vec_of_{}_{}", size, type_to_string(typ.as_ref())),
     }
 }
