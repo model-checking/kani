54985e03ffe6562c831f5411b47df73ba87f182d
Zyad Hassan <zyadh@amazon.com>
2021-09-03 01:33:48 +0000

    Added a comment to explain the purpose of the test

--- src/test/cbmc/Enum/min_offset.rs
+++ src/test/cbmc/Enum/min_offset.rs
@@ -1,5 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we properly handle structs for which the compiler reorders the
+// fields to optimize the layout. In such cases, the field with minimum offset
+// need not be the first field in the original struct (e.g. in "Foo" below, "b"
+// is the field with minimum offset even though "a" is the leftmost field in the
+// original struct).
+
 enum E {
     Foo { a: u64, b: u16 },
     Bar,
