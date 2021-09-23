36ee990adae93c4885169a6ed597c1c7906d4419
Daniel Schwartz-Narbonne <dsn@amazon.com>
2021-09-22 21:56:50 -0400

    copyright notices

--- src/test/rmc-multicrate/type-mismatch/mismatch/src/lib.rs
+++ src/test/rmc-multicrate/type-mismatch/mismatch/src/lib.rs
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 #[no_mangle]
 fn main() {
     let arr = [1, 2, 3];
--- src/test/rmc-multicrate/type-mismatch/uses_core/src/lib.rs
+++ src/test/rmc-multicrate/type-mismatch/uses_core/src/lib.rs
@@ -1,3 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
 #![no_std]
 
 pub fn foo(_x: &[usize]) -> core::ops::Range<usize> {
