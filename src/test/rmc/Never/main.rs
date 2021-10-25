6dc7ebe0357c070c59b7a232e64152a16b2a9b9c
Daniel Schwartz-Narbonne <dsn@amazon.com>
2021-10-25 17:20:48 -0400

    Fix test, remove FIXME

--- src/test/rmc/Never/main.rs
+++ src/test/rmc/Never/main.rs
@@ -1,10 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// rmc-flags: --function foo
 #![feature(never_type)]
+use std::convert::Infallible;
 
 /// Test using the never type
-#[no_mangle]
 pub fn foo(never: !) -> i32 {
-    return 1
+    return 1;
 }
+
+pub fn bar(infalliable: Infallible) -> i32 {
+    return 1;
+}
+
+// Give an empty main to make rustc happy.
+pub fn main() {}
