dec89f58c49a9c59598e7528cccc48b0e5898000
Celina G. Val <celinval@amazon.com>
2021-11-02 08:57:34 -0700

    Update try_fold.rs
    Change the unwind value to be the minimum value.

--- src/test/rmc/Iterator/try_fold.rs
+++ src/test/rmc/Iterator/try_fold.rs
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// cbmc-flags: --unwind 20
+// cbmc-flags: --unwind 3
 
 pub fn main() {
     let arr = [(1, 2), (2, 2)];
