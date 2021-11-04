ba34ec64faae63e1f416c6615d84206038a7cb6e
Celina G. Val <celinval@amazon.com>
2021-11-04 13:35:14 -0700

    Get rid of skip monomorphizer hack (#485) (#624)
    Remove the hack we added to monomorphizer to skip functions that we were not able to handle.
    
    The latest fixes that we added to RMC was enough to allow us to run the monomorphizer as is.

---
+++ src/test/rmc/FunctionAbstractions/ptr_read.rs
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use std::ptr::read;
+
+pub fn main() {
+    let var = 1;
+    unsafe {
+        assert_eq!(read(&var), var);
+    }
+}
