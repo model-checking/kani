d63fb1c8c25bb8695bed8b0d1727ad633003312f
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2021-11-18 14:28:08 -0800

    Inherit parent environment when running cargo build (#653)
    * Inherit parent environment when running cargo build
    
    * Fixed formatting
    
    * Removed unnecessary passing of PATH environment since all environment variables are now inherited
    
    Co-authored-by: Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>

--- scripts/rmc.py
+++ scripts/rmc.py
@@ -267,10 +267,10 @@ def cargo_build(
     build_cmd = ["cargo", "build", "--lib", "--target-dir", str(target_dir)]
     if build_target:
         build_cmd += ["--target", str(build_target)]
-    build_env = {"RUSTFLAGS": " ".join(rustflags),
-                 "RUSTC": rustc_path,
-                 "PATH": os.environ["PATH"]
-                 }
+    build_env = os.environ
+    build_env.update({"RUSTFLAGS": " ".join(rustflags),
+                      "RUSTC": rustc_path
+                      })
     if debug:
         add_rmc_rustc_debug_to_env(build_env)
     if verbose:
