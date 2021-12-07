model-checking/kani

commit 7820fa576c68a8e6f812007c44e9737dbd154dba

Add option to restrict vtable function pointers (#536)
Add vtable restriction option, need CBMC PR to use

scripts/rmc
@@ -15,6 +15,7 @@ RMC_C_STUB = MY_PATH / "library" / "rmc" / "stubs" / "C"
 EXIT_CODE_SUCCESS = 0
 CBMC_VERIFICATION_FAILURE_EXIT_CODE = 10
 
+
 def main():
     args = parse_args()
 
@@ -36,7 +37,8 @@ def main():
                 args.keep_temps,
                 args.mangler,
                 args.dry_run,
-                ["gen-c"]):
+                ["gen-c"],
+                args.restrict_vtable):
             return 1
 
         out_files = rmc.symbol_table_to_gotoc([json_runnable_filename], args.verbose, args.keep_temps, args.dry_run)
@@ -44,15 +46,23 @@ def main():
         rmc.link_c_lib(out_files, goto_runnable_filename, args.c_lib, args.verbose, args.quiet, args.function,
                        args.dry_run, args.keep_temps)
 
-        if EXIT_CODE_SUCCESS != rmc.goto_to_c(goto_runnable_filename, c_runnable_filename, args.verbose, args.dry_run):
+        if EXIT_CODE_SUCCESS != rmc.goto_to_c(
+                goto_runnable_filename,
+                c_runnable_filename,
+                restrictions_runnable_filename,
+                args.verbose,
+                args.dry_run):
             return 1
 
         rmc.gen_c_postprocess(c_runnable_filename, args.dry_run)
 
     symbol_table_json_filename = base + ".symtab.json"
+    restrictions_filename = base + ".restrictions.json"
     goto_filename = base + ".goto"
     c_filename = base + ".c"
     symbols_filename = base + ".symbols"
+
+    restrictions_filename = restrictions_filename if args.restrict_vtable else None
 
     if EXIT_CODE_SUCCESS != rmc.compile_single_rust_file(
             args.input,
@@ -65,7 +75,8 @@ def main():
             args.dry_run,
             args.use_abs,
             args.abs_type,
-            []):
+            [],
+            args.restrict_vtable):
         return 1
 
     out_files = rmc.symbol_table_to_gotoc([symbol_table_json_filename], args.verbose, args.keep_temps, args.dry_run)
@@ -85,7 +96,13 @@ def main():
                    args.keep_temps)
 
     if args.gen_c:
-        if EXIT_CODE_SUCCESS != rmc.goto_to_c(goto_filename, c_filename, args.verbose, args.dry_run):
+        if EXIT_CODE_SUCCESS != rmc.goto_to_c(
+                goto_filename,
+                c_filename,
+                restrictions_filename,
+                args.verbose,
+                args.dry_run,
+                args.restrict_vtable):
             return 1
 
     if args.gen_symbols:
@@ -97,12 +114,16 @@ def main():
         if EXIT_CODE_SUCCESS != rmc.run_goto_instrument(
                 goto_filename, goto_filename,
                 ['--add-library'],
-                args.verbose, dry_run=args.dry_run):
+                args.verbose,
+                dry_run=args.dry_run,
+                restrictions_filename=restrictions_filename):
             return 1
         if EXIT_CODE_SUCCESS != rmc.run_goto_instrument(
                 goto_filename, goto_filename,
                 ['--generate-function-body-options', 'assert-false', '--generate-function-body', '.*', '--drop-unused-functions'],
-                args.verbose, dry_run=args.dry_run):
+                args.verbose,
+                dry_run=args.dry_run,
+                restrictions_filename=restrictions_filename):
             return 1
     else:
         # Apply --drop-unused-functions if undefined-function-checks was turned off
@@ -128,6 +149,7 @@ def main():
         retcode = EXIT_CODE_SUCCESS
 
     return retcode
+
 
 def parse_args():
     # Create parser

scripts/rmc.py
@@ -48,9 +48,11 @@ class Scanner:
     def edit_output(self, text):
         return self.edit_fun(text)
 
+
 def is_exe(name):
     from shutil import which
     return which(name) is not None
+
 
 def ensure_dependencies_in_path():
     for program in [RMC_RUSTC_EXE, "symtab2gb", "cbmc", "cbmc-viewer", "goto-instrument", "goto-cc"]:
@@ -199,13 +201,20 @@ def run_cmd(
 
     return process.returncode
 
-def rustc_flags(mangler, symbol_table_passes):
+
+def rustc_flags(mangler, symbol_table_passes, restrict_vtable):
     flags = [
         "-Z", f"symbol-mangling-version={mangler}",
         "-Z", f"symbol_table_passes={' '.join(symbol_table_passes)}",
-    ]
+        "-Z", "human_readable_cgu_names",
+        f"--cfg={RMC_CFG}"]
+
     if "RUSTFLAGS" in os.environ:
         flags += os.environ["RUSTFLAGS"].split(" ")
+
+    if restrict_vtable:
+        flags += ["-Z", "restrict_vtable_fn_ptrs"]
+
     return flags
 
 # Generates a symbol table from a rust file
@@ -220,12 +229,13 @@ def compile_single_rust_file(
         dry_run=False,
         use_abs=False,
         abs_type="std",
-        symbol_table_passes=[]):
+        symbol_table_passes=[],
+        restrict_vtable=False):
     if not keep_temps:
         atexit.register(delete_file, output_filename)
         atexit.register(delete_file, base + ".type_map.json")
 
-    build_cmd = [RMC_RUSTC_EXE] + rustc_flags(mangler, symbol_table_passes)
+    build_cmd = [RMC_RUSTC_EXE] + rustc_flags(mangler, symbol_table_passes, restrict_vtable)
 
     if use_abs:
         build_cmd += ["-Z", "force-unstable-if-unmarked=yes",
@@ -249,7 +259,9 @@ def cargo_build(
         debug=False,
         mangler="v0",
         dry_run=False,
-        symbol_table_passes=[]):
+        symbol_table_passes=[],
+        restrict_vtable=False):
+
     ensure(os.path.isdir(crate), f"Invalid path to crate: {crate}")
 
     def get_config(option):
@@ -265,7 +277,7 @@ def cargo_build(
                 process.stdout))
         return process.stdout
 
-    rustflags = rustc_flags(mangler, symbol_table_passes) + get_config("--rmc-flags").split()
+    rustflags = rustc_flags(mangler, symbol_table_passes, restrict_vtable) + get_config("--rmc-flags").split()
     rustc_path = get_config("--rmc-path").strip()
     build_cmd = ["cargo", "build", "--target-dir", str(target_dir)]
     if build_target:
@@ -356,7 +368,6 @@ def run_visualize(
     # 4) cbmc-viewer --result results.xml --coverage coverage.xml
     #                --property property.xml --srcdir .
     #                --goto <cbmc_filename> --reportdir report
-
     def run_cbmc_local(cbmc_args, output_to, dry_run=False):
         cbmc_cmd = ["cbmc"] + cbmc_args + [cbmc_filename]
         return run_cmd(cbmc_cmd, label="cbmc", output_to=output_to, verbose=verbose, quiet=quiet, dry_run=dry_run)
@@ -396,13 +407,39 @@ def run_cbmc_viewer(
     return run_cmd(cmd, label="cbmc-viewer", verbose=verbose, quiet=quiet, dry_run=dry_run)
 
 # Handler for calling goto-instrument
-def run_goto_instrument(input_filename, output_filename, args, verbose=False, dry_run=False):
+def run_goto_instrument(
+        input_filename,
+        output_filename,
+        args,
+        verbose=False,
+        dry_run=False,
+        restrictions_filename=None):
     cmd = ["goto-instrument"] + args + [input_filename, output_filename]
+    if restrictions_filename:
+        processed = process_vtable_restrictions(restrictions_filename, verbose=verbose, dry_run=dry_run)
+        cmd += ["--function-pointer-restrictions-file", processed]
     return run_cmd(cmd, label="goto-instrument", verbose=verbose, dry_run=dry_run)
 
+# Processes vtable restrictions to the format CBMC expects
+# TODO: once restrictions are on by default, we should build release ahead of time
+def process_vtable_restrictions(restrictions_filename, verbose=False, dry_run=False):
+    cmd = ["cargo", "run", "--release", "--manifest-path", "src/tools/rmc-link-restrictions/Cargo.toml"]
+    outname = os.path.join(os.path.dirname(os.path.abspath(restrictions_filename)), "linked-restrictions.json")
+    cmd += ["--", restrictions_filename, outname]
+    if (run_cmd(cmd, label="rmc-link-restrictions", verbose=verbose, dry_run=dry_run) != EXIT_CODE_SUCCESS):
+        raise Exception("Failed to run command: {}".format(" ".join(cmd)))
+    return outname
+
 # Generates a C program from a goto program
-def goto_to_c(goto_filename, c_filename, verbose=False, dry_run=False):
-    return run_goto_instrument(goto_filename, c_filename, ["--dump-c"], verbose, dry_run=dry_run)
+def goto_to_c(goto_filename, c_filename, restrictions_filename, verbose=False, dry_run=False):
+    args = ["--dump-c"]
+    return run_goto_instrument(
+        goto_filename,
+        c_filename,
+        args,
+        verbose,
+        dry_run=dry_run,
+        restrictions_filename=restrictions_filename)
 
 # Fix remaining issues with output of --gen-c-runnable
 def gen_c_postprocess(c_filename, dry_run=False):

Cargo.toml
@@ -5,6 +5,7 @@ members = [
   "library/std",
   "library/test",
   "library/rmc",
+  "library/rmc_restrictions",
   "src/rustdoc-json-types",
   "src/tools/cargotest",
   "src/tools/clippy",
@@ -21,6 +22,7 @@ members = [
   "src/tools/build-manifest",
   "src/tools/remote-test-client",
   "src/tools/remote-test-server",
+  "src/tools/rmc-link-restrictions",
   "src/tools/rust-installer",
   "src/tools/rust-demangler",
   "src/tools/cargo",

compiler/cbmc/src/irep/serialize.rs
@@ -98,6 +98,32 @@ impl Serialize for InternedString {
     }
 }
 
+struct InternedStringVisitor;
+
+impl<'de> serde::Deserialize<'de> for InternedString {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(InternedStringVisitor)
+    }
+}
+
+impl<'de> serde::de::Visitor<'de> for InternedStringVisitor {
+    type Value = InternedString;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str("a String like thing")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(v.into())
+    }
+}
+
 impl Serialize for Symbol {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where

compiler/rustc_codegen_rmc/Cargo.toml
@@ -21,6 +21,7 @@ serde = "1"
 serde_json = "1"
 snap = "1"
 tracing = "0.1"
+rmc_restrictions = { path = "../../library/rmc_restrictions" }
 rustc_middle = { path = "../rustc_middle" }
 rustc-demangle = "0.1.21"
 rustc_arena = { path = "../rustc_arena" }

compiler/rustc_codegen_rmc/src/codegen/assumptions.rs
@@ -178,7 +178,7 @@ impl<'tcx> GotocCtx<'tcx> {
                     self.find_function(fname)
                 }
             }
-            ty::Dynamic(_, _) => unimplemented!(),
+            ty::Dynamic(_, _) => None,
             ty::Projection(_) | ty::Opaque(_, _) => {
                 let normalized = self.tcx.normalize_erasing_regions(ty::ParamEnv::reveal_all(), t);
                 self.codegen_assumption_ref_ptr(fname, t, normalized, is_ref)

compiler/rustc_codegen_rmc/src/codegen/rvalue.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 use super::typ::{is_pointer, pointee_type, TypeExt};
 use crate::utils::{dynamic_fat_ptr, slice_fat_ptr};
-use crate::GotocCtx;
+use crate::{GotocCtx, VtableCtx};
 use cbmc::goto_program::{BuiltinFn, Expr, Location, Stmt, Symbol, Type};
 use cbmc::utils::{aggr_tag, BUG_REPORT_URL};
 use cbmc::MachineModel;
@@ -722,7 +722,17 @@ impl<'tcx> GotocCtx<'tcx> {
 
         // Lookup in the symbol table using the full symbol table name/key
         let fn_name = self.symbol_name(instance);
+
         if let Some(fn_symbol) = self.symbol_table.lookup(&fn_name) {
+            if self.vtable_ctx.emit_vtable_restrictions {
+                // Add to the possible method names for this trait type
+                self.vtable_ctx.add_possible_method(
+                    self.normalized_trait_name(t).into(),
+                    idx,
+                    fn_name.into(),
+                );
+            }
+
             // Create a pointer to the method
             // Note that the method takes a self* as the first argument, but the vtable field type has a void* as the first arg.
             // So we need to cast it at the end.
@@ -746,13 +756,22 @@ impl<'tcx> GotocCtx<'tcx> {
         trait_ty: &'tcx ty::TyS<'tcx>,
     ) -> Expr {
         let drop_instance = Instance::resolve_drop_in_place(self.tcx, ty).polymorphize(self.tcx);
-        let drop_sym_name = self.symbol_name(drop_instance);
+        let drop_sym_name: InternedString = self.symbol_name(drop_instance).into();
 
         // The drop instance has the concrete object type, for consistency with
         // type codegen we need the trait type for the function parameter.
         let trait_fn_ty = self.trait_vtable_drop_type(trait_ty);
 
-        if let Some(drop_sym) = self.symbol_table.lookup(&drop_sym_name) {
+        if let Some(drop_sym) = self.symbol_table.lookup(drop_sym_name) {
+            if self.vtable_ctx.emit_vtable_restrictions {
+                // Add to the possible method names for this trait type
+                self.vtable_ctx.add_possible_method(
+                    self.normalized_trait_name(trait_ty).into(),
+                    VtableCtx::drop_index(),
+                    drop_sym_name,
+                );
+            }
+
             Expr::symbol_expression(drop_sym_name, drop_sym.clone().typ)
                 .address_of()
                 .cast_to(trait_fn_ty)
@@ -761,37 +780,37 @@ impl<'tcx> GotocCtx<'tcx> {
             // for it. Build and insert a function that just calls an unimplemented block
             // to maintain soundness.
             let drop_sym_name = format!("{}_unimplemented", self.symbol_name(drop_instance));
+            let drop_sym = self.ensure(&drop_sym_name, |ctx, name| {
+                // Function body
+                let unimplemented = ctx
+                    .codegen_unimplemented(
+                        format!("drop_in_place for {}", drop_sym_name).as_str(),
+                        Type::empty(),
+                        Location::none(),
+                        "https://github.com/model-checking/rmc/issues/281",
+                    )
+                    .as_stmt(Location::none());
 
-            // Function body
-            let unimplemented = self
-                .codegen_unimplemented(
-                    format!("drop_in_place for {}", drop_sym_name).as_str(),
-                    Type::empty(),
+                // Declare symbol for the single, self parameter
+                let param_name = format!("{}::1::var{:?}", drop_sym_name, 0);
+                let param_sym = Symbol::variable(
+                    param_name.clone(),
+                    param_name,
+                    ctx.codegen_ty(trait_ty).to_pointer(),
                     Location::none(),
-                    "https://github.com/model-checking/rmc/issues/281",
+                );
+                ctx.symbol_table.insert(param_sym.clone());
+
+                // Build and insert the function itself
+                Symbol::function(
+                    name,
+                    Type::code(vec![param_sym.to_function_parameter()], Type::empty()),
+                    Some(Stmt::block(vec![unimplemented], Location::none())),
+                    NO_PRETTY_NAME,
+                    Location::none(),
                 )
-                .as_stmt(Location::none());
-
-            // Declare symbol for the single, self parameter
-            let param_name = format!("{}::1::var{:?}", drop_sym_name, 0);
-            let param_sym = Symbol::variable(
-                param_name.clone(),
-                param_name,
-                self.codegen_ty(trait_ty).to_pointer(),
-                Location::none(),
-            );
-            self.symbol_table.insert(param_sym.clone());
-
-            // Build and insert the function itself
-            let sym = Symbol::function(
-                &drop_sym_name,
-                Type::code(vec![param_sym.to_function_parameter()], Type::empty()),
-                Some(Stmt::block(vec![unimplemented], Location::none())),
-                NO_PRETTY_NAME,
-                Location::none(),
-            );
-            self.symbol_table.insert(sym.clone());
-            Expr::symbol_expression(drop_sym_name, sym.typ).address_of().cast_to(trait_fn_ty)
+            });
+            drop_sym.to_expr().address_of().cast_to(trait_fn_ty)
         }
     }
 

compiler/rustc_codegen_rmc/src/codegen/statement.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 use super::typ::TypeExt;
 use super::typ::FN_RETURN_VOID_VAR_NAME;
-use crate::GotocCtx;
+use crate::{GotocCtx, VtableCtx};
 use cbmc::goto_program::{BuiltinFn, Expr, Location, Stmt, Type};
 use rustc_hir::def_id::DefId;
 use rustc_middle::mir;
@@ -143,8 +143,17 @@ impl<'tcx> GotocCtx<'tcx> {
                             let self_ref =
                                 self_ref.cast_to(trait_fat_ptr.typ().clone().to_pointer());
 
-                            let func_exp: Expr = fn_ptr.dereference();
-                            func_exp.call(vec![self_ref]).as_stmt(Location::none())
+                            let call = if self.vtable_ctx.emit_vtable_restrictions {
+                                self.virtual_call_with_restricted_fn_ptr(
+                                    trait_fat_ptr.typ().clone(),
+                                    VtableCtx::drop_index(),
+                                    fn_ptr,
+                                    vec![self_ref],
+                                )
+                            } else {
+                                fn_ptr.dereference().call(vec![self_ref])
+                            };
+                            call.as_stmt(Location::none())
                         }
                         _ => {
                             // Non-virtual, direct drop call
@@ -449,12 +458,17 @@ impl<'tcx> GotocCtx<'tcx> {
         let assert_nonnull = Stmt::assert(call_is_nonnull, &assert_msg, loc.clone());
 
         // Virtual function call and corresponding nonnull assertion.
-        let func_exp: Expr = fn_ptr.dereference();
-        vec![
-            assert_nonnull,
-            self.codegen_expr_to_place(place, func_exp.call(fargs.to_vec()))
-                .with_location(loc.clone()),
-        ]
+        let call = if self.vtable_ctx.emit_vtable_restrictions {
+            self.virtual_call_with_restricted_fn_ptr(
+                trait_fat_ptr.typ().clone(),
+                idx,
+                fn_ptr,
+                fargs.to_vec(),
+            )
+        } else {
+            fn_ptr.dereference().call(fargs.to_vec())
+        };
+        vec![assert_nonnull, self.codegen_expr_to_place(place, call).with_location(loc.clone())]
     }
 
     /// A place is similar to the C idea of a LHS. For example, the returned value of a function call is stored to a place.

commit a74254470a1d00bba3613aac50f4985c7c753be9

Fix rmc --gen-c
There was a regression introduced by 7820fa576c6 that broke rmc --gen-c.
This change fixes that. I am also fixing a random white space from my
previous commit.

scripts/rmc
@@ -28,6 +28,8 @@ def main():
         json_runnable_filename = base + "_runnable.json"
         goto_runnable_filename = base + "_runnable.goto"
         c_runnable_filename = base + "_runnable.c"
+        restrictions_runnable_filename = os.path.join(
+            args.target_dir, "debug", "deps") if args.restrict_vtable else None
         if EXIT_CODE_SUCCESS != rmc.compile_single_rust_file(args.input, base, json_runnable_filename, args, ["gen-c"]):
             return 1
 
@@ -78,8 +80,7 @@ def main():
                 c_filename,
                 restrictions_filename,
                 args.verbose,
-                args.dry_run,
-                args.restrict_vtable):
+                args.dry_run):
             return 1
 
     if args.gen_symbols:

scripts/rmc.py
@@ -222,7 +222,6 @@ def compile_single_rust_file(
         input_filename,
         base,
         output_filename,
-
         extra_args,
         symbol_table_passes=[]):
     if not extra_args.keep_temps: