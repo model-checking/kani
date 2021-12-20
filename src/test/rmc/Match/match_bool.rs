35b117fa7be8b7516756b81da66c1a8c5edc62eb
Celina G. Val <celinval@amazon.com>
2021-12-20 15:22:48 -0800

    Revert change to rustc SwitchTargets type.
    We have changed the visibility of one of the members of SwitchTargets to
    have access to their values during codegen. I re-wrote the code to use
    standard API so we don't need to keep using a forked version of the
    compiler. Issue #42.
    
    The only changes left are the ones to enable rmc codegen and a a few
    options we enabled. Those should be handled once we have a new driver.

---
+++ src/test/rmc/Match/match_bool.rs
@@ -0,0 +1,28 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// compile-flags: --crate-type lib
+// rmc-flags: --function match_bool
+
+#[rmc::proof]
+pub fn match_bool(arg: bool) {
+    let var = match arg {
+        true => !arg,
+        _ => arg,
+    };
+
+    let var2 = match arg {
+        _ => false,
+    };
+
+    assert!(var == var2);
+
+    let mut i = 0;
+
+    match arg {
+        true => i = 1,
+        false => i = 2,
+        _ => i = 3,
+    }
+
+    assert!(i == 1 || i == 2);
+}
