67ebe6daabf0f4e36ea9954dcfdddfb185f0a75f
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2021-12-20 15:17:54 -0800

    Added test for #357 (#709)

---
+++ src/test/rmc/SubSlice/subslice3.rs
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+fn main() {
+    let slice = &[1, 2, 3][..];
+    if let [head, tail @ ..] = slice {
+        assert!(head == &slice[0]);
+        assert!(tail == &slice[1..]);
+    } else {
+        unreachable!();
+    }
+}
