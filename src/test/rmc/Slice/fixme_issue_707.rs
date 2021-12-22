d2ad0fc9462bd71649bf2a89b986b1f76db10495
Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>
2021-12-22 13:20:41 -0500

    Add a test for issue 707 (#713)

---
+++ src/test/rmc/Slice/fixme_issue_707.rs
@@ -0,0 +1,10 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+fn main() {
+    let [x, y @ .., z] = [1, 2, 3, 4];
+    assert!(x == 1);
+    assert!(y[0] == 2);
+    assert!(y[1] == 3);
+    assert!(z == 4);
+}
