6f89fa1629860f0da31cd8d944336905b2067529
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-02-07 10:47:35 -0800

    Introduce an std wrapper crate to allow overriding standard library definitions (#795)
    * Introduce an std wrapper crate that can be used for overriding standard library definitions

---
+++ library/std/src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! The purpose of this crate is to allow kani to selectively override
+//! definitions from the standard library.  Definitions provided below would
+//! override the standard library versions.
+
+// See discussion in
+// https://rust-lang.zulipchat.com/#narrow/stream/182449-t-compiler.2Fhelp/topic/.E2.9C.94.20Globally.20override.20an.20std.20macro/near/268873354
+// for more details.
+
+// re-export all std symbols
+pub use std::*;
