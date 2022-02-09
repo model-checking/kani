c25ca5ba2904c3b51fe007bce80aa8485267937c
Zyad Hassan <zyadh@amazon.com>
2022-02-09 14:40:16 -0800

    Reverted updates to expected tests

--- tests/expected/reach/test.rs
+++ tests/expected/reach/test.rs
@@ -1,15 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-flags: --function foo --assertion-reach-checks
+// kani-flags: --function foo --assertion-reach-checks --output-format regular
 
 #[kani::proof]
 fn foo(x: i32) {
     if x > 5 {
+        // fails
         assert!(x < 4);
         if x < 3 {
+            // unreachable
             assert!(x == 2);
         }
     } else {
+        // passes
         assert!(x <= 5);
     }
 }
