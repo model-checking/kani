bb1d7f3f1fee2021fc8967aae56cae9939ad4aa8
Celina G. Val <celinval@amazon.com>
2022-02-15 14:43:40 -0800

    Rename cbmc package to cprover_bindings (#829)
    * Move and rename cbmc crate to cprover_bindings
    
    Since we are planning to publish this crate separately from kani. We are
    moving this to be outside of the main crate src.
    
    Also, we are renaming the package as discussed here:
    https://github.com/diffblue/cbmc/issues/6440
    
    * Fix scripts after cbmc package rename
    
    For the format script, we can now use cargo fmt directly.
    
    Co-authored-by: Daniel Schwartz-Narbonne <danielsn@users.noreply.github.com>

--- scripts/kani-fmt.sh
+++ scripts/kani-fmt.sh
@@ -13,25 +13,10 @@ ROOT_FOLDER=$(git rev-parse --show-toplevel)
 cd ${ROOT_FOLDER}
 
 # Verify crates.
-# TODO: We should be able to use workspace once we unfork from rustc.
-# https://github.com/model-checking/kani/issues/719
-CRATES=(
-    "bookrunner"
-    "cbmc"
-    "compiletest"
-    "kani-compiler"
-    "kani"
-    "kani_macros"
-    "kani_queries"
-    "kani_restrictions"
-    "rustc_codegen_kani"
-)
-
 error=0
-for crate in ${CRATES[@]}; do
-    # Check all crates. Only fail at the end.
-    cargo fmt "$@" -p ${crate} || error=1
-done
+
+# Check all crates. Only fail at the end.
+cargo fmt "$@" || error=1
 
 # Check test source files.
 TESTS=("tests/kani"
