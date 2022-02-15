f8e6eb5416735b8ff37c3a7692739613eb593519
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-02-15 09:02:28 -0800

    Override the print macros with a version suitable for verification (#825)

---
+++ tests/kani/Print/side_effects.rs
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// This test checks that Kani's modeling of print macros takes into account any
+// side effects of the arguments
+
+fn subtract_two(x: &mut i32) -> i32 {
+    let y = *x - 2;
+    // side effect:
+    *x = *x + 5;
+    y
+}
+
+fn main() {
+    let mut x = 5;
+    println!("calling function with side-effect from println!: {}", subtract_two(&mut x));
+    assert!(x == 10);
+
+    eprintln!("calling function with side-effect from eprintln!: {}", subtract_two(&mut x));
+    assert!(x == 15);
+
+    print!("calling function with side-effect from print!: {}", subtract_two(&mut x));
+    assert!(x == 20);
+
+    eprint!("calling function with side-effect from eprint!: {}", subtract_two(&mut x));
+    assert!(x == 25);
+}
