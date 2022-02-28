56ca888fea9b6681ad90360dc27a2db927cdf39e
Adrian Palacios <accorell@amazon.com>
2022-02-28 21:35:49 +0000

    Add "fixme" test and update negative test

--- tests/kani/Intrinsics/Compiler/variant_count.rs
+++ tests/kani/Intrinsics/Compiler/variant_count.rs
@@ -3,21 +3,14 @@
 // kani-verify-fail
 
 // Check that `variant_count` is not supported.
-// Commented out code below can be enabled to ensure the implementation works as
-// expected when support is added
+// This test can be replaced with `variant_count_fixme.rs` once the intrinsic is
+// supported and works as expected.
+
 #![feature(variant_count)]
 use std::mem;
 
 enum Void {}
-enum MyError {
-    Error1,
-    Error2,
-}
 
 fn main() {
     let _ = mem::variant_count::<Void>();
-    // assert!(mem::variant_count::<Void>() == 0);
-    // assert!(mem::variant_count::<MyError>() == 2);
-    // assert!(mem::variant_count::<Option<u32>>() == 2);
-    // assert!(mem::variant_count::<Result<u32, MyError>>() == 2);
 }
