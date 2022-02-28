817b5754d015d9f2b6b398f0b12d994697665726
Celina G. Val <celinval@amazon.com>
2022-02-28 10:08:53 -0800

    Update call_single_file.rs

--- src/cargo-kani/src/call_single_file.rs
+++ src/cargo-kani/src/call_single_file.rs
@@ -56,6 +56,10 @@ impl KaniSession {
             }
         } else {
             if self.args.function != "main" {
+                // Unless entry function for proof harness is main, compile code as lib.
+                // This ensures that rustc won't prune functions that are not reachable
+                // from main as well as enable compilation of crates that don't have a main
+                // function.
                 args.push("--crate-type".into());
                 args.push("lib".into());
             }
