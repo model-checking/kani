e4aa490650e4b3512254ef0e07da10f9f1949efd
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-03-04 17:21:14 -0800

    Add reachability checks for some auto-generated asserts (#838)

---
+++ tests/expected/reach/div-zero/unreachable/test.rs
@@ -0,0 +1,15 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: --assertion-reach-checks --output-format regular --no-default-checks
+
+// This test checks that kani injects a reachability check for
+// divide-by-zero checks and that it reports ones that are unreachable.
+// The check in this test is unreachable, so should be reported as UNREACHABLE
+
+fn div(x: u16, y: u16) -> u16 {
+    if y != 0 { x / y } else { 0 }
+}
+
+fn main() {
+    div(5, 0);
+}
