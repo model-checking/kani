15520ce6a715a86e81a25ef2f9a0a425e50b12e4
Ted Kaminski <tedinski@amazon.com>
2022-03-07 21:43:26 +0000

    Rename to kani_metadata

--- tools/kani-link-restrictions/src/main.rs
+++ tools/kani-link-restrictions/src/main.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use cbmc::InternedString;
-use kani_restrictions::{TraitDefinedMethod, VtableCtxResults};
+use kani_metadata::{TraitDefinedMethod, VtableCtxResults};
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
