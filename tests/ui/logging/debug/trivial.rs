a27b86e42de1234f57f025b5fbf34de2f28b7e02
Celina G. Val <celinval@amazon.com>
2022-03-11 14:06:34 -0800

    Update trivial.rs

--- tests/ui/logging/debug/trivial.rs
+++ tests/ui/logging/debug/trivial.rs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-// kani-flags: --function harness
+// kani-flags: --function harness --debug
 // This test is to make sure we are correctly printing debug messages in the compiler.
 //
 // We don't rely in a specific debug message since they can change at any point. We do however
