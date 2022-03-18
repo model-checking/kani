259a4c11b480d4f632aba3b0869a056c9548f9fb
jaisnan <jaisnan@amazon.com>
2022-03-18 02:12:50 +0000

    Add expect_fail message to description in check

--- src/kani-compiler/src/codegen_cprover_gotoc/overrides/hooks.rs
+++ src/kani-compiler/src/codegen_cprover_gotoc/overrides/hooks.rs
@@ -102,7 +102,7 @@ impl<'tcx> GotocHook<'tcx> for ExpectFail {
 
         // msg is the comment field that's set on source location which is presented as the Description field
         // to the end user
-        let msg = "EXPECTED FAIL";
+        let msg = format!("EXPECTED FAIL: {}", utils::extract_const_message(&fargs.remove(0)).unwrap());
 
         // property_class is used as a unique class identiier
         let property_class = PropertyClass::ExpectFail;
@@ -110,7 +110,7 @@ impl<'tcx> GotocHook<'tcx> for ExpectFail {
         let loc = tcx.codegen_span_option(span);
         Stmt::block(
             vec![
-                Stmt::assert_stmt(cond, property_class, msg, loc.clone()),
+                Stmt::assert_stmt(cond, property_class, &msg, loc.clone()),
                 Stmt::goto(tcx.current_fn().find_label(&target), loc.clone()),
             ],
             loc,
