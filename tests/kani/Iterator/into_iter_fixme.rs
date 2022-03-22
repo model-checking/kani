7a7b2bf4176aef9b699ba7fcfa8f07af1e625e70
Celina G. Val <celinval@amazon.com>
2022-03-22 14:06:53 -0700

    Add fixme tests that reproduce issues #555, #556, and #560 (#952)
    Add tests for a few open issues.

---
+++ tests/kani/Iterator/into_iter_fixme.rs
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// compile-flags: --edition 2018
+// kani-flags: --unwind 4
+//
+// This reproduces the issue seen in "Failures when iterating over results".
+// See https://github.com/model-checking/kani/issues/556 for more information.
+#[kani::proof]
+pub fn main() {
+    let numbers = vec![1, 10, -1];
+    let positives: Vec<_> = numbers.into_iter().filter(|&n| n > 0).collect();
+    assert_eq!(positives.len(), 2);
+}
