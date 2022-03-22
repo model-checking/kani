d2e72ebf7257a2b2fff4f79a2aeeba627d2d14c3
Celina G. Val <celinval@amazon.com>
2022-03-22 00:34:22 -0700

    Add harness for failure + expected files

--- tests/cargo-kani/vecdeque-cve/src/lib.rs
+++ tests/cargo-kani/vecdeque-cve/src/lib.rs
@@ -11,21 +11,40 @@
 #![feature(ptr_internals)]
 #![feature(rustc_allow_const_fn_unstable)]
 
+mod cve;
+mod fixed;
 mod raw_vec;
 
 #[cfg(cve)]
-mod cve;
-#[cfg(cve)]
 use crate::cve::VecDeque;
 
-#[cfg(not(cve))]
-mod fixed;
 #[cfg(not(cve))]
 use crate::fixed::VecDeque;
 
 /// Prove that reserving a min capacity that is less than current capacity is no-op.
 #[kani::proof]
 pub fn reserve_less_capacity_is_no_op() {
+    let mut vec_deque = VecDeque::<i8>::new();
+    let old_capacity = vec_deque.capacity();
+
+    // Insert an element to empty VecDeque.
+    let front = kani::any();
+    vec_deque.push_front(front);
+
+    // Change extra capacity to *any* value that is less than current capacity.
+    let new_capacity: usize = kani::any();
+    kani::assume(new_capacity < old_capacity);
+    vec_deque.reserve(new_capacity);
+
+    // Capacity should stay the same.
+    assert_eq!(vec_deque.capacity(), old_capacity);
+}
+
+/// Trigger failure described in the CVE.
+#[kani::proof]
+pub fn reserve_less_capacity_cve() {
+    use crate::cve::VecDeque;
+
     let mut vec_deque = VecDeque::<i8>::new();
     let old_capacity = vec_deque.capacity();
 
