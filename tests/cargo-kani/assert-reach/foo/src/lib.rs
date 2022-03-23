2934527e6ea075365a1e3524a5dd991db7758b1e
Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>
2022-03-23 16:42:21 -0400

    Restore copyright check (#975)
    * Add script for copyright check and extend for modified headers
    
    * Simplify copyright step in workflow
    
    * Add copyright header to files that were missing it
    
    * Use variable to determine exit code

--- tests/cargo-kani/assert-reach/foo/src/lib.rs
+++ tests/cargo-kani/assert-reach/foo/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 pub fn foo(x: u32) -> u32 {
     let y = x / 2;
     let z = y * 2;
