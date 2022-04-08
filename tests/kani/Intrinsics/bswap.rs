d90f6bcf5653199f568efa8db69acbd72e55346c
Adrian Palacios <accorell@amazon.com>
2022-04-08 21:33:25 +0000

    Address comments

--- tests/kani/Intrinsics/bswap.rs
+++ tests/kani/Intrinsics/bswap.rs
@@ -1,7 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Check that `bswap` returns the expected results
+// Check that `bswap` returns the expected results.
+// https://doc.rust-lang.org/std/intrinsics/fn.bswap.html
+
+// `bswap` reverses the bytes in an integer type `T`, for example:
+//
+// ```
+// let n = 0x12345678u32;
+// let m = std::intrinsics::bswap(n);
+
+// assert_eq!(m, 0x78563412);
+// ```
+
 #![feature(core_intrinsics)]
 
 const BITS_PER_BYTE: usize = 8;
@@ -16,7 +27,8 @@ macro_rules! test_bswap_intrinsic {
                 bytes_res
         }
 
-        // Checks that the original byte is equal to the swapped byte
+        // Checks that the order of bytes in the original integer is reversed in
+        // the swapped integer
         fn $check_name(a: $ty, b: $ty) {
             let bytes = std::mem::size_of::<$ty>();
             let i: usize = kani::any();
@@ -32,14 +44,31 @@ macro_rules! test_bswap_intrinsic {
 }
 
 #[kani::proof]
-fn main() {
+fn test_bswap_u8() {
     test_bswap_intrinsic!(u8, check_bswap_u8, get_byte_at_u8);
-    test_bswap_intrinsic!(u16, check_bswap_u16, get_byte_at_u16);
-    test_bswap_intrinsic!(u32, check_bswap_u32, get_byte_at_u32);
-    test_bswap_intrinsic!(u64, check_bswap_u64, get_byte_at_u64);
-    test_bswap_intrinsic!(u128, check_bswap_u128, get_byte_at_u128);
-    // `bswap` also works with signed integer types, but this causes overflows
-    // unless we restrict their values considerably (due to how bytes are
-    // extracted), making the signed versions not very interesting to test here.
-    // https://github.com/model-checking/kani/issues/934
 }
+
+#[kani::proof]
+fn test_bswap_u16() {
+    test_bswap_intrinsic!(u16, check_bswap_u16, get_byte_at_u16);
+}
+
+#[kani::proof]
+fn test_bswap_u32() {
+    test_bswap_intrinsic!(u32, check_bswap_u32, get_byte_at_u32);
+}
+
+#[kani::proof]
+fn test_bswap_u64() {
+    test_bswap_intrinsic!(u64, check_bswap_u64, get_byte_at_u64);
+}
+
+#[kani::proof]
+fn test_bswap_u128() {
+    test_bswap_intrinsic!(u128, check_bswap_u128, get_byte_at_u128);
+}
+
+// `bswap` also works with signed integer types, but this causes overflows
+// unless we restrict their values considerably (due to how bytes are
+// extracted), making the signed versions not very interesting to test here.
+// https://github.com/model-checking/kani/issues/934
