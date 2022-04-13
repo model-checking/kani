638a86160fbf3aefdfa1ad4d214db05fde925650
Nathan Chong <ncchong@amazon.com>
2022-04-12 22:55:00 -0400

    Fix copyright

--- tests/cargo-kani/vecdeque-cve/issue44800.rs
+++ tests/cargo-kani/vecdeque-cve/issue44800.rs
@@ -1,4 +1,6 @@
-// file: issue44800.rs
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 // Example from https://github.com/rust-lang/rust/issues/44800 with a smaller queue size
 #![feature(global_allocator, alloc_system, allocator_api)]
 extern crate alloc_system;
