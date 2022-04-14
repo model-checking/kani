2edb5b28e496a2487eb574eb0cbe3d6b3c7f670d
Ted Kaminski <tedinski@amazon.com>
2022-04-14 15:30:28 -0500

    Introduce kani-verifier: proxy binaries (#1039)

---
+++ src/kani-verifier/build.rs
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+use std::env::var;
+
+fn main() {
+    // We want to know what target triple we were built with, but this isn't normally provided to us.
+    // Note the difference between:
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-crates
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
+    // So "repeat" the info from build script (here) to our crate's build environment.
+    println!("cargo:rustc-env=TARGET={}", var("TARGET").unwrap());
+}
