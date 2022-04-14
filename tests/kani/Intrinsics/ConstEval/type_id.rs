ac436508d215b5b18bae1c24a83c1f4816d64af5
Adrian Palacios <accorell@amazon.com>
2022-04-14 17:24:47 +0000

    In `type_id` test, assert no duplicates

--- tests/kani/Intrinsics/ConstEval/type_id.rs
+++ tests/kani/Intrinsics/ConstEval/type_id.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 // Check that the `type_id` intrinsic is supported with common data types
+// and that there are no duplicate type IDs
 #![feature(core_intrinsics)]
 use std::intrinsics::type_id;
 
@@ -11,27 +12,38 @@ enum MyEnum {}
 
 #[kani::proof]
 fn main() {
-    // Scalar types
-    let _ = type_id::<i8>();
-    let _ = type_id::<i16>();
-    let _ = type_id::<i32>();
-    let _ = type_id::<i64>();
-    let _ = type_id::<i128>();
-    let _ = type_id::<isize>();
-    let _ = type_id::<u8>();
-    let _ = type_id::<u16>();
-    let _ = type_id::<u32>();
-    let _ = type_id::<u64>();
-    let _ = type_id::<u128>();
-    let _ = type_id::<usize>();
-    let _ = type_id::<f32>();
-    let _ = type_id::<f64>();
-    let _ = type_id::<bool>();
-    let _ = type_id::<char>();
-    // Compound types (tuple and array)
-    let _ = type_id::<(i32, i32)>();
-    let _ = type_id::<[i32; 5]>();
-    // Custom data types (struct and enum)
-    let _ = type_id::<MyStruct>();
-    let _ = type_id::<MyEnum>();
+    let type_ids = [
+        // Scalar types
+        type_id::<i8>(),
+        type_id::<i16>(),
+        type_id::<i32>(),
+        type_id::<i64>(),
+        type_id::<i128>(),
+        type_id::<isize>(),
+        type_id::<u8>(),
+        type_id::<u16>(),
+        type_id::<u32>(),
+        type_id::<u64>(),
+        type_id::<u128>(),
+        type_id::<usize>(),
+        type_id::<f32>(),
+        type_id::<f64>(),
+        type_id::<bool>(),
+        type_id::<char>(),
+        // Compound types (tuple and array)
+        type_id::<(i32, i32)>(),
+        type_id::<[i32; 5]>(),
+        // Custom data types (struct and enum)
+        type_id::<MyStruct>(),
+        type_id::<MyEnum>(),
+    ];
+
+    // Check that there are no duplicate type IDs
+    let i: usize = kani::any();
+    let j: usize = kani::any();
+    kani::assume(i < type_ids.len());
+    kani::assume(j < type_ids.len());
+    if i != j {
+        assert_ne!(type_ids[i], type_ids[j]);
+    }
 }
