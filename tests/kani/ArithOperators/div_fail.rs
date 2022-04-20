d209d2df20e2434daa1d4dc5d7c3fa118e1c7dbd
Adrian Palacios <accorell@amazon.com>
2022-04-20 14:51:59 +0000

    Add negative tests for regular div/rem

---
+++ tests/kani/ArithOperators/div_fail.rs
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-verify-fail
+
+// Check that division triggers overflow checks.
+// Covers the case where `x == T::MIN && y == -1`.
+
+#[kani::proof]
+fn main() {
+    let a: i8 = i8::MIN;
+    let b: i8 = -1;
+    let _ = a / b;
+}
