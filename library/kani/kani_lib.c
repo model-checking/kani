c9b5441998081cff54e8fc46783cc900a2892327
Zyad Hassan <zyadh@amazon.com>
2022-04-21 08:28:47 -0700

    Address PR comments

--- library/kani/kani_lib.c
+++ library/kani/kani_lib.c
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -10,19 +11,22 @@
 //     fn __rust_realloc(ptr: *mut u8, old_size: usize, align: usize, new_size: usize) -> *mut u8;
 // The implementation pretty much follows CBMC's implementation:
 // https://github.com/diffblue/cbmc/blob/94b81b22799876b52632ba9cdc3f486f87abadb8/src/ansi-c/library/stdlib.c#L472
-uint8_t* __rust_realloc(uint8_t* ptr, size_t old_size, size_t align, size_t new_size) {
+uint8_t *__rust_realloc(uint8_t *ptr, size_t old_size, size_t align, size_t new_size)
+{
     // if current ptr is NULL, this behaves like malloc
-    if (ptr == 0)
+    if (ptr == 0) {
+        assert(old_size == 0);
         return malloc(new_size);
+    }
 
-    // if malloc-size is 0, free original object and return malloc(0) which
-    // returns an invalid pointer
+    // if malloc-size is 0, free original object and return malloc(0), which in
+    // CBMC returns a pointer to a zero-sized object
     if (new_size == 0) {
         free(ptr);
         return malloc(0);
     }
 
-    uint8_t* result = malloc(new_size);
+    uint8_t *result = malloc(new_size);
     if (result) {
         size_t bytes_to_copy = new_size < old_size ? new_size : old_size;
         memcpy(result, ptr, bytes_to_copy);
