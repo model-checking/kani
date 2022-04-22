376035809e25e6de5e64a9cdee4bff6a9ea3ee59
Zyad Hassan <zyadh@amazon.com>
2022-04-21 18:10:12 -0700

    Add asserts for UB

--- library/kani/kani_lib.c
+++ library/kani/kani_lib.c
@@ -7,24 +7,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-// This is a C implementation of the __rust_realloc function that has the following signature
+// This is a C implementation of the __rust_realloc function that has the following signature:
 //     fn __rust_realloc(ptr: *mut u8, old_size: usize, align: usize, new_size: usize) -> *mut u8;
-// The implementation pretty much follows CBMC's implementation:
-// https://github.com/diffblue/cbmc/blob/94b81b22799876b52632ba9cdc3f486f87abadb8/src/ansi-c/library/stdlib.c#L472
+// This low-level function is called by std::alloc:realloc, and its
+// implementation is provided by the compiler backend, so we need to provide an
+// implementation for it.
+// For safety, refer to the documentation of GlobalAlloc::realloc:
+// https://doc.rust-lang.org/std/alloc/trait.GlobalAlloc.html#method.realloc
 uint8_t *__rust_realloc(uint8_t *ptr, size_t old_size, size_t align, size_t new_size)
 {
-    // if current ptr is NULL, this behaves like malloc
-    if (ptr == 0) {
-        assert(old_size == 0);
-        return malloc(new_size);
-    }
+    // Passing a NULL pointer is undefined behavior
+    __CPROVER_assert(ptr != 0, "realloc called with a null pointer");
+    __CPROVER_assume(ptr != 0);
 
-    // if malloc-size is 0, free original object and return malloc(0), which in
-    // CBMC returns a pointer to a zero-sized object
-    if (new_size == 0) {
-        free(ptr);
-        return malloc(0);
-    }
+    // Passing a new_size of 0 is undefined behavior
+    __CPROVER_assert(new_size > 0, "realloc called with a new_size of 0");
+    __CPROVER_assume(new_size > 0);
 
     uint8_t *result = malloc(new_size);
     if (result) {
