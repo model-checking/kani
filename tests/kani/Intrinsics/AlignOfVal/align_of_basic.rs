a84b84f59478fcb5d273d5af95a4054cc00d0d72
Adrian Palacios <accorell@amazon.com>
2022-04-23 20:07:49 +0000

    Basic tests for `size_of_val` and `min_alig_of_val`

---
+++ tests/kani/Intrinsics/AlignOfVal/align_of_basic.rs
@@ -0,0 +1,44 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that we get the expected results for the `min_align_of_val` intrinsic
+// with common data types
+#![feature(core_intrinsics)]
+use std::intrinsics::min_align_of_val;
+
+struct MyStruct {
+    val: u32,
+}
+
+enum MyEnum {
+    Variant
+}
+
+#[kani::proof]
+fn main() {
+    // Scalar types
+    unsafe {
+    assert!(min_align_of_val(&0i8) == 1);
+    assert!(min_align_of_val(&0i16) == 2);
+    assert!(min_align_of_val(&0i32) == 4);
+    assert!(min_align_of_val(&0i64) == 8);
+    assert!(min_align_of_val(&0i128) == 8);
+    assert!(min_align_of_val(&0isize) == 8);
+    assert!(min_align_of_val(&0u8) == 1);
+    assert!(min_align_of_val(&0u16) == 2);
+    assert!(min_align_of_val(&0u32) == 4);
+    assert!(min_align_of_val(&0u64) == 8);
+    assert!(min_align_of_val(&0u128) == 8);
+    assert!(min_align_of_val(&0usize) == 8);
+    assert!(min_align_of_val(&0f32) == 4);
+    assert!(min_align_of_val(&0f64) == 8);
+    assert!(min_align_of_val(&false) == 1);
+    assert!(min_align_of_val(&(0 as char)) == 4);
+    // Compound types (tuple and array)
+    assert!(min_align_of_val(&(0i32, 0i32)) == 4);
+    assert!(min_align_of_val(&[0i32; 5]) == 4);
+    // Custom data types (struct and enum)
+    assert!(min_align_of_val(&MyStruct { val: 0u32 }) == 4);
+    assert!(min_align_of_val(&MyEnum::Variant) == 1);
+    }
+}
