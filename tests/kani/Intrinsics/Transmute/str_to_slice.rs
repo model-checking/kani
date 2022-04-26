543da9e290c17cf012570f43808f21b9c8098a5d
Adrian Palacios <accorell@amazon.com>
2022-04-26 22:27:32 +0000

    Add tests for transmute

---
+++ tests/kani/Intrinsics/Transmute/str_to_slice.rs
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Checks that `transmute` works as expected when turning a `str` into
+// `&[u8]`.
+
+// This test is a modified version of the example found in
+// https://doc.rust-lang.org/std/intrinsics/fn.transmute.html
+
+#[kani::proof]
+fn main() {
+    let slice = unsafe { std::mem::transmute::<&str, &[u8]>("Rust") };
+    assert_eq!(slice, &[82, 117, 115, 116]);
+}
