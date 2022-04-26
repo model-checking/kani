48a02c46cc12cf31af4cbc9ad76025c2747aaead
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-04-26 16:26:08 -0700

    Error out for crates with global ASM (#1112)

---
+++ tests/cargo-kani/asm/global_error/src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Kani should error out if run without --ignore-global-asm even if the crate
+// with global ASM is not called
+#[kani::proof]
+fn doesnt_call_crate_with_global_asm() {
+    let x = 3;
+    let y = 11;
+    assert_eq!(x * y, 33);
+}
