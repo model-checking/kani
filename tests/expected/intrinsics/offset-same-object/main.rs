9af460d637379938f5a12602dba71d67fe7c2361
Adrian Palacios <accorell@amazon.com>
2022-04-26 23:05:57 +0000

    Move "same object" test to expected

--- tests/expected/intrinsics/offset-same-object/main.rs
+++ tests/expected/intrinsics/offset-same-object/main.rs
@@ -1,7 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-verify-fail
 
+// Checks that `ptr_offset_from` fails if the object that the arguments
+// point to are not the same.
 #![feature(core_intrinsics)]
 use std::intrinsics::ptr_offset_from;
 
@@ -12,11 +13,6 @@ fn main() {
     let ptr1: *const i32 = &a[1];
     let ptr2: *const i32 = &b[3];
     unsafe {
-        // Offset operations result in Undefined Behavior if
-        // some conditions are violated. More info at:
-        // https://doc.rust-lang.org/std/primitive.pointer.html#method.offset_from
-        // In particular, the below call to `ptr_offset_from` is expected
-        // to fail because `ptr1` and `ptr2` point to different objects.
         let offset = ptr_offset_from(ptr2, ptr1);
     }
 }
