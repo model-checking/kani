7382f4d598afe2bfbfcfd723e1d2b11dfcf4d08b
Adrian Palacios <accorell@amazon.com>
2022-04-27 00:30:58 +0000

    Fix tests

--- tests/kani/Intrinsics/PtrGuaranteedCmp/eq.rs
+++ tests/kani/Intrinsics/PtrGuaranteedCmp/eq.rs
@@ -1,15 +1,19 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-verify-fail
 
-// Checks that `ptr_guaranteed_eq` returns true when both pointers
-// are equal.
+// Checks that `ptr_guaranteed_eq` returns true if the pointers are equal, false
+// otherwise.
 #![feature(core_intrinsics)]
 use std::intrinsics::ptr_guaranteed_eq;
 
 #[kani::proof]
-fn test_ptr_eq(ptr1: *const u8, ptr2: *const u8) {
+fn test_ptr_eq_eq(ptr1: *const u8, ptr2: *const u8) {
     kani::assume(ptr1 == ptr2);
     assert!(ptr_guaranteed_eq(ptr1, ptr2));
-    kani::expect_fail(ptr1 != ptr2, "Pointers aren't different");
+}
+
+#[kani::proof]
+fn test_ptr_eq_ne(ptr1: *const u8, ptr2: *const u8) {
+    kani::assume(ptr1 != ptr2);
+    assert!(!ptr_guaranteed_eq(ptr1, ptr2));
 }
--- tests/kani/Intrinsics/PtrGuaranteedCmp/ne.rs
+++ tests/kani/Intrinsics/PtrGuaranteedCmp/ne.rs
@@ -1,15 +1,19 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-verify-fail
 
-// Checks that `ptr_guaranteed_ne` returns true when both pointers
-// are different.
+// Checks that `ptr_guaranteed_ne` returns true if the pointers are different,
+// false otherwise.
 #![feature(core_intrinsics)]
 use std::intrinsics::ptr_guaranteed_ne;
 
 #[kani::proof]
-fn test_ptr_eq(ptr1: *const u8, ptr2: *const u8) {
+fn test_ptr_ne_ne(ptr1: *const u8, ptr2: *const u8) {
     kani::assume(ptr1 != ptr2);
     assert!(ptr_guaranteed_ne(ptr1, ptr2));
-    kani::expect_fail(ptr1 == ptr2, "Pointers aren't equal");
+}
+
+#[kani::proof]
+fn test_ptr_ne_eq(ptr1: *const u8, ptr2: *const u8) {
+    kani::assume(ptr1 == ptr2);
+    assert!(!ptr_guaranteed_ne(ptr1, ptr2));
 }
