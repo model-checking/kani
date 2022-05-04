2685c1448f3ea16620f131891928b06481430dd8
Zyad Hassan <zyadh@amazon.com>
2022-05-04 11:29:19 -0700

    Use a specific version of CBMC for both Ubuntu and macOS

--- scripts/setup/macos-10.15/install_cbmc.sh
+++ scripts/setup/macos-10.15/install_cbmc.sh
@@ -4,5 +4,14 @@
 
 set -eux
 
-# Install CBMC for macOS 10.15
-brew install cbmc
+if [[ $# -ne 1 ]]; then
+  echo "$0: Error: Specify the CBMC version to install"
+  exit 1
+fi
+
+CBMC_VERSION=$1
+
+# Install CBMC for macOS 10.15 from CBMC tap
+# https://github.com/diffblue/cbmc/blob/develop/doc/ADR/homebrew_tap.md
+brew tap diffblue/cbmc
+brew install diffblue/cbmc/cbmc@${CBMC_VERSION}
