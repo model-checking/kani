017b7c12af2fb8cbcd9411b46a95d806bb3c2a08
jaisnan <jaisnan@amazon.com>
2022-05-10 18:21:45 +0000

    Update test suite

--- tests/ui/Property-Class-UI/unwind_expect_fail/unwind_success/main.rs
+++ tests/ui/Property-Class-UI/unwind_expect_fail/unwind_success/main.rs
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #[kani::proof]
-#[kani::unwind(9)]
+#[kani::unwind(10)]
 fn main() {
     let i: i32 = kani::any();
     kani::assume(i < 10);
-    kani::expect_fail(i < 20, "Blocked by assumption above.");
+    kani::expect_fail(i > 10, "Blocked by assumption above.");
     let mut counter = 0;
     loop {
         counter += 1;
