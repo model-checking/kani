26e8b3379ec0d67b5199195c657118cd52ae8c39
Adrian Palacios <accorell@amazon.com>
2022-05-16 21:09:35 +0000

    Add missed changes on one test

--- tests/expected/intrinsics/copy-nonoverlapping/copy-unaligned-dst/main.rs
+++ tests/expected/intrinsics/copy-nonoverlapping/copy-unaligned-dst/main.rs
@@ -1,9 +1,9 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Checks that `copy` fails when `dst` is not aligned.
+// Checks that `copy_nonoverlapping` fails when `dst` is not aligned.
 #[kani::proof]
-fn test_copy_unaligned() {
+fn test_copy_nonoverlapping_unaligned() {
     let arr: [i32; 3] = [0, 1, 0];
     let src: *const i32 = arr.as_ptr();
 
@@ -11,6 +11,6 @@ fn test_copy_unaligned() {
         // Get an unaligned pointer with a single-byte offset
         let dst_i8: *const i8 = src.add(1) as *mut i8;
         let dst_unaligned = unsafe { dst_i8.add(1) as *mut i32 };
-        core::intrinsics::copy(src, dst_unaligned, 1);
+        core::intrinsics::copy_nonoverlapping(src, dst_unaligned, 1);
     }
 }
