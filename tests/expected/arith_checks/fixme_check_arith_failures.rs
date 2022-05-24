46748e44b24fb72d7f3700045c9d38cc9bb8f3ab
Celina G. Val <celinval@amazon.com>
2022-05-24 13:53:20 -0700

    Document override + test

--- tests/expected/arith_checks/fixme_check_arith_failures.rs
+++ tests/expected/arith_checks/fixme_check_arith_failures.rs
@@ -1,10 +1,10 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Check that code after an overflow check failure is unreachable.
-
-#[cfg_attr(kani, kani::proof)]
-fn main() {
+//! Checks that code after an overflow check failure should be unreachable.
+//! Related issue: https://github.com/model-checking/kani/issues/1208
+#[kani::proof]
+fn check_arith_overflow() {
     let a = [0; 5];
     let ptr0: *const i32 = &a[0];
     let ptr1: *const i32 = &a[1];
