6eab5663080baf3f4b328e3c04122e9506fa4745
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-05-25 10:50:11 -0700

    Fixed how the number of checks is calculated (#1220)

---
+++ tests/expected/empty/main.rs
@@ -0,0 +1,7 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+/// This test checks that zero checks are reported for an empty test
+
+#[kani::proof]
+fn check_nothing() {}
