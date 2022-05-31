1a31f85a66d45915950d290a034513699070207c
Ted Kaminski <tedinski@amazon.com>
2022-05-31 22:31:41 +0000

    Add nixos support

--- src/os_hacks.rs
+++ src/os_hacks.rs
@@ -9,7 +9,8 @@ use std::ffi::OsString;
 use std::path::Path;
 use std::process::Command;
 
-use anyhow::Result;
+use anyhow::{Result, Context, bail};
+use os_info::Info;
 
 use crate::cmd::AutoRun;
 
@@ -44,4 +45,51 @@ pub fn setup_python_deps_on_ubuntu_18_04(pyroot: &Path, pkg_versions: &[&str]) -
     std::fs::remove_dir_all(pyroot.join("lib"))?;
 
     Ok(())
+}
+
+/// This is the final step of setup, where we look for OSes that require additional setup steps
+/// beyond the usual ones that we have done already.
+pub fn setup_os_hacks(kani_dir: &Path, os: &Info) -> Result<()> {
+    match os.os_type() {
+        os_info::Type::NixOS => setup_nixos_patchelf(kani_dir),
+        _ => Ok(())
+    }
+}
+
+/// On NixOS, the dynamic linker does not live at the standard path, and so our downloaded
+/// pre-built binaries need patching.
+fn setup_nixos_patchelf(kani_dir: &Path) -> Result<()> {
+    // Encode our assumption that we're working on x86 here, because when we add ARM
+    // support, we need to look for a different path.
+    assert!(env!("TARGET") == "x86_64-unknown-linux-gnu");
+    if Path::new("/lib64/ld-linux-x86-64.so.2").exists() {
+        // if the expected path exists, I guess things are fine?
+        return Ok(())
+    }
+
+    println!("[NixOS detected] Applying 'patchelf' to downloaded binaries");
+    
+    // patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" ~/.kani/kani-0.1.0/bin/*
+    if let Some(nix_cc) = std::env::var_os("NIX_CC") {
+        let path = Path::new(&nix_cc).join("nix-support/dynamic-linker");
+        let interp_raw = std::fs::read_to_string(path).context("Couldn't read $NIX_CC/nix-support/dynamic-linker")?;
+        let interp = interp_raw.trim();
+
+        let bin = kani_dir.join("bin");
+        for entry in std::fs::read_dir(bin)? {
+            let file = entry?;
+            if file.file_type()?.is_file() {
+                Command::new("patchelf").args(&["--set-interpreter", interp]).arg(file.file_name()).run()?;
+            }
+        }
+    } else {
+        bail!("On NixOS but 'NIX_CC` environment variable not set, couldn't apply patchelf.");
+    }
+
+    Ok(())
+}
+
+#[test]
+fn check() {
+    assert_eq!("", env!("TARGET"));
 }
