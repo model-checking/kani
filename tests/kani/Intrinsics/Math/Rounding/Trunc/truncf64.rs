f3afb10e713cbd25e3f4dd24920aa3da20fbfad0
Adrian Palacios <accorell@amazon.com>
2022-06-07 23:20:45 +0000

    Update `truncf64` test

--- tests/kani/Intrinsics/Math/Rounding/Trunc/truncf64.rs
+++ tests/kani/Intrinsics/Math/Rounding/Trunc/truncf64.rs
@@ -1,16 +1,34 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 #![feature(core_intrinsics)]
+use std::intrinsics::truncf64;
 
-// Checks that `truncf64` returns the integer part of a number.
+// Checks that `truncf64` does return:
+//  * The integral part of a number for a couple of concrete cases.
+//  * A value that is closer to zero in all cases.
+
 #[kani::proof]
-fn main() {
+fn test_one() {
+    let one = 1.0;
+    let trunc_res = unsafe { truncf64(one) };
+    assert!(trunc_res == 1.0);
+}
+
+#[kani::proof]
+fn test_one_frac() {
+    let one_frac = 1.9;
+    let trunc_res = unsafe { truncf64(one_frac) };
+    assert!(trunc_res == 1.0);
+}
+
+#[kani::proof]
+fn test_towards_zero() {
     let x: f64 = kani::any();
-    kani::assume(x.is_finite());
-
-    let trunc_res = unsafe { std::intrinsics::truncf64(x) };
-
-    // The expected result is the number minus its fractional part
-    let expected_res = x - x.fract();
-    assert!(trunc_res == expected_res);
+    kani::assume(!x.is_nan());
+    let trunc_res = unsafe { truncf64(x) };
+    if x.is_sign_positive() {
+        assert!(trunc_res <= x);
+    } else {
+        assert!(trunc_res >= x);
+    }
 }
