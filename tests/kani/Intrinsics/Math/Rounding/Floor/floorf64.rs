5f8e2e69dd69a3e9033c5c101d059507e421da40
Adrian Palacios <accorell@amazon.com>
2022-06-08 21:06:18 +0000

    Rename to `result`

--- tests/kani/Intrinsics/Math/Rounding/Floor/floorf64.rs
+++ tests/kani/Intrinsics/Math/Rounding/Floor/floorf64.rs
@@ -12,44 +12,44 @@ use std::intrinsics::floorf64;
 #[kani::proof]
 fn test_one() {
     let one = 1.0;
-    let ceil_res = unsafe { floorf64(one) };
-    assert!(ceil_res == 1.0);
+    let result = unsafe { floorf64(one) };
+    assert!(result == 1.0);
 }
 
 #[kani::proof]
 fn test_one_frac() {
     let one_frac = 1.2;
-    let ceil_res = unsafe { floorf64(one_frac) };
-    assert!(ceil_res == 1.0);
+    let result = unsafe { floorf64(one_frac) };
+    assert!(result == 1.0);
 }
 
 #[kani::proof]
 fn test_one_neg() {
     let one_neg = -1.8;
-    let ceil_res = unsafe { floorf64(one_neg) };
-    assert!(ceil_res == -2.0);
+    let result = unsafe { floorf64(one_neg) };
+    assert!(result == -2.0);
 }
 
 #[kani::proof]
 fn test_conc() {
     let conc = -42.6;
-    let ceil_res = unsafe { floorf64(conc) };
-    assert!(ceil_res == -43.0);
+    let result = unsafe { floorf64(conc) };
+    assert!(result == -43.0);
 }
 
 #[kani::proof]
 fn test_conc_sci() {
     let conc = 5.4e-2;
-    let ceil_res = unsafe { floorf64(conc) };
-    assert!(ceil_res == 0.0);
+    let result = unsafe { floorf64(conc) };
+    assert!(result == 0.0);
 }
 
 #[kani::proof]
 fn test_towards_inf() {
     let x: f64 = kani::any();
     kani::assume(!x.is_nan());
-    let ceil_res = unsafe { floorf64(x) };
-    assert!(ceil_res <= x);
+    let result = unsafe { floorf64(x) };
+    assert!(result <= x);
 }
 
 #[kani::proof]
@@ -57,8 +57,8 @@ fn test_diff_one() {
     let x: f64 = kani::any();
     kani::assume(!x.is_nan());
     kani::assume(!x.is_infinite());
-    let ceil_res = unsafe { floorf64(x) };
-    let diff = (x - ceil_res).abs();
+    let result = unsafe { floorf64(x) };
+    let diff = (x - result).abs();
     // `diff` can be 1.0 if `x` is very small (e.g., -6.938894e-18)
     assert!(diff <= 1.0);
     assert!(diff >= 0.0);
