adebe367f26f1eacbe23e9b336599627c5590d51
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2022-06-08 12:05:00 -0700

    Report CBMC verification time (#1247)

---
+++ tests/expected/report/verification-time/main.rs
@@ -0,0 +1,22 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// This test is meant for checking that the "Verification time:" line (which
+// reports the time spent in CBMC) is printed in the output
+
+fn is_sorted(s: &[i32]) -> bool {
+    for i in 0..s.len() - 1 {
+        if !(s[i] <= s[i + 1]) {
+            return false;
+        }
+    }
+    true
+}
+
+#[kani::proof]
+#[kani::unwind(6)]
+fn check_sorted() {
+    let mut arr: [i32; 5] = kani::any();
+    arr.sort();
+    assert!(is_sorted(&arr));
+}
