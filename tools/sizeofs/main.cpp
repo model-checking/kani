28a9117b8e5a8d17336d0b2d10ba2fa1a2c9d1d6
Sebastien Soudan <sebastien.soudan@gmail.com>
2022-06-29 10:22:39 -0700

    initial m1 support
    * Modified check_target() in compiler_interface.rs to accept 'arm64-apple-*',
    * machine_model_from_session() in goto_ctx.rs to use the parameters for this target
    * added a small cpp in tools/sizeofs/main.cpp to generate these)
    * fixed a few tests that had hardcoded assumes on type sizes and alignements.

---
+++ tools/sizeofs/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <type_traits>
+ 
+int main() 
+{    
+    std::cout << "let bool_width = " << sizeof(bool)*8 << ";" << std::endl;
+    std::cout << "let char_is_unsigned = " << (std::is_unsigned<bool>::value?"true":"false") << ";" << std::endl;
+    std::cout << "let char_width = " << sizeof(char)*8 << ";" << std::endl;   
+    std::cout << "let double_width = " << sizeof(double)*8 << ";" << std::endl;
+    std::cout << "let float_width = " << sizeof(float)*8 << ";" << std::endl;
+    std::cout << "let int_width = " << sizeof(int)*8 << ";" << std::endl;
+    std::cout << "let long_double_width = " << sizeof(long double)*8 << ";" << std::endl;
+    std::cout << "let long_int_width = " << sizeof(long int)*8 << ";" << std::endl;
+    std::cout << "let long_long_int_width = " << sizeof(long long int)*8 << ";" << std::endl;
+    // memory_operand_size
+    // null_is_zero 
+    std::cout << "let short_int_width = " << sizeof(short int)*8 << ";" << std::endl;
+    std::cout << "let single_width = " << sizeof(float)*8 << ";" << std::endl;
+    std::cout << "let wchar_t_is_unsigned = " << (std::is_unsigned<wchar_t>::value?"true":"false") << ";" << std::endl;
+    std::cout << "let wchar_t_width = " << sizeof(wchar_t)*8 << ";" << std::endl;    
+    // word_size
+    // rounding_mode    
+}
+
+ 
