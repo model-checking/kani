6031464102cb7defa762bce67739ab1484c048cc
Yoshi Takashima <ytakashl@amazon.com>
2022-06-29 18:49:30 -0400

    Changed any_vec to use exact_vec for vector generation. Show diff.

--- library/kani/src/vec.rs
+++ library/kani/src/vec.rs
@@ -1,13 +1,13 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-use crate::{any, assume, Arbitrary, Invariant};
+use crate::{any, assume, Invariant};
 
 /// Generates an arbitrary vector whose length is at most MAX_LENGTH.
 pub fn any_vec<T, const MAX_LENGTH: usize>() -> Vec<T>
 where
-    T: Arbitrary,
+    T: Invariant,
 {
-    let mut v = Vec::with_capacity(MAX_LENGTH);
+    let mut v = exact_vec::<T, MAX_LENGTH>();
     let real_length: usize = any();
     assume(real_length <= MAX_LENGTH);
     unsafe { v.set_len(real_length) };
