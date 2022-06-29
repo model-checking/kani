0e32dabdb48aa6e2c2472e78ac70d8b68038d49e
Yoshi Takashima <ytakashl@amazon.com>
2022-06-29 18:41:57 -0400

    Adjusted test case to fail. Detailed that failure in comments.

--- tests/kani/Vectors/any/fixme_append.rs
+++ tests/kani/Vectors/any/fixme_append.rs
@@ -2,10 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 /// Append 2 arbitrary vectors, lengths at most 1 and 2 respectively,
-/// and assert they have been appended.  However, times out with
-/// unwinding > 1. However, note that this works fine with exact_vec.
+/// and assert they have been appended.  This test case takes 53
+/// minutes with #[kani::unwind(75)], but takes less than a second
+/// with semantically equivalent exact_vec. This performance problem
+/// is the subject of issue #1322.
 #[kani::proof]
-#[kani::unwind(75)]
+#[kani::unwind(1)]
 fn main() {
     let mut v1: Vec<u128> = kani::vec::any_vec::<_, 1>();
     kani::assume(v1.len() == 1);
