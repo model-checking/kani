e824ef059f1566a7ef7ccaeeb069e438eeff70f9
Yoshi Takashima <ytakashl@amazon.com>
2022-06-29 10:54:08 -0400

    Loosened trait requirements with Arbitrary.

--- library/kani/src/vec.rs
+++ library/kani/src/vec.rs
@@ -1,11 +1,11 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-use crate::{any, Invariant};
+use crate::{any, Arbitrary};
 
 /// Generates an arbitrary vector given a length.
 pub fn any_vec<T, const VEC_LENGTH: usize>() -> Vec<T>
 where
-    T: Invariant,
+    T: Arbitrary,
 {
     let boxed_any_sice: Box<[T; VEC_LENGTH]> = Box::new(any());
     <[T]>::into_vec(boxed_any_sice)
