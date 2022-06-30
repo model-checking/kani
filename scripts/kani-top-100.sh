2482ad36b699e7d079feb3b72ed6b69478549fd1
Yoshi Takashima <ytakashl@amazon.com>
2022-06-30 19:50:40 -0400

    String comparison bug again.

--- scripts/kani-top-100.sh
+++ scripts/kani-top-100.sh
@@ -149,7 +149,7 @@ function print_errors_for_each_repo_result {
     IS_FAIL='0'
 
     error_code="$(cat $DIRECTORY/$EXIT_CODE_SUFFIX)"
-    if [ "$error_code" -ne "0" ]; then
+    if ! [ "$error_code" = "0" ]; then
 	echo -e "Error exit: code $error_code\n"
 	IS_FAIL='1'
     fi
