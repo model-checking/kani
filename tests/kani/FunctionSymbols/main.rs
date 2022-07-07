6367313aac47142f5bd43d792c9c81e61f359cb2
Fabian Zaiser <fzaiser@amazon.com>
2022-07-07 11:50:33 -0400

    Reorganize tests and add comments

--- tests/kani/FunctionSymbols/main.rs
+++ tests/kani/FunctionSymbols/main.rs
@@ -1,37 +1,7 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// examples discovered while debugging issue https://github.com/model-checking/kani/issues/1243
-
-#[kani::proof]
-fn example1() {
-    f(h);
-}
-
-fn f(g: impl Fn() -> ()) {
-    move || g();
-}
-
-fn h() {}
-
-#[kani::proof]
-fn example2() {
-    std::iter::empty().try_fold(0, map_try_fold(&mut |x: usize| x, usize::checked_add));
-}
-
-fn map_try_fold<'a, T, B, Acc, R>(
-    f: &'a mut impl FnMut(T) -> B,
-    mut g: impl FnMut(Acc, B) -> R + 'a,
-) -> impl FnMut(Acc, T) -> R + 'a {
-    move |acc, elt| g(acc, f(elt))
-}
-
-#[kani::proof]
-fn example3() {
-    Vec::<String>::new().join("");
-}
-
-// other FnDef tests:
+//! Test code generation for FnDef items
 
 #[kani::proof]
 fn test_reify_fn_pointer() {
