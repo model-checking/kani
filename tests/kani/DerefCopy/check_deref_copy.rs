b154ec93af1877ac3d9072eec31d966a940ba5e1
Celina G. Val <celinval@amazon.com>
2022-07-22 10:06:34 -0700

    Update toolchain to nightly-2022-07-19 (#1399)
    * Update toolchain to nightly-2022-07-19
    
    This update required the following changes:
      - Add support to ProjectionElem::OpaqueCast
      - Add support to Rvalue::CopyForDeref
      - Add support to ConstValue::ZST
      - Rename debugging_opts to unstable_opts
      - Change to mem::uninit/zeroed validity checks
      - Change vecdeque harness due to std capacity check
    
    Co-authored-by: Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>

---
+++ tests/kani/DerefCopy/check_deref_copy.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// Modifications Copyright Kani Contributors
+// See GitHub history for details.
+
+/// Adapted from:
+/// <https://github.com/rust-lang/rust/blob/29c5a028b0c92aa5da6a8eb6d6585a389fcf1035/src/test/mir-opt/derefer_test.rs>
+#[kani::proof]
+fn check_deref_copy() {
+    let mut a = (42, 43);
+    let mut b = (99, &mut a);
+    let x = &mut (*b.1).0;
+    let y = &mut (*b.1).1;
+    assert_eq!(*x, 42);
+    assert_eq!(*y, 43);
+}
