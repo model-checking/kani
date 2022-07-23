5b0c1597254d82183dabf5a025a045eabfddf531
Yoshi Takashima <ytakashl@amazon.com>
2022-07-22 20:55:36 -0400

    Clippy todos and publish fix for kani_macros.

--- library/kani_macros/src/lib.rs
+++ library/kani_macros/src/lib.rs
@@ -8,6 +8,9 @@
 // So we have to enable this on the commandline (see kani-rustc) with:
 //   RUSTFLAGS="-Zcrate-attr=feature(register_tool) -Zcrate-attr=register_tool(kanitool)"
 
+// todo: resolve clippy warnings
+#![allow(clippy::len_zero,clippy::redundant_clone)]
+
 // proc_macro::quote is nightly-only, so we'll cobble things together instead
 use proc_macro::TokenStream;
 
