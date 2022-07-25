2985448e94db5cfcf4ec59f97749e7e500018ff4
Yoshi Takashima <ytakashl@amazon.com>
2022-07-25 16:25:17 -0400

    Fixed clippy warnings under library/

--- library/kani_macros/src/lib.rs
+++ library/kani_macros/src/lib.rs
@@ -8,9 +8,6 @@
 // So we have to enable this on the commandline (see kani-rustc) with:
 //   RUSTFLAGS="-Zcrate-attr=feature(register_tool) -Zcrate-attr=register_tool(kanitool)"
 
-// todo: resolve clippy warnings
-#![allow(clippy::len_zero, clippy::redundant_clone)]
-
 // proc_macro::quote is nightly-only, so we'll cobble things together instead
 use proc_macro::TokenStream;
 
@@ -42,7 +39,7 @@ pub fn proof(_attr: TokenStream, item: TokenStream) -> TokenStream {
 pub fn proof(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut result = TokenStream::new();
 
-    assert!(attr.to_string().len() == 0, "#[kani::proof] does not take any arguments");
+    assert!(attr.to_string().is_empty(), "#[kani::proof] does not take any arguments");
     result.extend("#[kanitool::proof]".parse::<TokenStream>().unwrap());
     // no_mangle is a temporary hack to make the function "public" so it gets codegen'd
     result.extend("#[no_mangle]".parse::<TokenStream>().unwrap());
@@ -70,7 +67,7 @@ pub fn unwind(attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut result = TokenStream::new();
 
     // Translate #[kani::unwind(arg)] to #[kanitool::unwind(arg)]
-    let insert_string = "#[kanitool::unwind(".to_owned() + &attr.clone().to_string() + ")]";
+    let insert_string = "#[kanitool::unwind(".to_owned() + &attr.to_string() + ")]";
     result.extend(insert_string.parse::<TokenStream>().unwrap());
 
     result.extend(item);
