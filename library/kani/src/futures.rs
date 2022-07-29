ec9e14403d7e4aea46c00b74e63e0e9ba7e7782b
Fabian Zaiser <fzaiser@amazon.com>
2022-07-29 18:58:53 -0400

    Address review comments

--- library/kani/src/futures.rs
+++ library/kani/src/futures.rs
@@ -1,15 +1,20 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+//! This module contains functions to work with futures (and async/.await) in Kani.
+
 use std::{
     future::Future,
     pin::Pin,
     task::{Context, RawWaker, RawWakerVTable, Waker},
 };
 
-/// Polls the future in a loop until completion (a very simple executor)
+/// A very simple executor: it polls the future in a busy loop until completion
 ///
 /// This is intended as a drop-in replacement for `futures::block_on`, which Kani cannot handle.
+/// Whereas a clever executor like `block_on` in `futures` or `tokio` would interact with the OS scheduler
+/// to be woken up when a resource becomes available, this is not supported by Kani.
+/// As a consequence, this function completely ignores the waker infrastructure and just polls the given future in a busy loop.
 pub fn block_on<T>(mut fut: impl Future<Output = T>) -> T {
     let waker = unsafe { Waker::from_raw(NOOP_RAW_WAKER) };
     let cx = &mut Context::from_waker(&waker);
