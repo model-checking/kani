5674243608a7642f4b537143e084c5cd09b33f3e
Yoshi Takashima <ytakashl@amazon.com>
2022-08-03 17:13:22 -0400

    Added basic test case.

---
+++ tests/cargo-kani/proptest/src/lib.rs
@@ -0,0 +1,18 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! cargo-kani test crate for proptest.
+
+use proptest::test_runner::Config;
+
+// check if the proptest library is linked and macro is working.
+proptest::proptest!{
+    fn successfully_linked_proptest(_ in proptest::strategy::Just(()) ) {
+        let config = Config::default();
+        assert_eq!(
+            config.cases,
+            256,
+            "Default .cases should be 256. Check library/proptest/src/test_runner/config.rs"
+        );
+    }
+}
