8a6ae0263c22c496420241c8425d3dbf1eec2562
Sanjit Bhat <sanjit.bhat@gmail.com>
2022-08-04 11:35:38 -0400

    test: add f32, f64 tests

---
+++ tests/ui/exe-trace/f64/main.rs
@@ -0,0 +1,26 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// kani-flags: --harness harness --enable-unstable --gen-exe-trace
+
+#[kani::proof]
+pub fn harness() {
+    let f64_1: f64 = kani::any();
+    let f64_2: f64 = kani::any();
+    let f64_3: f64 = kani::any();
+    let f64_4: f64 = kani::any();
+    let f64_5: f64 = kani::any();
+    let f64_6: f64 = kani::any();
+    let f64_7: f64 = kani::any();
+    let f64_8: f64 = kani::any();
+    assert!(
+        !(f64_1 == f64::NEG_INFINITY
+            && f64_2 == f64::MIN
+            && f64_3 == -101f64
+            && f64_4 == 0f64
+            && f64_5 == f64::MIN_POSITIVE
+            && f64_6 == 101f64
+            && f64_7 == f64::MAX
+            && f64_8 == f64::INFINITY)
+    );
+}
