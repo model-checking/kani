39f8d64d690d2fc9709139eb325b3118cc4c09a6
Sanjit Bhat <sanjit.bhat@gmail.com>
2022-08-04 11:20:49 -0400

    test: split tests into different directories per primitive

---
+++ tests/ui/exe-trace/README.md
@@ -0,0 +1,1 @@
+These tests check that the correct deterministic values are read and formatted into an executable trace unit test case.
---
+++ tests/ui/exe-trace/usize/main.rs
@@ -0,0 +1,12 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// kani-flags: --harness harness --enable-unstable --gen-exe-trace
+
+#[kani::proof]
+pub fn harness() {
+    let usize_1: usize = kani::any();
+    let usize_2: usize = kani::any();
+    let usize_3: usize = kani::any();
+    assert!(!(usize_1 == usize::MIN && usize_2 == 101 && usize_3 == usize::MAX));
+}
