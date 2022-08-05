2461c74f187c97ef59d1e6ee9a85f2c70b394da3
Daniel Schwartz-Narbonne <dsn@amazon.com>
2022-08-05 11:22:48 -0400

    PR Comments

--- tests/kani/Array/array_init.rs
+++ tests/kani/Array/array_init.rs
@@ -16,3 +16,52 @@ fn init_option() {
     kani::assume(i < 6);
     assert_eq!(a[i], Some(4));
 }
+
+#[kani::proof]
+fn init_double_array() {
+    let a = [Some(4u8); 6];
+    let b = [a; 3];
+    let i: usize = kani::any();
+    kani::assume(i < 3);
+    let j: usize = kani::any();
+    kani::assume(j < 6);
+    assert_eq!(b[i][j], Some(4));
+}
+
+#[repr(C)]
+struct NonCopy {
+    a: u8,
+    b: u32,
+    c: Option<u16>,
+}
+
+#[kani::proof]
+fn init_array_of_non_copystruct() {
+    let v = NonCopy { a: 1, b: 2, c: Some(3) };
+    // If length is >1, Rust complains that the struct must be `Copy`.
+    let a = [v; 1];
+    let i: usize = kani::any();
+    kani::assume(i < 1);
+    assert_eq!(a[i].a, 1);
+    assert_eq!(a[i].b, 2);
+    assert_eq!(a[i].c, Some(3));
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Copyable {
+    a: u8,
+    b: u32,
+    c: Option<u16>,
+}
+
+#[kani::proof]
+fn init_array_of_struct() {
+    let v = Copyable { a: 1, b: 2, c: Some(3) };
+    let a = [v; 6];
+    let i: usize = kani::any();
+    kani::assume(i < 6);
+    assert_eq!(a[i].a, 1);
+    assert_eq!(a[i].b, 2);
+    assert_eq!(a[i].c, Some(3));
+}
