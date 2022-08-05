11794be0d025d5d7a46fca04e2ca3cc0c892616b
Yoshi Takashima <ytakashl@amazon.com>
2022-08-05 18:06:22 -0400

    Rustfmt.

--- tests/cargo-kani/proptest/src/arbitrary_boolean.rs
+++ tests/cargo-kani/proptest/src/arbitrary_boolean.rs
@@ -1,11 +1,10 @@
-
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //! A proptest that uses arbitrary "any" function
 
-use proptest::bool;
 use proptest::arbitrary::any;
+use proptest::bool;
 
 proptest::proptest! {
     fn arbitrary_boolean((_, (a,b)) in (any::<()>(), any::<(bool, bool)>()) ) {
