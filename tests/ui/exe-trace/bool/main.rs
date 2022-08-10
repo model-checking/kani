a990c9cbf2227b37a17d8e3b37a8aa6905896abd
Sanjit Bhat <sanjit.bhat@gmail.com>
2022-08-10 12:56:20 -0400

    feat: add bool test

---
+++ tests/ui/exe-trace/bool/main.rs
@@ -0,0 +1,10 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// kani-flags: --harness harness --enable-unstable --gen-exe-trace
+
+#[kani::proof]
+pub fn harness() {
+    let bool_1: bool = kani::any();
+    assert!(bool_1 != true);
+}
