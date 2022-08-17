7e84b0047c9d99375698cbec04df3ed1d563a228
Celina G. Val <celinval@amazon.com>
2022-08-17 14:22:09 -0700

    Remove deprecated function and adapt tests (#1534)
    - Removed kani::any_raw() and Invariants.
     - Removed handling of old __VERIFIER_* functions.

--- tests/kani/Arbitrary/arbitrary_structs.rs
+++ tests/kani/Arbitrary/arbitrary_structs.rs
@@ -1,12 +1,12 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-// Check that users can use any Option, Result and array if they implement Arbitrary or Invariant.
-#![cfg_attr(kani, feature(min_specialization))]
-
+//! Check that users can use any Option, Result and array if they implement Arbitrary type.
+//! Note: This test could use some clean up. It uses macros and PercentTraint because it used to
+//! check the Invariant trait as well but we have removed that.
 extern crate kani;
+
 use kani::Arbitrary;
-use kani::Invariant;
 
 trait PercentTrait {
     fn val(&self) -> u8;
@@ -30,35 +30,13 @@ macro_rules! percent_type {
     };
 }
 
-percent_type!(Percent);
-percent_type!(PercentInvariant);
 percent_type!(PercentArbitrary);
-
-unsafe impl Invariant for PercentInvariant {
-    fn is_valid(&self) -> bool {
-        self.ok()
-    }
-}
 
 impl Arbitrary for PercentArbitrary {
     fn any() -> Self {
         let val = kani::any();
         kani::assume(val <= 100);
         PercentArbitrary { inner: val }
-    }
-}
-
-unsafe impl Invariant for Percent {
-    fn is_valid(&self) -> bool {
-        self.ok()
-    }
-}
-
-impl Arbitrary for Percent {
-    fn any() -> Self {
-        let val = kani::any();
-        kani::assume(val <= 100);
-        Percent { inner: val }
     }
 }
 
@@ -88,24 +66,8 @@ where
 
 #[kani::proof]
 #[kani::unwind(12)]
-fn check_invariant() {
-    check::<PercentInvariant>();
-    check_result::<PercentInvariant>();
-    check_array::<PercentInvariant>();
-}
-
-#[kani::proof]
-#[kani::unwind(12)]
 fn check_arbitrary() {
     check::<PercentArbitrary>();
     check_result::<PercentArbitrary>();
     check_array::<PercentArbitrary>();
-}
-
-#[kani::proof]
-#[kani::unwind(12)]
-fn check_both() {
-    check::<Percent>();
-    check_result::<Percent>();
-    check_array::<Percent>();
 }
