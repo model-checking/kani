20f92a14cfd638fd9cb2779e12e5d6bd7cb11488
Daniel Schwartz-Narbonne <dsn@amazon.com>
2022-09-02 22:56:29 -0400

    add comment

--- kani-compiler/kani_queries/src/unsound_experiments.rs
+++ kani-compiler/kani_queries/src/unsound_experiments.rs
@@ -5,5 +5,9 @@
 
 #[derive(Debug, Default)]
 pub struct UnsoundExperiments {
+    /// Zero initilize variables.
+    /// This is useful for experiments to see whether assigning constant values produces better
+    /// performance by allowing CBMC to do more constant propegation.
+    /// Unfortunatly, it is unsafe to use for production code, since it may unsoundly hide bugs.
     pub zero_init_vars: bool,
 }
