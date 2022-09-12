ce613fe6da4c559b5de0ad1cb3b9355a8661b5a4
Sacha-Élie Ayoun <sachaayoun@gmail.com>
2022-09-12 14:57:15 +0000

    add an uninitialized memory access fixme test

---
+++ tests/kani/UninitializedMemory/fixme_main.rs
@@ -0,0 +1,14 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// At the moment, Kani is unable to detect the usage of uninitialized memory
+// See https://github.com/model-checking/kani/issues/920
+
+#[kani::proof]
+fn main() {
+    let mut v: Vec<u8> = Vec::with_capacity(8);
+    unsafe {
+        v.set_len(3);
+    }
+    let _b = v[0]; //< reading uninitialized memory
+}
