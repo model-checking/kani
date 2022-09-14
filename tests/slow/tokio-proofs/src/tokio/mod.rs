a2ecd4b9de45404bb141e4bf98e6f9740761bf75
Fabian Zaiser <fzaiser@amazon.com>
2022-09-14 13:16:12 +0100

    Add async/await examples from tokio's test to a new "slow" test suite (#1332)
    * Make tokio tests compile with Kani
    
    * Add expected file
    
    * Lower unwind bound
    
    * Raise unwind bounds for some tests
    
    * Disable tests that hit bugs or take too long
    
    * Disable failing tests
    
    * Add a "slow" test suite
    
    * Add Github action to run slow tests every night
    
    * Fix tests and expected
    
    * Address review comments
    
    * Rerun CI
    
    Co-authored-by: Fabian Zaiser <fabian.zaiser@gmail.com>

---
+++ tests/slow/tokio-proofs/src/tokio/mod.rs
@@ -0,0 +1,22 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+mod io_chain;
+mod io_copy;
+mod io_lines;
+mod io_mem_stream;
+mod io_read;
+mod io_read_buf;
+mod io_read_exact;
+mod io_read_line;
+mod io_read_to_end;
+mod io_read_to_string;
+mod io_read_until;
+mod io_take;
+mod io_util_empty;
+mod io_write;
+mod io_write_all;
+mod io_write_all_buf;
+mod io_write_buf;
+mod io_write_int;
+mod support;
