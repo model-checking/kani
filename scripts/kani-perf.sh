5d15ace49561a016d87740dfae46ce574b08f6a8
Zyad Hassan <zyadh@amazon.com>
2022-09-19 18:00:29 -0700

    Add back period

--- scripts/kani-perf.sh
+++ scripts/kani-perf.sh
@@ -8,7 +8,7 @@ set -o nounset
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 KANI_DIR=$SCRIPT_DIR/..
 
-# Build Kani using release mode
+# Build Kani using release mode.
 cargo build --release
 
 PERF_DIR="${KANI_DIR}/tests/perf"
