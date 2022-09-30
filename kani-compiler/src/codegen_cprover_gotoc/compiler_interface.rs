03758554ba434d9f67a4108f505fa0ab21cadf6f
Daniel Schwartz-Narbonne <dsn@amazon.com>
2022-09-29 20:42:29 -0400

    PR Comments

--- kani-compiler/src/codegen_cprover_gotoc/compiler_interface.rs
+++ kani-compiler/src/codegen_cprover_gotoc/compiler_interface.rs
@@ -395,14 +395,21 @@ fn symbol_table_to_gotoc(tcx: &TyCtxt, file: &Path) {
     let output_filename = file.with_extension("symtab.out");
     let input_filename = file.with_extension("symtab.json");
 
-    let args =
-        vec![input_filename.into_os_string(), "--out".into(), output_filename.into_os_string()];
+    let args = vec![
+        input_filename.clone().into_os_string(),
+        "--out".into(),
+        output_filename.into_os_string(),
+    ];
     // TODO get symtab2gb path from self
     let mut cmd = Command::new("symtab2gb");
     cmd.args(args);
+    debug!("calling: `{:?} {:?}`", cmd.get_program(), cmd.get_args());
+
     if cmd.status().is_err() {
-        let err_msg =
-            format!("Failed symtab2gb on file {}.", file.with_extension("symtab.json").display());
+        let err_msg = format!(
+            "Failed to generate goto model:\n\tsymtab2gb failed on file {}.",
+            input_filename.display()
+        );
         tcx.sess.err(&err_msg);
         tcx.sess.abort_if_errors();
     }
