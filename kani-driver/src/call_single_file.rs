1263926b28cd3cf54e788177eae741e5506821c8
Celina G. Val <celinval@amazon.com>
2022-10-06 17:08:59 -0700

    Add support to --function for MIR Linker
    Implement --reachability=pub_fns

--- kani-driver/src/call_single_file.rs
+++ kani-driver/src/call_single_file.rs
@@ -45,7 +45,11 @@ impl KaniSession {
 
         let mut kani_args = self.kani_specific_flags();
         if self.args.mir_linker {
-            kani_args.push("--reachability=harnesses".into());
+            if self.args.function.is_some() {
+                kani_args.push("--reachability=pub_fns".into());
+            } else {
+                kani_args.push("--reachability=harnesses".into());
+            }
         } else {
             kani_args.push("--reachability=legacy".into());
         }
