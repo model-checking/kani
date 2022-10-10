e427a6ad4f128f8796c550d8907d436d564eaac2
Ted Kaminski <tedinski@amazon.com>
2022-10-10 22:48:30 +0000

    Fix undefined symbol errors when rustup defaults to nightly rust

--- src/lib.rs
+++ src/lib.rs
@@ -84,6 +84,9 @@ fn exec(bin: &str) -> Result<()> {
     // Add: kani, cbmc, viewer (pyroot), and our rust toolchain directly to our PATH
     let path = prepend_search_path(&[bin_kani, bin_pyroot, bin_toolchain], env::var_os("PATH"))?;
 
+    // Ensure our environment variables for linker search paths won't cause failures, before we execute:
+    fixup_dynamic_linking_environment();
+
     let mut cmd = Command::new(program);
     cmd.args(env::args_os().skip(1)).env("PYTHONPATH", pythonpath).env("PATH", path).arg0(bin);
 
@@ -101,5 +104,66 @@ fn prepend_search_path(paths: &[PathBuf], original: Option<OsString>) -> Result<
             let new_iter = paths.iter().cloned().chain(orig);
             Ok(env::join_paths(new_iter)?)
         }
+    }
+}
+
+/// `rustup` set dynamic linker paths, it's not fully clear why. `rustup run` exists, which may aid
+/// in running rust binaries that dynamically link to the rust standard library. This might be why.
+/// All toolchain binaries have `RUNPATH` set, so it's not needed by e.g. rustc. (Same for Kani)
+///
+/// However, this causes problems for us when the default rust toolchain is nightly. Then
+/// `LD_LIBRARY_PATH` is set to a nightly `lib` that may contain a different version of
+/// `librustc_driver-*.so`. This manifests in errors like:
+/// `kani-compiler: symbol lookup error: ... undefined symbol`
+///
+/// Consequently, let's strip anything that looks like a toolchain from our environment.
+fn fixup_dynamic_linking_environment() {
+    #[cfg(not(target_os = "macos"))]
+    pub const LOADER_PATH: &str = "LD_LIBRARY_PATH";
+    #[cfg(target_os = "macos")]
+    pub const LOADER_PATH: &str = "DYLD_FALLBACK_LIBRARY_PATH";
+
+    if let Some(paths) = env::var_os(LOADER_PATH) {
+        let new_val =
+            env::join_paths(env::split_paths(&paths).filter(unlike_toolchain_path)).unwrap();
+        // unwrap safety: we're just filtering, so it should always succeed
+        env::set_var(LOADER_PATH, new_val);
+    }
+}
+
+/// Determines if a path looks unlike a toolchain library path. These often looks like:
+/// `/home/user/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib`
+fn unlike_toolchain_path(path: &PathBuf) -> bool {
+    let mut components = path.iter().rev();
+
+    // effectively matching `*/toolchains/*/lib`
+    !(components.next() == Some(std::ffi::OsStr::new("lib"))
+        && components.next().is_some()
+        && components.next() == Some(std::ffi::OsStr::new("toolchains")))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_unlike_toolchain_path() {
+        fn trial(s: &str) -> bool {
+            unlike_toolchain_path(&PathBuf::from(s))
+        }
+        // filter these out:
+        assert!(!trial("/home/user/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib"));
+        assert!(!trial("/home/user/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/"));
+        assert!(!trial("/home/user/.rustup/toolchains/nightly/lib"));
+        assert!(!trial("/home/user/.rustup/toolchains/stable/lib"));
+        // minimally:
+        assert!(!trial("toolchains/nightly/lib"));
+        // keep these:
+        assert!(trial("/home/user/.rustup/toolchains"));
+        assert!(trial("/usr/lib"));
+        assert!(trial("/home/user/lib/toolchains"));
+        // don't error on these:
+        assert!(trial(""));
+        assert!(trial("/"));
     }
 }
