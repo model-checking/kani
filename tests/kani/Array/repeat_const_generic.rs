783006a83ef5cf74659c39a75863899817745db0
Ted Kaminski <tedinski@amazon.com>
2022-10-11 14:57:58 +0000

    comment test a bit more

--- tests/kani/Array/repeat_const_generic.rs
+++ tests/kani/Array/repeat_const_generic.rs
@@ -1,7 +1,8 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Ensure Kani can evaluate `[e; N]` when `N` is a const generic
+// Ensure Kani can compile `[e; N]` when `N` is a const generic.
+// Test from <https://github.com/model-checking/kani/issues/1728>
 
 struct Foo<const N: usize> {
     field: [u64; N],
@@ -9,6 +10,8 @@ struct Foo<const N: usize> {
 
 impl<const N: usize> Foo<N> {
     fn new() -> Self {
+        // Was a crash during codegen because N hadn't been "monomorphized"
+        // and so could not be evaluated to a compile-time constant.
         Self { field: [0; N] }
     }
 }
@@ -18,7 +21,8 @@ mod proofs {
     use super::*;
 
     #[kani::proof]
-    fn kani_crash() {
-        Foo::<32>::new();
+    fn hope_kani_does_not_crash() {
+        let x = Foo::<32>::new();
+        assert!(x.len() == 32);
     }
 }
