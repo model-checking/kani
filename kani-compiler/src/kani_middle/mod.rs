877abf6016f4cb9531a3f7816094b35ad18f4ba3
Celina G. Val <celinval@amazon.com>
2022-11-01 16:33:54 -0700

    Create kani_middle module to host backend agnostic code
      - For now I just moved the reachability code as is.

---
+++ kani-compiler/src/kani_middle/mod.rs
@@ -0,0 +1,5 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//! This module contains code that are backend agnostic. For example, MIR analysis
+//! and transformations.
+pub mod reachability;
