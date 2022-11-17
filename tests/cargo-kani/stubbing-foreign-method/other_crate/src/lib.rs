448d487593e0b6bbe34058c522bc58b514cf3189
Aaron Bembenek <aaronbem@amazon.com>
2022-11-17 11:17:00 -0500

    Re-organized tests.

--- tests/cargo-kani/stubbing-foreign-method/other_crate/src/lib.rs
+++ tests/cargo-kani/stubbing-foreign-method/other_crate/src/lib.rs
@@ -1,22 +1,6 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-pub fn pub_fn() -> u32 {
-    0
-}
-
-pub fn fn_delegating_to_priv_fn() -> u32 {
-    priv_fn()
-}
-
-fn priv_fn() -> u32 {
-    0
-}
-
-fn the_answer() -> u32 {
-    42
-}
-
 pub struct PubType {}
 
 impl PubType {
@@ -58,25 +42,5 @@ impl PrivType {
 
     fn the_answer(&self) -> u32 {
         42
-    }
-}
-
-pub mod pub_mod {
-    pub fn pub_fn() -> u32 {
-        0
-    }
-
-    pub fn fn_delegating_to_priv_fn() -> u32 {
-        priv_mod::pub_fn()
-    }
-
-    mod priv_mod {
-        pub fn pub_fn() -> u32 {
-            0
-        }
-
-        fn the_answer() -> u32 {
-            42
-        }
     }
 }
--- tests/kani/StubbingMethod/main.rs
+++ tests/kani/StubbingMethod/main.rs
@@ -3,33 +3,7 @@
 //
 // kani-flags: --harness main --enable-unstable --enable-stubbing
 //
-//! This tests stubbing for local functions and methods.
-
-fn local_fn() -> u32 {
-    0
-}
-
-fn the_answer() -> u32 {
-    42
-}
-
-mod local_mod {
-    pub fn pub_fn() -> u32 {
-        0
-    }
-
-    pub fn fn_delegating_to_priv_fn() -> u32 {
-        priv_fn()
-    }
-
-    fn priv_fn() -> u32 {
-        0
-    }
-
-    fn the_answer() -> u32 {
-        42
-    }
-}
+//! This tests stubbing for local methods.
 
 struct LocalType {}
 
@@ -58,18 +32,9 @@ impl LocalType {
 // TODO: Split up these assertions into separate harnesses, once stubbing is able to support that.
 // <https://github.com/model-checking/kani/issues/1861>
 #[kani::proof]
-#[kani::stub(local_fn, the_answer)]
-#[kani::stub(local_mod::pub_fn, the_answer)]
-#[kani::stub(local_mod::priv_fn, local_mod::the_answer)]
 #[kani::stub(LocalType::pub_fn, LocalType::the_answer)]
 #[kani::stub(LocalType::priv_fn, LocalType::the_answer)]
 fn main() {
-    // Test function stubbing
-    assert_eq!(local_fn(), 42);
-    assert_eq!(local_mod::pub_fn(), 42);
-    assert_eq!(local_mod::fn_delegating_to_priv_fn(), 42);
-
-    // Test method stubbing
     assert_eq!(LocalType::new().pub_fn(), 42);
     assert_eq!(LocalType::new().fn_delegating_to_priv_fn(), 42);
 }
