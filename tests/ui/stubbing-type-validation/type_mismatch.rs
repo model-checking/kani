07e614f52ce7b266a7bf3e9fffd60abefd526d5a
Aaron Bembenek <aaronbem@amazon.com>
2022-11-22 13:37:01 -0500

    Add more tests.

--- tests/ui/stubbing-type-validation/type_mismatch.rs
+++ tests/ui/stubbing-type-validation/type_mismatch.rs
@@ -18,8 +18,8 @@ fn g1(_x: bool, _y: i32, _z: &bool, _zz: bool) -> bool {
     true
 }
 
-fn g2(_x: bool, _y: u32, _z: &mut bool, _zz: bool) -> bool {
-    true
+fn g2(_x: bool, _y: u32, _z: &mut bool, _zz: bool) -> i32 {
+    42
 }
 
 fn h1<S>(_x: S) -> bool {
@@ -30,8 +30,21 @@ fn h2<T>(_x: T) -> bool {
     true
 }
 
+fn i1<X: Copy, Y: Copy>(x: &X, _y: &Y) -> X {
+    *x
+}
+fn i2<X: Copy, Y: Copy>(_x: &X, y: &Y) -> Y {
+    *y
+}
+
+fn j1<X, Y>(_x: &X, _y1: &Y, _y2: &Y) {}
+
+fn j2<X, Y>(_x1: &X, _x2: &X, _y: &Y) {}
+
 #[kani::proof]
 #[kani::stub(f1, f2)]
 #[kani::stub(g1, g2)]
 #[kani::stub(h1, h2)]
+#[kani::stub(i1, i2)]
+#[kani::stub(j1, j2)]
 fn harness() {}
