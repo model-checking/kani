054abc1a1b2ce714e5366909291af9a72436ee61
Celina G. Val <celinval@amazon.com>
2022-12-02 18:02:22 -0800

    Add test for conflict harness name
    - Harnesses with the same name in different crates can interfere with
    each other.

---
+++ tests/cargo-kani/ws-name-conflict/lib1/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Different harnesses with the same name. This one should pass. The simbling crate should fail.
+
+#[kani::proof]
+fn check_lib() {
+    assert!(1 == 1, "This should succeed");
+}
---
+++ tests/cargo-kani/ws-name-conflict/lib2/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Different harnesses with the same name. This one should fail. The simbling crate should pass.
+
+#[kani::proof]
+fn check_lib() {
+    assert_eq!(1 + 1, 1, "This should fail");
+}
