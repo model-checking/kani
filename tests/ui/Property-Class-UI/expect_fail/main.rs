369a06734b880cd6f48f43a2fdb8b41505b41549
Zyad Hassan <zyadh@amazon.com>
2022-12-20 16:11:53 -0800

    Delete kani::expect_fail and associated hook

--- tests/ui/Property-Class-UI/expect_fail/main.rs
+++
@@ -1,9 +0,0 @@
-// Copyright Kani Contributors
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-
-#[kani::proof]
-fn main() {
-    let i: i32 = kani::any();
-    kani::assume(i < 10);
-    kani::expect_fail(i > 20, "Blocked by assumption above.");
-}
