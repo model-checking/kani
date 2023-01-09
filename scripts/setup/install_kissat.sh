c4c8cd2b3dd8370f45fd098a187a9286776f5820
Zyad Hassan <zyadh@amazon.com>
2023-01-09 10:34:22 -0800

    Check expected version of kissat

--- scripts/setup/install_kissat.sh
+++ scripts/setup/install_kissat.sh
@@ -4,18 +4,28 @@
 
 set -eu
 
-# Kissat release 3.0.0
-FILE="rel-3.0.0.tar.gz"
+# Source kani-dependencies to get KISSAT_VERSION
+source kani-dependencies
+
+if [ -z "${KISSAT_VERSION:-}" ]; then
+  echo "$0: Error: KISSAT_VERSION is not specified"
+  exit 1
+fi
+
+
+# Kissat release
+FILE="rel-${KISSAT_VERSION}.tar.gz"
 URL="https://github.com/arminbiere/kissat/archive/refs/tags/$FILE"
 
 set -x
 
 wget -O "$FILE" "$URL"
 tar -xvzf $FILE
-cd kissat-rel-3.0.0
+DIR_NAME="kissat-rel-${KISSAT_VERSION}"
+cd $DIR_NAME
 ./configure && make kissat && sudo install build/kissat /usr/local/bin
 cd -
 
 # Clean up on success
 rm $FILE
-rm -rf kissat-rel-3.0.0
+rm -rf $DIR_NAME
