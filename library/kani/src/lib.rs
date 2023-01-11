d87134ebd1cf37365c175d52f31748c7df9da925
Daniel Schwartz-Narbonne <dsn@amazon.com>
2023-01-11 13:59:17 -0500

    rename to any_with

--- library/kani/src/lib.rs
+++ library/kani/src/lib.rs
@@ -109,7 +109,7 @@ pub fn any<T: Arbitrary>() -> T {
     T::any()
 }
 
-/// This creates an symbolic *valid* value of type `T`.
+/// This creates a symbolic *valid* value of type `T`.
 /// The value is constrained to be a value accepted by the predicate passed to the filter.
 /// You can assign the return value of this function to a variable that you want to make symbolic.
 /// The explanation field gives a mechanism to explain why the assumption is required for the proof.
@@ -120,7 +120,7 @@ pub fn any<T: Arbitrary>() -> T {
 /// under all possible `NonZeroU8` input values between 0 and 12.
 ///
 /// ```rust
-/// let inputA = kani::filter_any::<std::num::NonZeroU8>(|x| x < 12, "explanation");
+/// let inputA = kani::any_with::<std::num::NonZeroU8>(|x| *x < 12, "explanation");
 /// fn_under_verification(inputA);
 /// ```
 ///
@@ -128,7 +128,7 @@ pub fn any<T: Arbitrary>() -> T {
 /// trait. The Arbitrary trait is used to build a symbolic value that represents all possible
 /// valid values for type `T`.
 #[inline(always)]
-pub fn filter_any<T: Arbitrary, F: FnOnce(&T) -> bool>(f: F, _msg: &'static str) -> T {
+pub fn any_with<T: Arbitrary, F: FnOnce(&T) -> bool>(f: F, _msg: &'static str) -> T {
     let result = T::any();
     assume(f(&result));
     result
