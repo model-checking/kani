112a680cc92b05f7d1fb7459e3778f0b74886938
Celina G. Val <celinval@amazon.com>
2023-02-02 15:55:20 -0800

    Update loop_with_drop.rs

--- tests/kani/Loops/loop_with_drop.rs
+++ tests/kani/Loops/loop_with_drop.rs
@@ -1,8 +1,9 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-//! Ensure that Kani correctly unwind the loop with drop instructions which may .
+//! Ensure that Kani correctly unwinds the loop with drop instructions.
 //! This was related to https://github.com/model-checking/kani/issues/2164
+
 /// Dummy function with a for loop that only runs 2 iterations.
 fn bounded_loop<T: Default>(b: bool, other: T) -> T {
     let mut ret = other;
