a90b77b9784f75f453a126cad3a7934631865d98
Celina G. Val <celinval@amazon.com>
2023-02-02 14:21:01 -0800

    Work around loop identification issues with CBMC
    CBMC has a heuristic to identify loops that identifies any jump to a
    previously declared basic block as the back edge of a loop.
    
    For rust, this happens often due to drop elaboration. See
    https://rustc-dev-guide.rust-lang.org/mir/drop-elaboration.html#drop-elaboration-1
    for more details.
    
    To avoid that issue, we now codegen basic blocks in topological order.

---
+++ tests/kani/Loops/loop_with_drop.rs
@@ -0,0 +1,23 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+//! Ensure that Kani correctly unwind the loop with drop instructions which may .
+//! This was related to https://github.com/model-checking/kani/issues/2164
+/// Dummy function with a for loop that only runs 2 iterations.
+fn bounded_loop<T: Default>(b: bool, other: T) -> T {
+    let mut ret = other;
+    for i in 0..2 {
+        ret = match b {
+            true => T::default(),
+            false => ret,
+        };
+    }
+    return ret;
+}
+
+/// Harness that should succeed. We add a conservative loop bound.
+#[kani::proof]
+#[kani::unwind(3)]
+fn harness() {
+    let _ = bounded_loop(kani::any(), ());
+}
