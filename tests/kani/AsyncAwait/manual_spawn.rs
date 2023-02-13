2e38a6ab42e43baeeaca3f4f4a876abed33a8005
Fabian Zaiser <fzaiser@amazon.com>
2023-02-13 22:21:00 +0800

    Fix test

--- tests/kani/AsyncAwait/manual_spawn.rs
+++ tests/kani/AsyncAwait/manual_spawn.rs
@@ -297,16 +297,16 @@ fn deterministic_schedule() {
 fn nondeterministic_schedule() {
     let x = Arc::new(AtomicI64::new(0)); // Surprisingly, Arc verified faster than Rc
     let x2 = x.clone();
-    kani::spawnable_block_on(
+    spawnable_block_on(
         async move {
             let x3 = x2.clone();
-            kani::spawn(async move {
+            spawn(async move {
                 x3.fetch_add(1, Ordering::Relaxed);
             });
-            kani::yield_now().await;
+            yield_now().await;
             x2.fetch_add(1, Ordering::Relaxed);
         },
-        kani::NondetFairScheduling::new(2),
+        NondetFairScheduling::new(2),
     );
     assert_eq!(x.load(Ordering::Relaxed), 2);
 }
