8148ca47b8bbb62d94dd3376d9c67fbc6fcf42c1
Celina G. Val <celinval@amazon.com>
2023-02-16 10:39:40 -0800

    Fix stub name resolution infinite loop

---
+++ tests/kani/Stubbing/glob_path.rs
@@ -0,0 +1,31 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// kani-flags: --harness check_stub --enable-unstable --enable-stubbing
+//! Test that stub can solve glob cycles even when the path expands the cycle.
+
+pub mod mod_a {
+    pub use crate::mod_b::*;
+    pub use crate::*;
+
+    /// This method always fail.
+    pub fn method_a() {
+        mod_a::mod_b::mod_a::mod_b::noop();
+        panic!();
+    }
+}
+
+pub mod mod_b {
+    pub use crate::mod_a::*;
+    pub use crate::*;
+
+    /// This harness replace `method_a` which always fail by `method_b` that should always succeed.
+    #[kani::proof]
+    #[kani::stub(mod_a::mod_b::mod_a::method_a, mod_b::noop)]
+    pub fn check_stub() {
+        method_a();
+    }
+
+    /// This method always succeed.
+    pub fn noop() {}
+}
