cf264470329c8ff40935334f33fd9794c0dcace6
Celina G. Val <celinval@amazon.com>
2023-02-21 11:29:07 -0800

    Move attribute handling to `kani_middle` and polish warnings (#2211)
    As part of removing one harness restriction from stubbing, we will need to run the rust compiler driver multiple times. However, we want to only produce one metadata file with information about all the compiled artifacts.
    
    Thus, I'm planning to move the harness metadata generation to the KaniCompiler as opposed to the GotoCCtxt. As a first step, I'm moving the attributes handling out of the GotoCCtxt.
    
    The only behavior change in this PR is related to some of the warnings we print. I tried to standardize it a bit (following our new guidelines) and I moved the stub warning to be part of the driver.

--- kani-driver/src/metadata.rs
+++ kani-driver/src/metadata.rs
@@ -6,7 +6,8 @@ use std::path::Path;
 use tracing::debug;
 
 use kani_metadata::{
-    HarnessMetadata, InternedString, KaniMetadata, TraitDefinedMethod, VtableCtxResults,
+    HarnessAttributes, HarnessMetadata, InternedString, KaniMetadata, TraitDefinedMethod,
+    VtableCtxResults,
 };
 use std::collections::{BTreeSet, HashMap};
 use std::fs::File;
@@ -156,8 +157,7 @@ pub fn mock_proof_harness(
         original_file: "<unknown>".into(),
         original_start_line: 0,
         original_end_line: 0,
-        solver: None,
-        unwind_value,
+        attributes: HarnessAttributes { unwind_value, proof: true, ..Default::default() },
         goto_file: None,
     }
 }
