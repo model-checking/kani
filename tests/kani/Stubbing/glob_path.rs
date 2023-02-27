063a196cf901a118ace9507a469fe06ad16d2dd1
Celina G. Val <celinval@amazon.com>
2023-02-27 13:06:49 -0800

    Apply suggestions from code review
    Co-authored-by: Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>

--- tests/kani/Stubbing/glob_path.rs
+++ tests/kani/Stubbing/glob_path.rs
@@ -8,7 +8,7 @@ pub mod mod_a {
     pub use crate::mod_b::*;
     pub use crate::*;
 
-    /// This method always fail.
+    /// This method always fails.
     pub fn method_a() {
         mod_a::mod_b::mod_a::mod_b::noop();
         panic!();
@@ -19,13 +19,13 @@ pub mod mod_b {
     pub use crate::mod_a::*;
     pub use crate::*;
 
-    /// This harness replace `method_a` which always fail by `method_b` that should always succeed.
+    /// This harness replaces `method_a` (always fails), by `method_b` (always succeeds).
     #[kani::proof]
     #[kani::stub(mod_a::mod_b::mod_a::method_a, mod_b::noop)]
     pub fn check_stub() {
         method_a();
     }
 
-    /// This method always succeed.
+    /// This method always succeeds.
     pub fn noop() {}
 }
