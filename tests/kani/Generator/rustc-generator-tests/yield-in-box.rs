aee643bf77dc7752e804e79a938d6f9ba0856f22
Celina G. Val <celinval@amazon.com>
2023-04-20 10:18:52 +0200

    Update yield-in-box.rs
    Fix parenthesis

--- tests/kani/Generator/rustc-generator-tests/yield-in-box.rs
+++ tests/kani/Generator/rustc-generator-tests/yield-in-box.rs
@@ -22,14 +22,14 @@ fn main() {
         //~ WARN unused generator that must be used
         let y = 2u32;
         {
-            let _t = Box::new(&x, yield 0, &y);
+            let _t = Box::new((&x, yield 0, &y));
         }
-        match Box::new(&x, yield 0, &y) {
+        match Box::new((&x, yield 0, &y)) {
             _t => {}
         }
     };
 
     let mut g = |_| Box::new(yield);
     assert_eq!(Pin::new(&mut g).resume(1), GeneratorState::Yielded(()));
-    assert_eq!(Pin::new(&mut g).resume(2), GeneratorState::Complete(box 2));
+    assert_eq!(Pin::new(&mut g).resume(2), GeneratorState::Complete(Box::new(2)));
 }
