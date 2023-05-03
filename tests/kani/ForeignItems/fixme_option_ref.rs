826273841f02462b4f9903e60ba460def7006e9a
Celina G. Val <celinval@amazon.com>
2023-05-03 14:53:15 -0700

    Update tests/kani/ForeignItems/fixme_option_ref.rs
    Co-authored-by: Adrian Palacios <73246657+adpaco-aws@users.noreply.github.com>

--- tests/kani/ForeignItems/fixme_option_ref.rs
+++ tests/kani/ForeignItems/fixme_option_ref.rs
@@ -6,12 +6,12 @@
 //! See <https://github.com/model-checking/kani/issues/2152> for more details.
 
 extern "C" {
-    // In rust, you say nullable pointer by using option of reference.
-    // Rust guarantees that this has the bitwise represntation
-    // Some(&x) => &x;
-    // None => NULL;
+    // In Rust, you say nullable pointer by using option of reference.
+    // Rust guarantees that this has the bitwise representation:
+    //  - Some(&x) => &x;
+    //  - None => NULL;
     // FIXME: we need to notice when this happens and do a bitcast, or C is unhappy
-    // https://github.com/model-checking/kani/issues/3
+    // <https://github.com/model-checking/kani/issues/3>
     fn takes_ptr_option(p: Option<&u32>) -> u32;
 }
 
