6e91d1386f8ade781e7e5ffaf23817219133f92f
Celina G. Val <celinval@amazon.com>
2023-05-05 14:09:48 -0700

    Limit FFI calls by default to explicitly supported ones (#2428)
    C-FFI is fairly unstable right now and the lack of error handling causes a poor experience when things go wrong.
    
    In this PR, we limit its usage to only support CBMC built-in functions that are explicitly included in Kani and alloc functions from kani_lib.c. User's should still be enable to go back to previous behavior by enabling unstable C-FFI support
    
    Co-authored-by: Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>

--- tests/kani/FunctionCall/Variadic/fixme_main.rs
+++ tests/kani/FunctionCall/Variadic/fixme_main.rs
@@ -17,8 +17,10 @@ pub unsafe extern "C" fn my_add(num: c_long, mut args: ...) -> c_long {
 #[kani::proof]
 fn main() {
     let arg0: c_long = 2;
-    let arg1: c_long = 3;
     let arg1: c_long = 4;
     let x = unsafe { my_add(0, arg0, arg1) };
-    assert!(x == 7);
+    assert!(x == 0);
+
+    let x = unsafe { my_add(2, arg0, arg1) };
+    assert!(x == 6);
 }
