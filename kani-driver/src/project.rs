model-checking/kani

commit 2a09d79db02ae9e9f2d7611bf2a2a21327ffe13d

Perform reachability analysis on a per-harness basis (#2439)
Kani compiler used to generate one `goto-program` for all harnesses in one crate. In some cases, this actually had a negative impact on the harness verification time. This was first reported in #1659

The main changes were done in the compiler's module `compiler_interface` and the module `project` from the driver. The compiler will now gather all the harnesses beforehand and it will perform reachability + codegen steps for each harness. All files related to a harness `goto-program` will follow the naming convention bellow:
```
<BASE_NAME>_<MANGLED_NAME>.<EXTENSION>
```
This applies to symtab / goto / type_map / restriction files.

The metadata file is still generated once per target crate, and its name is still the same (`<BASE_NAME>.kani-metadata.json`).

On the driver side, the way we process the artifacts have changed. The driver will now read the metadata for each crate, and collect all artifacts based on the symtab goto file that is recorded in the metadata of each harness.

These changes do not apply for `--function`. We still keep all artifacts based on the crate's `<BASE_NAME>` and we have a separate logic to handle that. Fixing this is captured by #2129.

kani-driver/src/project.rs
@@ -19,11 +19,10 @@
 use crate::metadata::{from_json, merge_kani_metadata, mock_proof_harness};
 use crate::session::KaniSession;
 use crate::util::{crate_name, guess_rlib_name};
-use anyhow::Result;
+use anyhow::{Context, Result};
 use kani_metadata::{
     artifact::convert_type, ArtifactType, ArtifactType::*, HarnessMetadata, KaniMetadata,
 };
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufWriter;
 use std::ops::Deref;
@@ -51,7 +50,7 @@ pub struct Project {
     /// A flag that indicated whether all artifacts have been merged or not.
     ///
     /// This allow us to provide a consistent behavior for `--function`.
-    /// For these this option, we still merge all the artifacts together, so the
+    /// For this option, we still merge all the artifacts together, so the
     /// `merged_artifacts` flag will be set to `true`.
     /// When this flag is `true`, there should only be up to one artifact of any given type.
     /// When this flag is `false`, there may be multiple artifacts for any given type. However,
@@ -100,6 +99,56 @@ impl Project {
                 && expected_path.as_ref().map_or(true, |goto_file| *goto_file == artifact.path)
         })
     }
+
+    /// Try to build a new project from the build result metadata.
+    ///
+    /// This method will parse the metadata in order to gather all artifacts generated by the
+    /// compiler.
+    fn try_new(
+        session: &KaniSession,
+        outdir: PathBuf,
+        metadata: Vec<KaniMetadata>,
+        cargo_metadata: Option<cargo_metadata::Metadata>,
+        failed_targets: Option<Vec<String>>,
+    ) -> Result<Self> {
+        // For each harness (test or proof) from each metadata, read the path for the goto
+        // SymTabGoto file. Use that path to find all the other artifacts.
+        let mut artifacts = vec![];
+        for crate_metadata in &metadata {
+            for harness_metadata in
+                crate_metadata.test_harnesses.iter().chain(crate_metadata.proof_harnesses.iter())
+            {
+                let symtab_out = Artifact::try_new(
+                    harness_metadata.goto_file.as_ref().expect("Expected a model file"),
+                    SymTabGoto,
+                )?;
+                let goto_path = convert_type(&symtab_out.path, symtab_out.typ, Goto);
+
+                // Link
+                session.link_goto_binary(&[symtab_out.to_path_buf()], &goto_path)?;
+                let goto = Artifact::try_new(&goto_path, Goto)?;
+
+                // All other harness artifacts that may have been generated as part of the build.
+                artifacts.extend([TypeMap, VTableRestriction, PrettyNameMap].iter().filter_map(
+                    |typ| {
+                        let artifact = Artifact::try_from(&symtab_out, *typ).ok()?;
+                        Some(artifact)
+                    },
+                ));
+                artifacts.push(symtab_out);
+                artifacts.push(goto);
+            }
+        }
+
+        Ok(Project {
+            outdir,
+            metadata,
+            artifacts,
+            merged_artifacts: false,
+            cargo_metadata,
+            failed_targets,
+        })
+    }
 }
 
 /// Information about a build artifact.
@@ -127,19 +176,26 @@ impl Deref for Artifact {
 impl Artifact {
     /// Create a new artifact if the given path exists.
     pub fn try_new(path: &Path, typ: ArtifactType) -> Result<Self> {
-        Ok(Artifact { path: path.canonicalize()?, typ })
+        Ok(Artifact {
+            path: path.canonicalize().context(format!("Failed to process {}", path.display()))?,
+            typ,
+        })
     }
 
     /// Check if this artifact has the given type.
     pub fn has_type(&self, typ: ArtifactType) -> bool {
         self.typ == typ
     }
-}
 
-/// Generate the expected path to a cargo artifact and return it if the artifact actually exists.
-fn cargo_artifact(metadata: &Path, typ: ArtifactType) -> Option<Artifact> {
-    let path = convert_type(metadata, Metadata, typ);
-    if path.exists() { Artifact::try_new(&path, typ).ok() } else { None }
+    /// Try to derive an artifact based on a different artifact of a different type.
+    /// For example:
+    /// ```no_run
+    /// let artifact = Artifact::try_new(&"/tmp/file.kani_metadata.json", Metadata).unwrap();
+    /// let goto = Artifact::try_from(artifact, Goto); // Will try to create "/tmp/file.goto"
+    /// ```
+    pub fn try_from(artifact: &Artifact, typ: ArtifactType) -> Result<Self> {
+        Self::try_new(&convert_type(&artifact.path, artifact.typ, typ), typ)
+    }
 }
 
 /// Store the KaniMetadata into a file.
@@ -154,32 +210,37 @@ fn dump_metadata(metadata: &KaniMetadata, path: &Path) {
 /// be collected from the project.
 pub fn cargo_project(session: &KaniSession, keep_going: bool) -> Result<Project> {
     let outputs = session.cargo_build(keep_going)?;
-    let mut artifacts = vec![];
     let outdir = outputs.outdir.canonicalize()?;
     if session.args.function.is_some() {
+        let mut artifacts = vec![];
         // For the `--function` support, we still use a glob to link everything.
         // Yes, this is broken, but it has been broken for quite some time. :(
         // Merge goto files.
+        // https://github.com/model-checking/kani/issues/2129
         let joined_name = "cbmc-linked";
         let base_name = outdir.join(joined_name);
         let goto = base_name.with_extension(Goto);
         let all_gotos = outputs
             .metadata
             .iter()
-            .map(|artifact| {
-                convert_type(&artifact, ArtifactType::Metadata, ArtifactType::SymTabGoto)
-            })
+            .map(|artifact| convert_type(&artifact, Metadata, SymTabGoto))
             .collect::<Vec<_>>();
         session.link_goto_binary(&all_gotos, &goto)?;
-        artifacts.push(Artifact::try_new(&goto, Goto)?);
+        let goto_artifact = Artifact::try_new(&goto, Goto)?;
 
         // Merge metadata files.
         let per_crate: Vec<_> =
             outputs.metadata.iter().filter_map(|f| from_json::<KaniMetadata>(f).ok()).collect();
         let merged_metadata = merge_kani_metadata(per_crate);
-        let metadata = metadata_with_function(session, joined_name, merged_metadata);
+        let metadata = metadata_with_function(
+            session,
+            joined_name,
+            merged_metadata,
+            goto_artifact.with_extension(SymTabGoto),
+        );
         let metadata_file = base_name.with_extension(Metadata);
         dump_metadata(&metadata, &metadata_file);
+        artifacts.push(goto_artifact);
         artifacts.push(Artifact::try_new(&metadata_file, Metadata)?);
 
         Ok(Project {
@@ -191,33 +252,19 @@ pub fn cargo_project(session: &KaniSession, keep_going: bool) -> Result<Project>
             failed_targets: outputs.failed_targets,
         })
     } else {
-        // For the MIR Linker we know there is only one artifact per verification target. Use
-        // that in our favor.
-        let mut metadata = vec![];
-        for meta_file in outputs.metadata {
-            // Link the artifact.
-            let canonical_metafile = meta_file.canonicalize()?;
-            let symtab_out = convert_type(&canonical_metafile, Metadata, SymTabGoto);
-            let goto = convert_type(&canonical_metafile, Metadata, Goto);
-            session.link_goto_binary(&[symtab_out], &goto)?;
-
-            // Store project information.
-            let crate_metadata: KaniMetadata = from_json(&meta_file)?;
-            let crate_name = &crate_metadata.crate_name;
-            artifacts.extend(
-                BUILD_ARTIFACTS.iter().filter_map(|typ| cargo_artifact(&canonical_metafile, *typ)),
-            );
-            debug!(?crate_name, ?crate_metadata, "cargo_project");
-            metadata.push(crate_metadata);
-        }
-        Ok(Project {
+        // For the MIR Linker we know there is only one metadata per crate. Use that in our favor.
+        let metadata = outputs
+            .metadata
+            .iter()
+            .map(|md_file| from_json(md_file))
+            .collect::<Result<Vec<_>>>()?;
+        Project::try_new(
+            session,
             outdir,
-            artifacts,
             metadata,
-            merged_artifacts: false,
-            cargo_metadata: Some(outputs.cargo_metadata),
-            failed_targets: outputs.failed_targets,
-        })
+            Some(outputs.cargo_metadata),
+            outputs.failed_targets,
+        )
     }
 }
 
@@ -230,8 +277,8 @@ pub fn standalone_project(input: &Path, session: &KaniSession) -> Result<Project
 struct StandaloneProjectBuilder<'a> {
     /// The directory where all outputs should be directed to.
     outdir: PathBuf,
-    /// The collection of artifacts that may be generated.
-    artifacts: HashMap<ArtifactType, Artifact>,
+    /// The metadata file for the target crate.
+    metadata: Artifact,
     /// The input file.
     input: PathBuf,
     /// The crate name.
@@ -239,10 +286,6 @@ struct StandaloneProjectBuilder<'a> {
     /// The Kani session.
     session: &'a KaniSession,
 }
-
-/// All the type of artifacts that may be generated as part of the build.
-const BUILD_ARTIFACTS: [ArtifactType; 7] =
-    [Metadata, Goto, SymTab, SymTabGoto, TypeMap, VTableRestriction, PrettyNameMap];
 
 impl<'a> StandaloneProjectBuilder<'a> {
     /// Create a `StandaloneProjectBuilder` from the given input and session.
@@ -256,11 +299,10 @@ impl<'a> StandaloneProjectBuilder<'a> {
             input.canonicalize().unwrap().parent().unwrap().to_path_buf()
         };
         let crate_name = crate_name(&input);
-        let artifacts =
-            BUILD_ARTIFACTS.map(|typ| (typ, standalone_artifact(&outdir, &crate_name, typ)));
+        let metadata = standalone_artifact(&outdir, &crate_name, Metadata);
         Ok(StandaloneProjectBuilder {
             outdir,
-            artifacts: HashMap::from(artifacts),
+            metadata,
             input: input.to_path_buf(),
             crate_name,
             session,
@@ -271,50 +313,30 @@ impl<'a> StandaloneProjectBuilder<'a> {
     fn build(self) -> Result<Project> {
         // Register artifacts that may be generated by the compiler / linker for future deletion.
         let rlib_path = guess_rlib_name(&self.outdir.join(self.input.file_name().unwrap()));
-        self.session.record_temporary_files(&[&rlib_path]);
-        self.session.record_temporary_files(&self.artifacts.values().collect::<Vec<_>>());
+        self.session.record_temporary_file(&rlib_path);
+        self.session.record_temporary_file(&self.metadata.path);
 
         // Build and link the artifacts.
         debug!(krate=?self.crate_name, input=?self.input, ?rlib_path, "build compile");
         self.session.compile_single_rust_file(&self.input, &self.crate_name, &self.outdir)?;
-        let symtab_out = self.artifact(SymTabGoto);
-        let goto = self.artifact(Goto);
 
-        if symtab_out.exists() {
-            debug!(?symtab_out, "build link");
-            self.session.link_goto_binary(&[symtab_out.to_path_buf()], goto)?;
-        }
-
-        // Create the project with the artifacts built by the compiler.
-        let metadata_path = self.artifact(Metadata);
-        let metadata = if metadata_path.exists() {
-            metadata_with_function(self.session, &self.crate_name, from_json(metadata_path)?)
+        let metadata = if let Ok(goto_model) = Artifact::try_from(&self.metadata, SymTabGoto) {
+            metadata_with_function(
+                self.session,
+                &self.crate_name,
+                from_json(&self.metadata)?,
+                goto_model.path,
+            )
         } else {
-            // TODO: The compiler should still produce a metadata file even when no harness exists.
-            KaniMetadata {
-                crate_name: self.crate_name,
-                proof_harnesses: vec![],
-                unsupported_features: vec![],
-                test_harnesses: vec![],
-            }
+            from_json(&self.metadata)?
         };
 
-        Ok(Project {
-            outdir: self.outdir,
-            metadata: vec![metadata],
-            artifacts: self
-                .artifacts
-                .into_values()
-                .filter(|artifact| artifact.path.exists())
-                .collect(),
-            merged_artifacts: false,
-            cargo_metadata: None,
-            failed_targets: None,
-        })
-    }
-
-    fn artifact(&self, typ: ArtifactType) -> &Path {
-        &self.artifacts.get(&typ).unwrap().path
+        // Create the project with the artifacts built by the compiler.
+        let result = Project::try_new(self.session, self.outdir, vec![metadata], None, None);
+        if let Ok(project) = &result {
+            self.session.record_temporary_files(&project.artifacts);
+        }
+        result
     }
 }
 
@@ -324,10 +346,16 @@ fn metadata_with_function(
     session: &KaniSession,
     crate_name: &str,
     mut metadata: KaniMetadata,
+    model_file: PathBuf,
 ) -> KaniMetadata {
     if let Some(name) = &session.args.function {
         // --function is untranslated, create a mock harness
-        metadata.proof_harnesses.push(mock_proof_harness(name, None, Some(crate_name)));
+        metadata.proof_harnesses.push(mock_proof_harness(
+            name,
+            None,
+            Some(crate_name),
+            Some(model_file),
+        ));
     }
     metadata
 }

cprover_bindings/src/irep/goto_binary_serde.rs
@@ -9,7 +9,7 @@ use std::fs::File;
 use std::hash::Hash;
 use std::io::{self, BufReader};
 use std::io::{BufWriter, Bytes, Error, ErrorKind, Read, Write};
-use std::path::PathBuf;
+use std::path::Path;
 
 /// Writes a symbol table to a file in goto binary format in version 5.
 ///
@@ -18,7 +18,7 @@ use std::path::PathBuf;
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::SymbolTable) {
+pub fn write_goto_binary_file(filename: &Path, source: &crate::goto_program::SymbolTable) {
     let out_file = File::create(filename).unwrap();
     let mut writer = BufWriter::new(out_file);
     let mut serializer = GotoBinarySerializer::new(&mut writer);
@@ -33,7 +33,7 @@ pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn read_goto_binary_file(filename: &PathBuf) -> io::Result<()> {
+pub fn read_goto_binary_file(filename: &Path) -> io::Result<()> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let mut deserializer = GotoBinaryDeserializer::new(reader);

kani-compiler/src/codegen_cprover_gotoc/codegen/function.rs
@@ -4,10 +4,8 @@
 //! This file contains functions related to codegenning MIR functions into gotoc
 
 use crate::codegen_cprover_gotoc::GotocCtx;
-use crate::kani_middle::attributes::{extract_harness_attributes, is_test_harness_closure};
 use cbmc::goto_program::{Expr, Stmt, Symbol};
 use cbmc::InternString;
-use kani_metadata::{HarnessAttributes, HarnessMetadata};
 use rustc_middle::mir::traversal::reverse_postorder;
 use rustc_middle::mir::{Body, HasLocalDecls, Local};
 use rustc_middle::ty::{self, Instance};
@@ -89,9 +87,6 @@ impl<'tcx> GotocCtx<'tcx> {
             let stmts = self.current_fn_mut().extract_block();
             let body = Stmt::block(stmts, loc);
             self.symbol_table.update_fn_declaration_with_definition(&name, body);
-
-            self.record_kani_attributes();
-            self.record_test_harness_metadata();
         }
         self.reset_current_fn();
     }
@@ -243,51 +238,5 @@ impl<'tcx> GotocCtx<'tcx> {
             )
         });
         self.reset_current_fn();
-    }
-
-    /// We record test harness information in kani-metadata, just like we record
-    /// proof harness information. This is used to support e.g. cargo-kani assess.
-    ///
-    /// Note that we do not actually spot the function that was annotated by `#[test]`
-    /// but instead the closure that gets put into the "test description" that macro
-    /// expands into. (See comment below) This ends up being preferrable, actually,
-    /// as it add asserts for tests that return `Result` types.
-    fn record_test_harness_metadata(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        if is_test_harness_closure(self.tcx, def_id) {
-            self.test_harnesses.push(self.generate_metadata(None))
-        }
-    }
-
-    /// This updates the goto context with any information that should be accumulated from a function's
-    /// attributes.
-    ///
-    /// Handle all attributes i.e. `#[kani::x]` (which kani_macros translates to `#[kanitool::x]` for us to handle here)
-    fn record_kani_attributes(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        let attributes = extract_harness_attributes(self.tcx, def_id);
-        if attributes.is_some() {
-            self.proof_harnesses.push(self.generate_metadata(attributes));
-        }
-    }
-
-    /// Create the default proof harness for the current function
-    fn generate_metadata(&self, attributes: Option<HarnessAttributes>) -> HarnessMetadata {
-        let current_fn = self.current_fn();
-        let pretty_name = current_fn.readable_name().to_owned();
-        let mangled_name = current_fn.name();
-        let loc = self.codegen_span(&current_fn.mir().span);
-
-        HarnessMetadata {
-            pretty_name,
-            mangled_name,
-            crate_name: current_fn.krate(),
-            original_file: loc.filename().unwrap(),
-            original_start_line: loc.start_line().unwrap() as usize,
-            original_end_line: loc.end_line().unwrap() as usize,
-            attributes: attributes.unwrap_or_default(),
-            // We record the actual path after codegen before we dump the metadata into a file.
-            goto_file: None,
-        }
     }
 }

kani-compiler/src/codegen_cprover_gotoc/codegen/span.rs
@@ -3,32 +3,21 @@
 
 //! MIR Span related functions
 
-use crate::codegen_cprover_gotoc::GotocCtx;
+use crate::{codegen_cprover_gotoc::GotocCtx, kani_middle::SourceLocation};
 use cbmc::goto_program::Location;
 use rustc_middle::mir::{Local, VarDebugInfo, VarDebugInfoContents};
 use rustc_span::Span;
 
 impl<'tcx> GotocCtx<'tcx> {
     pub fn codegen_span(&self, sp: &Span) -> Location {
-        let smap = self.tcx.sess.source_map();
-        let lo = smap.lookup_char_pos(sp.lo());
-        let start_line = lo.line;
-        let start_col = 1 + lo.col_display;
-        let hi = smap.lookup_char_pos(sp.hi());
-        let end_line = hi.line;
-        let end_col = 1 + hi.col_display;
-        let filename0 = lo.file.name.prefer_local().to_string_lossy().to_string();
-        let filename1 = match std::fs::canonicalize(filename0.clone()) {
-            Ok(pathbuf) => pathbuf.to_str().unwrap().to_string(),
-            Err(_) => filename0,
-        };
+        let loc = SourceLocation::new(self.tcx, sp);
         Location::new(
-            filename1,
+            loc.filename,
             self.current_fn.as_ref().map(|x| x.readable_name().to_string()),
-            start_line,
-            Some(start_col),
-            end_line,
-            Some(end_col),
+            loc.start_line,
+            Some(loc.start_col),
+            loc.end_line,
+            Some(loc.end_col),
         )
     }
 

commit 3d05d3f5991b30ca0baffe437541234e19c4bbed

Fix symtab json file removal and reduce regression scope (#2447)
This fixes a regression introduced in #2439 when write symtab json is enabled. We still need to take that into consideration and remove them if needed.

This change also simplifies the write symtab json regression to avoid the out of disk space issue we've been seeing since #2439.

.github/workflows/kani.yml
@@ -45,12 +45,14 @@ jobs:
           os: ubuntu-20.04
 
       - name: Build Kani
-        run: cargo build-dev
+        run: cargo build-dev -- --features write_json_symtab
 
-      - name: Execute Kani regression
-        env:
-          KANI_ENABLE_WRITE_JSON_SYMTAB: 1
-        run: ./scripts/kani-regression.sh
+      - name: Run tests
+        run: |
+          cargo run -p compiletest --quiet -- --suite kani --mode kani --quiet --no-fail-fast
+          cargo run -p compiletest --quiet -- --suite expected --mode expected --quiet --no-fail-fast
+          cargo run -p compiletest --quiet -- --suite cargo-kani --mode cargo-kani --quiet --no-fail-fast
+
 
   benchcomp-tests:
     runs-on: ubuntu-20.04

kani-driver/src/project.rs
@@ -129,12 +129,12 @@ impl Project {
                 let goto = Artifact::try_new(&goto_path, Goto)?;
 
                 // All other harness artifacts that may have been generated as part of the build.
-                artifacts.extend([TypeMap, VTableRestriction, PrettyNameMap].iter().filter_map(
-                    |typ| {
+                artifacts.extend(
+                    [SymTab, TypeMap, VTableRestriction, PrettyNameMap].iter().filter_map(|typ| {
                         let artifact = Artifact::try_from(&symtab_out, *typ).ok()?;
                         Some(artifact)
-                    },
-                ));
+                    }),
+                );
                 artifacts.push(symtab_out);
                 artifacts.push(goto);
             }

scripts/kani-regression.sh
@@ -27,11 +27,7 @@ check_kissat_version.sh
 ${SCRIPT_DIR}/kani-fmt.sh --check
 
 # Build all packages in the workspace
-if [[ "" != "${KANI_ENABLE_WRITE_JSON_SYMTAB-}" ]]; then
-  cargo build-dev -- --features write_json_symtab
-else
-  cargo build-dev
-fi
+cargo build-dev
 
 # Unit tests
 cargo test -p cprover_bindings
@@ -69,11 +65,8 @@ for testp in "${TESTS[@]}"; do
       --quiet --no-fail-fast
 done
 
-# Don't run std regression if using JSON symtab to avoid OOM issues.
-if [[ -z "${KANI_ENABLE_WRITE_JSON_SYMTAB-}" ]]; then
-    # Check codegen for the standard library
-    time "$SCRIPT_DIR"/std-lib-regression.sh
-fi
+# Check codegen for the standard library
+time "$SCRIPT_DIR"/std-lib-regression.sh
 
 # We rarely benefit from re-using build artifacts in the firecracker test,
 # and we often end up with incompatible leftover artifacts: