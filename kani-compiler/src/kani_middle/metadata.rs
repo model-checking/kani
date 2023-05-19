model-checking/kani

commit 2a09d79db02ae9e9f2d7611bf2a2a21327ffe13d

Perform reachability analysis on a per-harness basis (#2439)
Kani compiler used to generate one `goto-program` for all harnesses in one crate. In some cases, this actually had a negative impact on the harness verification time. This was first reported in #1659

The main changes were done in the compiler's module `compiler_interface` and the module `project` from the driver. The compiler will now gather all the harnesses beforehand and it will perform reachability + codegen steps for each harness. All files related to a harness `goto-program` will follow the naming convention bellow:
```
<BASE_NAME>_<MANGLED_NAME>.<EXTENSION>
```
This applies to symtab / goto / type_map / restriction files.

The metadata file is still generated once per target crate, and its name is still the same (`<BASE_NAME>.kani-metadata.json`).

On the driver side, the way we process the artifacts have changed. The driver will now read the metadata for each crate, and collect all artifacts based on the symtab goto file that is recorded in the metadata of each harness.

These changes do not apply for `--function`. We still keep all artifacts based on the crate's `<BASE_NAME>` and we have a separate logic to handle that. Fixing this is captured by #2129.

kani-compiler/src/kani_middle/metadata.rs
@@ -1,0 +1,69 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//! This module handles Kani metadata generation. For example, generating HarnessMetadata for a
+//! given function.
+
+use std::path::Path;
+
+use crate::kani_middle::attributes::test_harness_name;
+use kani_metadata::{ArtifactType, HarnessAttributes, HarnessMetadata};
+use rustc_hir::def_id::DefId;
+use rustc_middle::ty::{Instance, TyCtxt};
+
+use super::{attributes::extract_harness_attributes, SourceLocation};
+
+/// Create the harness metadata for a proof harness for a given function.
+pub fn gen_proof_metadata(tcx: TyCtxt, def_id: DefId, base_name: &Path) -> HarnessMetadata {
+    let attributes = extract_harness_attributes(tcx, def_id);
+    let pretty_name = tcx.def_path_str(def_id);
+    // Main function a special case in order to support `--function main`
+    // TODO: Get rid of this: https://github.com/model-checking/kani/issues/2129
+    let mangled_name = if pretty_name == "main" {
+        pretty_name.clone()
+    } else {
+        tcx.symbol_name(Instance::mono(tcx, def_id)).to_string()
+    };
+
+    let loc = SourceLocation::def_id_loc(tcx, def_id);
+    let file_stem = format!("{}_{mangled_name}", base_name.file_stem().unwrap().to_str().unwrap());
+    let model_file = base_name.with_file_name(file_stem).with_extension(ArtifactType::SymTabGoto);
+
+    HarnessMetadata {
+        pretty_name,
+        mangled_name,
+        crate_name: tcx.crate_name(def_id.krate).to_string(),
+        original_file: loc.filename,
+        original_start_line: loc.start_line,
+        original_end_line: loc.end_line,
+        attributes: attributes.unwrap_or_default(),
+        // TODO: This no longer needs to be an Option.
+        goto_file: Some(model_file),
+    }
+}
+
+/// Create the harness metadata for a test description.
+#[allow(dead_code)]
+pub fn gen_test_metadata<'tcx>(
+    tcx: TyCtxt<'tcx>,
+    test_desc: DefId,
+    test_fn: Instance<'tcx>,
+    base_name: &Path,
+) -> HarnessMetadata {
+    let pretty_name = test_harness_name(tcx, test_desc);
+    let mangled_name = tcx.symbol_name(test_fn).to_string();
+    let loc = SourceLocation::def_id_loc(tcx, test_desc);
+    let file_stem = format!("{}_{mangled_name}", base_name.file_stem().unwrap().to_str().unwrap());
+    let model_file = base_name.with_file_name(file_stem).with_extension(ArtifactType::SymTabGoto);
+
+    HarnessMetadata {
+        pretty_name,
+        mangled_name,
+        crate_name: tcx.crate_name(test_desc.krate).to_string(),
+        original_file: loc.filename,
+        original_start_line: loc.start_line,
+        original_end_line: loc.end_line,
+        attributes: HarnessAttributes::default(),
+        // TODO: This no longer needs to be an Option.
+        goto_file: Some(model_file),
+    }
+}

kani-compiler/src/kani_middle/mod.rs
@@ -6,7 +6,10 @@
 use std::collections::HashSet;
 
 use kani_queries::{QueryDb, UserInput};
-use rustc_hir::{def::DefKind, def_id::LOCAL_CRATE};
+use rustc_hir::{
+    def::DefKind,
+    def_id::{DefId, LOCAL_CRATE},
+};
 use rustc_middle::mir::mono::MonoItem;
 use rustc_middle::span_bug;
 use rustc_middle::ty::layout::{
@@ -24,6 +27,7 @@ use self::attributes::{check_attributes, check_unstable_features};
 pub mod analysis;
 pub mod attributes;
 pub mod coercion;
+pub mod metadata;
 pub mod provide;
 pub mod reachability;
 pub mod resolve;
@@ -70,6 +74,40 @@ pub fn check_reachable_items(tcx: TyCtxt, queries: &QueryDb, items: &[MonoItem])
         }
     }
     tcx.sess.abort_if_errors();
+}
+
+/// Structure that represents the source location of a definition.
+/// TODO: Use `InternedString` once we move it out of the cprover_bindings.
+/// <https://github.com/model-checking/kani/issues/2435>
+pub struct SourceLocation {
+    pub filename: String,
+    pub start_line: usize,
+    pub start_col: usize,
+    pub end_line: usize,
+    pub end_col: usize,
+}
+
+impl SourceLocation {
+    pub fn new(tcx: TyCtxt, span: &Span) -> Self {
+        let smap = tcx.sess.source_map();
+        let lo = smap.lookup_char_pos(span.lo());
+        let start_line = lo.line;
+        let start_col = 1 + lo.col_display;
+        let hi = smap.lookup_char_pos(span.hi());
+        let end_line = hi.line;
+        let end_col = 1 + hi.col_display;
+        let local_filename = lo.file.name.prefer_local().to_string_lossy().to_string();
+        let filename = match std::fs::canonicalize(local_filename.clone()) {
+            Ok(pathbuf) => pathbuf.to_str().unwrap().to_string(),
+            Err(_) => local_filename,
+        };
+        SourceLocation { filename, start_line, start_col, end_line, end_col }
+    }
+
+    pub fn def_id_loc(tcx: TyCtxt, def_id: DefId) -> Self {
+        let span = tcx.def_span(def_id);
+        Self::new(tcx, &span)
+    }
 }
 
 /// Get the FnAbi of a given instance with no extra variadic arguments.

kani-compiler/src/codegen_cprover_gotoc/mod.rs
@@ -7,6 +7,6 @@ mod context;
 mod overrides;
 mod utils;
 
-pub use compiler_interface::GotocCodegenBackend;
+pub use compiler_interface::{GotocCodegenBackend, UnsupportedConstructs};
 pub use context::GotocCtx;
 pub use context::VtableCtx;

kani-driver/src/metadata.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use anyhow::Result;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use tracing::{debug, trace};
 
 use kani_metadata::{
@@ -149,6 +149,7 @@ pub fn mock_proof_harness(
     name: &str,
     unwind_value: Option<u32>,
     krate: Option<&str>,
+    model_file: Option<PathBuf>,
 ) -> HarnessMetadata {
     HarnessMetadata {
         pretty_name: name.into(),
@@ -158,7 +159,7 @@ pub fn mock_proof_harness(
         original_start_line: 0,
         original_end_line: 0,
         attributes: HarnessAttributes { unwind_value, proof: true, ..Default::default() },
-        goto_file: None,
+        goto_file: model_file,
     }
 }
 
@@ -192,9 +193,9 @@ mod tests {
     #[test]
     fn check_find_proof_harness() {
         let harnesses = vec![
-            mock_proof_harness("check_one", None, None),
-            mock_proof_harness("module::check_two", None, None),
-            mock_proof_harness("module::not_check_three", None, None),
+            mock_proof_harness("check_one", None, None, None),
+            mock_proof_harness("module::check_two", None, None, None),
+            mock_proof_harness("module::not_check_three", None, None, None),
         ];
         let ref_harnesses = harnesses.iter().collect::<Vec<_>>();
         assert_eq!(

cprover_bindings/src/irep/goto_binary_serde.rs
@@ -9,7 +9,7 @@ use std::fs::File;
 use std::hash::Hash;
 use std::io::{self, BufReader};
 use std::io::{BufWriter, Bytes, Error, ErrorKind, Read, Write};
-use std::path::PathBuf;
+use std::path::Path;
 
 /// Writes a symbol table to a file in goto binary format in version 5.
 ///
@@ -18,7 +18,7 @@ use std::path::PathBuf;
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::SymbolTable) {
+pub fn write_goto_binary_file(filename: &Path, source: &crate::goto_program::SymbolTable) {
     let out_file = File::create(filename).unwrap();
     let mut writer = BufWriter::new(out_file);
     let mut serializer = GotoBinarySerializer::new(&mut writer);
@@ -33,7 +33,7 @@ pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn read_goto_binary_file(filename: &PathBuf) -> io::Result<()> {
+pub fn read_goto_binary_file(filename: &Path) -> io::Result<()> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let mut deserializer = GotoBinaryDeserializer::new(reader);

kani-compiler/src/codegen_cprover_gotoc/codegen/function.rs
@@ -4,10 +4,8 @@
 //! This file contains functions related to codegenning MIR functions into gotoc
 
 use crate::codegen_cprover_gotoc::GotocCtx;
-use crate::kani_middle::attributes::{extract_harness_attributes, is_test_harness_closure};
 use cbmc::goto_program::{Expr, Stmt, Symbol};
 use cbmc::InternString;
-use kani_metadata::{HarnessAttributes, HarnessMetadata};
 use rustc_middle::mir::traversal::reverse_postorder;
 use rustc_middle::mir::{Body, HasLocalDecls, Local};
 use rustc_middle::ty::{self, Instance};
@@ -89,9 +87,6 @@ impl<'tcx> GotocCtx<'tcx> {
             let stmts = self.current_fn_mut().extract_block();
             let body = Stmt::block(stmts, loc);
             self.symbol_table.update_fn_declaration_with_definition(&name, body);
-
-            self.record_kani_attributes();
-            self.record_test_harness_metadata();
         }
         self.reset_current_fn();
     }
@@ -243,51 +238,5 @@ impl<'tcx> GotocCtx<'tcx> {
             )
         });
         self.reset_current_fn();
-    }
-
-    /// We record test harness information in kani-metadata, just like we record
-    /// proof harness information. This is used to support e.g. cargo-kani assess.
-    ///
-    /// Note that we do not actually spot the function that was annotated by `#[test]`
-    /// but instead the closure that gets put into the "test description" that macro
-    /// expands into. (See comment below) This ends up being preferrable, actually,
-    /// as it add asserts for tests that return `Result` types.
-    fn record_test_harness_metadata(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        if is_test_harness_closure(self.tcx, def_id) {
-            self.test_harnesses.push(self.generate_metadata(None))
-        }
-    }
-
-    /// This updates the goto context with any information that should be accumulated from a function's
-    /// attributes.
-    ///
-    /// Handle all attributes i.e. `#[kani::x]` (which kani_macros translates to `#[kanitool::x]` for us to handle here)
-    fn record_kani_attributes(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        let attributes = extract_harness_attributes(self.tcx, def_id);
-        if attributes.is_some() {
-            self.proof_harnesses.push(self.generate_metadata(attributes));
-        }
-    }
-
-    /// Create the default proof harness for the current function
-    fn generate_metadata(&self, attributes: Option<HarnessAttributes>) -> HarnessMetadata {
-        let current_fn = self.current_fn();
-        let pretty_name = current_fn.readable_name().to_owned();
-        let mangled_name = current_fn.name();
-        let loc = self.codegen_span(&current_fn.mir().span);
-
-        HarnessMetadata {
-            pretty_name,
-            mangled_name,
-            crate_name: current_fn.krate(),
-            original_file: loc.filename().unwrap(),
-            original_start_line: loc.start_line().unwrap() as usize,
-            original_end_line: loc.end_line().unwrap() as usize,
-            attributes: attributes.unwrap_or_default(),
-            // We record the actual path after codegen before we dump the metadata into a file.
-            goto_file: None,
-        }
     }
 }

kani-compiler/src/codegen_cprover_gotoc/codegen/span.rs
@@ -3,32 +3,21 @@
 
 //! MIR Span related functions
 
-use crate::codegen_cprover_gotoc::GotocCtx;
+use crate::{codegen_cprover_gotoc::GotocCtx, kani_middle::SourceLocation};
 use cbmc::goto_program::Location;
 use rustc_middle::mir::{Local, VarDebugInfo, VarDebugInfoContents};
 use rustc_span::Span;
 
 impl<'tcx> GotocCtx<'tcx> {
     pub fn codegen_span(&self, sp: &Span) -> Location {
-        let smap = self.tcx.sess.source_map();
-        let lo = smap.lookup_char_pos(sp.lo());
-        let start_line = lo.line;
-        let start_col = 1 + lo.col_display;
-        let hi = smap.lookup_char_pos(sp.hi());
-        let end_line = hi.line;
-        let end_col = 1 + hi.col_display;
-        let filename0 = lo.file.name.prefer_local().to_string_lossy().to_string();
-        let filename1 = match std::fs::canonicalize(filename0.clone()) {
-            Ok(pathbuf) => pathbuf.to_str().unwrap().to_string(),
-            Err(_) => filename0,
-        };
+        let loc = SourceLocation::new(self.tcx, sp);
         Location::new(
-            filename1,
+            loc.filename,
             self.current_fn.as_ref().map(|x| x.readable_name().to_string()),
-            start_line,
-            Some(start_col),
-            end_line,
-            Some(end_col),
+            loc.start_line,
+            Some(loc.start_col),
+            loc.end_line,
+            Some(loc.end_col),
         )
     }
 

commit 19b7a7da12c15037c174960948708ac5d131c9ba

Fix regression on concrete playback inplace (#2454)
This fixes a regression from #2439. The compiler should store the location of the function body instead of the declaration. Storing the correct location fixes how concrete playback stores the generated unit test.

kani-compiler/src/kani_middle/metadata.rs
@@ -8,7 +8,7 @@ use std::path::Path;
 use crate::kani_middle::attributes::test_harness_name;
 use kani_metadata::{ArtifactType, HarnessAttributes, HarnessMetadata};
 use rustc_hir::def_id::DefId;
-use rustc_middle::ty::{Instance, TyCtxt};
+use rustc_middle::ty::{Instance, InstanceDef, TyCtxt, WithOptConstParam};
 
 use super::{attributes::extract_harness_attributes, SourceLocation};
 
@@ -24,7 +24,8 @@ pub fn gen_proof_metadata(tcx: TyCtxt, def_id: DefId, base_name: &Path) -> Harne
         tcx.symbol_name(Instance::mono(tcx, def_id)).to_string()
     };
 
-    let loc = SourceLocation::def_id_loc(tcx, def_id);
+    let body = tcx.instance_mir(InstanceDef::Item(WithOptConstParam::unknown(def_id)));
+    let loc = SourceLocation::new(tcx, &body.span);
     let file_stem = format!("{}_{mangled_name}", base_name.file_stem().unwrap().to_str().unwrap());
     let model_file = base_name.with_file_name(file_stem).with_extension(ArtifactType::SymTabGoto);
 
@@ -51,7 +52,8 @@ pub fn gen_test_metadata<'tcx>(
 ) -> HarnessMetadata {
     let pretty_name = test_harness_name(tcx, test_desc);
     let mangled_name = tcx.symbol_name(test_fn).to_string();
-    let loc = SourceLocation::def_id_loc(tcx, test_desc);
+    let body = tcx.instance_mir(InstanceDef::Item(WithOptConstParam::unknown(test_desc)));
+    let loc = SourceLocation::new(tcx, &body.span);
     let file_stem = format!("{}_{mangled_name}", base_name.file_stem().unwrap().to_str().unwrap());
     let model_file = base_name.with_file_name(file_stem).with_extension(ArtifactType::SymTabGoto);
 

kani-compiler/src/kani_middle/mod.rs
@@ -6,10 +6,7 @@
 use std::collections::HashSet;
 
 use kani_queries::{QueryDb, UserInput};
-use rustc_hir::{
-    def::DefKind,
-    def_id::{DefId, LOCAL_CRATE},
-};
+use rustc_hir::{def::DefKind, def_id::LOCAL_CRATE};
 use rustc_middle::mir::mono::MonoItem;
 use rustc_middle::span_bug;
 use rustc_middle::ty::layout::{
@@ -102,11 +99,6 @@ impl SourceLocation {
             Err(_) => local_filename,
         };
         SourceLocation { filename, start_line, start_col, end_line, end_col }
-    }
-
-    pub fn def_id_loc(tcx: TyCtxt, def_id: DefId) -> Self {
-        let span = tcx.def_span(def_id);
-        Self::new(tcx, &span)
     }
 }
 