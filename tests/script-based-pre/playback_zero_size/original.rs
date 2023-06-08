cf203d30eca62469ab6dbdb115ff6ce7ceb3c36c
Yoshiki Takashima <ytakashl@amazon.com>
2023-06-08 10:11:06 -0400

    Escape Zero-size types in playback (#2508)

---
+++ tests/script-based-pre/playback_zero_size/original.rs
@@ -0,0 +1,9 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//! This checks that Kani zero-size playback, 2 of them in a row.
+
+#[kani::proof]
+fn any_is_ok() {
+    let unit: () = kani::any();
+    kani::cover!(unit == ());
+}
