eb7035a075286be2e872943aadfe259e7fac1990
Celina G. Val <celinval@amazon.com>
2023-06-21 14:18:38 -0700

    Fix flaky tests
     - The object bits test itself doesn't create that many objects since
       an array is represented as the same allocated object. Use LinkedList
       instead.
     - Do not rely on property number.
     - Do not rely on the order that failed checks is printed.

--- tests/expected/object-bits/insufficient/main.rs
+++ tests/expected/object-bits/insufficient/main.rs
@@ -1,15 +1,11 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-
-// Checks for error message with an --object-bits value that is too small
-
 // kani-flags: --default-unwind 30 --enable-unstable --cbmc-args --object-bits 5
+//! Checks for error message with an --object-bits value that is too small
+//! Use linked list to ensure that each member represents a new object.
 
 #[kani::proof]
 fn main() {
-    let mut arr: [i32; 100] = kani::Arbitrary::any_array();
-    for i in 0..30 {
-        arr[i] = kani::any();
-    }
-    assert!(arr[0] > arr[0] - arr[99]);
+    let arr: [i32; 18] = kani::Arbitrary::any_array();
+    std::hint::black_box(std::collections::LinkedList::from(arr));
 }
