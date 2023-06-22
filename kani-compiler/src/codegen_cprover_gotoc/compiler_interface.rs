model-checking/kani

commit 2a09d79db02ae9e9f2d7611bf2a2a21327ffe13d

Perform reachability analysis on a per-harness basis (#2439)
Kani compiler used to generate one `goto-program` for all harnesses in one crate. In some cases, this actually had a negative impact on the harness verification time. This was first reported in #1659

The main changes were done in the compiler's module `compiler_interface` and the module `project` from the driver. The compiler will now gather all the harnesses beforehand and it will perform reachability + codegen steps for each harness. All files related to a harness `goto-program` will follow the naming convention bellow:
```
<BASE_NAME>_<MANGLED_NAME>.<EXTENSION>
```
This applies to symtab / goto / type_map / restriction files.

The metadata file is still generated once per target crate, and its name is still the same (`<BASE_NAME>.kani-metadata.json`).

On the driver side, the way we process the artifacts have changed. The driver will now read the metadata for each crate, and collect all artifacts based on the symtab goto file that is recorded in the metadata of each harness.

These changes do not apply for `--function`. We still keep all artifacts based on the crate's `<BASE_NAME>` and we have a separate logic to handle that. Fixing this is captured by #2129.

kani-compiler/src/kani_middle/mod.rs
@@ -6,7 +6,10 @@
 use std::collections::HashSet;
 
 use kani_queries::{QueryDb, UserInput};
-use rustc_hir::{def::DefKind, def_id::LOCAL_CRATE};
+use rustc_hir::{
+    def::DefKind,
+    def_id::{DefId, LOCAL_CRATE},
+};
 use rustc_middle::mir::mono::MonoItem;
 use rustc_middle::span_bug;
 use rustc_middle::ty::layout::{
@@ -24,6 +27,7 @@ use self::attributes::{check_attributes, check_unstable_features};
 pub mod analysis;
 pub mod attributes;
 pub mod coercion;
+pub mod metadata;
 pub mod provide;
 pub mod reachability;
 pub mod resolve;
@@ -70,6 +74,40 @@ pub fn check_reachable_items(tcx: TyCtxt, queries: &QueryDb, items: &[MonoItem])
         }
     }
     tcx.sess.abort_if_errors();
+}
+
+/// Structure that represents the source location of a definition.
+/// TODO: Use `InternedString` once we move it out of the cprover_bindings.
+/// <https://github.com/model-checking/kani/issues/2435>
+pub struct SourceLocation {
+    pub filename: String,
+    pub start_line: usize,
+    pub start_col: usize,
+    pub end_line: usize,
+    pub end_col: usize,
+}
+
+impl SourceLocation {
+    pub fn new(tcx: TyCtxt, span: &Span) -> Self {
+        let smap = tcx.sess.source_map();
+        let lo = smap.lookup_char_pos(span.lo());
+        let start_line = lo.line;
+        let start_col = 1 + lo.col_display;
+        let hi = smap.lookup_char_pos(span.hi());
+        let end_line = hi.line;
+        let end_col = 1 + hi.col_display;
+        let local_filename = lo.file.name.prefer_local().to_string_lossy().to_string();
+        let filename = match std::fs::canonicalize(local_filename.clone()) {
+            Ok(pathbuf) => pathbuf.to_str().unwrap().to_string(),
+            Err(_) => local_filename,
+        };
+        SourceLocation { filename, start_line, start_col, end_line, end_col }
+    }
+
+    pub fn def_id_loc(tcx: TyCtxt, def_id: DefId) -> Self {
+        let span = tcx.def_span(def_id);
+        Self::new(tcx, &span)
+    }
 }
 
 /// Get the FnAbi of a given instance with no extra variadic arguments.

kani-compiler/src/codegen_cprover_gotoc/mod.rs
@@ -7,6 +7,6 @@ mod context;
 mod overrides;
 mod utils;
 
-pub use compiler_interface::GotocCodegenBackend;
+pub use compiler_interface::{GotocCodegenBackend, UnsupportedConstructs};
 pub use context::GotocCtx;
 pub use context::VtableCtx;

cprover_bindings/src/irep/goto_binary_serde.rs
@@ -9,7 +9,7 @@ use std::fs::File;
 use std::hash::Hash;
 use std::io::{self, BufReader};
 use std::io::{BufWriter, Bytes, Error, ErrorKind, Read, Write};
-use std::path::PathBuf;
+use std::path::Path;
 
 /// Writes a symbol table to a file in goto binary format in version 5.
 ///
@@ -18,7 +18,7 @@ use std::path::PathBuf;
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::SymbolTable) {
+pub fn write_goto_binary_file(filename: &Path, source: &crate::goto_program::SymbolTable) {
     let out_file = File::create(filename).unwrap();
     let mut writer = BufWriter::new(out_file);
     let mut serializer = GotoBinarySerializer::new(&mut writer);
@@ -33,7 +33,7 @@ pub fn write_goto_binary_file(filename: &PathBuf, source: &crate::goto_program::
 /// - src/util/irep_serialization.h
 /// - src/util/irep_hash_container.h
 /// - src/util/irep_hash.h
-pub fn read_goto_binary_file(filename: &PathBuf) -> io::Result<()> {
+pub fn read_goto_binary_file(filename: &Path) -> io::Result<()> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let mut deserializer = GotoBinaryDeserializer::new(reader);

kani-compiler/src/codegen_cprover_gotoc/codegen/function.rs
@@ -4,10 +4,8 @@
 //! This file contains functions related to codegenning MIR functions into gotoc
 
 use crate::codegen_cprover_gotoc::GotocCtx;
-use crate::kani_middle::attributes::{extract_harness_attributes, is_test_harness_closure};
 use cbmc::goto_program::{Expr, Stmt, Symbol};
 use cbmc::InternString;
-use kani_metadata::{HarnessAttributes, HarnessMetadata};
 use rustc_middle::mir::traversal::reverse_postorder;
 use rustc_middle::mir::{Body, HasLocalDecls, Local};
 use rustc_middle::ty::{self, Instance};
@@ -89,9 +87,6 @@ impl<'tcx> GotocCtx<'tcx> {
             let stmts = self.current_fn_mut().extract_block();
             let body = Stmt::block(stmts, loc);
             self.symbol_table.update_fn_declaration_with_definition(&name, body);
-
-            self.record_kani_attributes();
-            self.record_test_harness_metadata();
         }
         self.reset_current_fn();
     }
@@ -243,51 +238,5 @@ impl<'tcx> GotocCtx<'tcx> {
             )
         });
         self.reset_current_fn();
-    }
-
-    /// We record test harness information in kani-metadata, just like we record
-    /// proof harness information. This is used to support e.g. cargo-kani assess.
-    ///
-    /// Note that we do not actually spot the function that was annotated by `#[test]`
-    /// but instead the closure that gets put into the "test description" that macro
-    /// expands into. (See comment below) This ends up being preferrable, actually,
-    /// as it add asserts for tests that return `Result` types.
-    fn record_test_harness_metadata(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        if is_test_harness_closure(self.tcx, def_id) {
-            self.test_harnesses.push(self.generate_metadata(None))
-        }
-    }
-
-    /// This updates the goto context with any information that should be accumulated from a function's
-    /// attributes.
-    ///
-    /// Handle all attributes i.e. `#[kani::x]` (which kani_macros translates to `#[kanitool::x]` for us to handle here)
-    fn record_kani_attributes(&mut self) {
-        let def_id = self.current_fn().instance().def_id();
-        let attributes = extract_harness_attributes(self.tcx, def_id);
-        if attributes.is_some() {
-            self.proof_harnesses.push(self.generate_metadata(attributes));
-        }
-    }
-
-    /// Create the default proof harness for the current function
-    fn generate_metadata(&self, attributes: Option<HarnessAttributes>) -> HarnessMetadata {
-        let current_fn = self.current_fn();
-        let pretty_name = current_fn.readable_name().to_owned();
-        let mangled_name = current_fn.name();
-        let loc = self.codegen_span(&current_fn.mir().span);
-
-        HarnessMetadata {
-            pretty_name,
-            mangled_name,
-            crate_name: current_fn.krate(),
-            original_file: loc.filename().unwrap(),
-            original_start_line: loc.start_line().unwrap() as usize,
-            original_end_line: loc.end_line().unwrap() as usize,
-            attributes: attributes.unwrap_or_default(),
-            // We record the actual path after codegen before we dump the metadata into a file.
-            goto_file: None,
-        }
     }
 }

kani-compiler/src/codegen_cprover_gotoc/codegen/span.rs
@@ -3,32 +3,21 @@
 
 //! MIR Span related functions
 
-use crate::codegen_cprover_gotoc::GotocCtx;
+use crate::{codegen_cprover_gotoc::GotocCtx, kani_middle::SourceLocation};
 use cbmc::goto_program::Location;
 use rustc_middle::mir::{Local, VarDebugInfo, VarDebugInfoContents};
 use rustc_span::Span;
 
 impl<'tcx> GotocCtx<'tcx> {
     pub fn codegen_span(&self, sp: &Span) -> Location {
-        let smap = self.tcx.sess.source_map();
-        let lo = smap.lookup_char_pos(sp.lo());
-        let start_line = lo.line;
-        let start_col = 1 + lo.col_display;
-        let hi = smap.lookup_char_pos(sp.hi());
-        let end_line = hi.line;
-        let end_col = 1 + hi.col_display;
-        let filename0 = lo.file.name.prefer_local().to_string_lossy().to_string();
-        let filename1 = match std::fs::canonicalize(filename0.clone()) {
-            Ok(pathbuf) => pathbuf.to_str().unwrap().to_string(),
-            Err(_) => filename0,
-        };
+        let loc = SourceLocation::new(self.tcx, sp);
         Location::new(
-            filename1,
+            loc.filename,
             self.current_fn.as_ref().map(|x| x.readable_name().to_string()),
-            start_line,
-            Some(start_col),
-            end_line,
-            Some(end_col),
+            loc.start_line,
+            Some(loc.start_col),
+            loc.end_line,
+            Some(loc.end_col),
         )
     }
 

kani-compiler/src/codegen_cprover_gotoc/context/goto_ctx.rs
@@ -17,11 +17,11 @@ use super::current_fn::CurrentFnCtx;
 use super::vtable_ctx::VtableCtx;
 use crate::codegen_cprover_gotoc::overrides::{fn_hooks, GotocHooks};
 use crate::codegen_cprover_gotoc::utils::full_crate_name;
+use crate::codegen_cprover_gotoc::UnsupportedConstructs;
 use cbmc::goto_program::{DatatypeComponent, Expr, Location, Stmt, Symbol, SymbolTable, Type};
 use cbmc::utils::aggr_tag;
-use cbmc::InternedString;
-use cbmc::{MachineModel, RoundingMode};
-use kani_metadata::{HarnessMetadata, UnsupportedFeature};
+use cbmc::{InternedString, MachineModel};
+use kani_metadata::HarnessMetadata;
 use kani_queries::{QueryDb, UserInput};
 use rustc_data_structures::fx::FxHashMap;
 use rustc_data_structures::owning_ref::OwningRef;
@@ -35,10 +35,8 @@ use rustc_middle::ty::layout::{
 };
 use rustc_middle::ty::{self, Instance, Ty, TyCtxt};
 use rustc_session::cstore::MetadataLoader;
-use rustc_session::Session;
 use rustc_span::source_map::{respan, Span};
 use rustc_target::abi::call::FnAbi;
-use rustc_target::abi::Endian;
 use rustc_target::abi::{HasDataLayout, TargetDataLayout};
 use rustc_target::spec::Target;
 use std::path::Path;
@@ -70,19 +68,22 @@ pub struct GotocCtx<'tcx> {
     /// a global counter for generating unique IDs for checks
     pub global_checks_count: u64,
     /// A map of unsupported constructs that were found while codegen
-    pub unsupported_constructs: FxHashMap<InternedString, Vec<Location>>,
+    pub unsupported_constructs: UnsupportedConstructs,
     /// A map of concurrency constructs that are treated sequentially.
     /// We collect them and print one warning at the end if not empty instead of printing one
     /// warning at each occurrence.
-    pub concurrent_constructs: FxHashMap<InternedString, Vec<Location>>,
+    pub concurrent_constructs: UnsupportedConstructs,
 }
 
 /// Constructor
 impl<'tcx> GotocCtx<'tcx> {
-    pub fn new(tcx: TyCtxt<'tcx>, queries: QueryDb) -> GotocCtx<'tcx> {
+    pub fn new(
+        tcx: TyCtxt<'tcx>,
+        queries: QueryDb,
+        machine_model: &MachineModel,
+    ) -> GotocCtx<'tcx> {
         let fhks = fn_hooks();
-        let mm = machine_model_from_session(tcx.sess);
-        let symbol_table = SymbolTable::new(mm);
+        let symbol_table = SymbolTable::new(machine_model.clone());
         let emit_vtable_restrictions = queries.get_emit_vtable_restrictions();
         GotocCtx {
             tcx,
@@ -113,32 +114,6 @@ impl<'tcx> GotocCtx<'tcx> {
 
     pub fn current_fn_mut(&mut self) -> &mut CurrentFnCtx<'tcx> {
         self.current_fn.as_mut().unwrap()
-    }
-
-    /// Maps the goto-context "unsupported features" data into the
-    /// KaniMetadata "unsupported features" format.
-    ///
-    /// These are different because the KaniMetadata is a flat serializable list,
-    /// while we need a more richly structured HashMap in the goto context.
-    pub(crate) fn unsupported_metadata(&self) -> Vec<UnsupportedFeature> {
-        self.unsupported_constructs
-            .iter()
-            .map(|(construct, location)| UnsupportedFeature {
-                feature: construct.to_string(),
-                locations: location
-                    .iter()
-                    .map(|l| {
-                        // We likely (and should) have no instances of
-                        // calling `codegen_unimplemented` without file/line.
-                        // So while we map out of `Option` here, we expect them to always be `Some`
-                        kani_metadata::Location {
-                            filename: l.filename().unwrap_or_default(),
-                            start_line: l.start_line().unwrap_or_default(),
-                        }
-                    })
-                    .collect(),
-            })
-            .collect()
     }
 }
 
@@ -432,118 +407,5 @@ impl MetadataLoader for GotocMetadataLoader {
 
     fn get_dylib_metadata(&self, target: &Target, filename: &Path) -> Result<MetadataRef, String> {
         self.get_rlib_metadata(target, filename)
-    }
-}
-
-/// Builds a machine model which is required by CBMC
-fn machine_model_from_session(sess: &Session) -> MachineModel {
-    // The model assumes a `x86_64-unknown-linux-gnu`, `x86_64-apple-darwin`
-    // or `aarch64-apple-darwin` platform. We check the target platform in function
-    // `check_target` from src/kani-compiler/src/codegen_cprover_gotoc/compiler_interface.rs
-    // and error if it is not any of the ones we expect.
-    let architecture = &sess.target.arch;
-    let pointer_width = sess.target.pointer_width.into();
-
-    // The model assumes the following values for session options:
-    //   * `min_global_align`: 1
-    //   * `endian`: `Endian::Little`
-    //
-    // We check these options in function `check_options` from
-    // src/kani-compiler/src/codegen_cprover_gotoc/compiler_interface.rs
-    // and error if their values are not the ones we expect.
-    let alignment = sess.target.options.min_global_align.unwrap_or(1);
-    let is_big_endian = match sess.target.options.endian {
-        Endian::Little => false,
-        Endian::Big => true,
-    };
-
-    // The values below cannot be obtained from the session so they are
-    // hardcoded using standard ones for the supported platforms
-    // see /tools/sizeofs/main.cpp.
-    // For reference, the definition in CBMC:
-    //https://github.com/diffblue/cbmc/blob/develop/src/util/config.cpp
-    match architecture.as_ref() {
-        "x86_64" => {
-            let bool_width = 8;
-            let char_is_unsigned = false;
-            let char_width = 8;
-            let double_width = 64;
-            let float_width = 32;
-            let int_width = 32;
-            let long_double_width = 128;
-            let long_int_width = 64;
-            let long_long_int_width = 64;
-            let short_int_width = 16;
-            let single_width = 32;
-            let wchar_t_is_unsigned = false;
-            let wchar_t_width = 32;
-
-            MachineModel {
-                architecture: architecture.to_string(),
-                alignment,
-                bool_width,
-                char_is_unsigned,
-                char_width,
-                double_width,
-                float_width,
-                int_width,
-                is_big_endian,
-                long_double_width,
-                long_int_width,
-                long_long_int_width,
-                memory_operand_size: int_width / 8,
-                null_is_zero: true,
-                pointer_width,
-                rounding_mode: RoundingMode::ToNearest,
-                short_int_width,
-                single_width,
-                wchar_t_is_unsigned,
-                wchar_t_width,
-                word_size: int_width,
-            }
-        }
-        "aarch64" => {
-            let bool_width = 8;
-            let char_is_unsigned = true;
-            let char_width = 8;
-            let double_width = 64;
-            let float_width = 32;
-            let int_width = 32;
-            let long_double_width = 64;
-            let long_int_width = 64;
-            let long_long_int_width = 64;
-            let short_int_width = 16;
-            let single_width = 32;
-            let wchar_t_is_unsigned = false;
-            let wchar_t_width = 32;
-
-            MachineModel {
-                // CBMC calls it arm64, not aarch64
-                architecture: "arm64".to_string(),
-                alignment,
-                bool_width,
-                char_is_unsigned,
-                char_width,
-                double_width,
-                float_width,
-                int_width,
-                is_big_endian,
-                long_double_width,
-                long_int_width,
-                long_long_int_width,
-                memory_operand_size: int_width / 8,
-                null_is_zero: true,
-                pointer_width,
-                rounding_mode: RoundingMode::ToNearest,
-                short_int_width,
-                single_width,
-                wchar_t_is_unsigned,
-                wchar_t_width,
-                word_size: int_width,
-            }
-        }
-        _ => {
-            panic!("Unsupported architecture: {architecture}");
-        }
     }
 }

kani-compiler/src/kani_middle/attributes.rs
@@ -78,30 +78,11 @@ pub fn is_test_harness_description(tcx: TyCtxt, def_id: DefId) -> bool {
     tcx.sess.contains_name(attrs, rustc_span::symbol::sym::rustc_test_marker)
 }
 
-/// Is this the closure inside of a test description const (i.e. macro expanded from a `#[test]`)?
-///
-/// We're trying to detect the closure (`||`) inside code like:
-///
-/// ```ignore
-/// #[rustc_test_marker]
-/// pub const check_2: test::TestDescAndFn = test::TestDescAndFn {
-///     desc: ...,
-///     testfn: test::StaticTestFn(|| test::assert_test_result(check_2())),
-/// };
-/// ```
-pub fn is_test_harness_closure(tcx: TyCtxt, def_id: DefId) -> bool {
-    if !def_id.is_local() {
-        return false;
-    }
-
-    let local_def_id = def_id.expect_local();
-    let hir_id = tcx.hir().local_def_id_to_hir_id(local_def_id);
-
-    // The parent item of the closure appears to reliably be the `const` declaration item.
-    let parent_id = tcx.hir().get_parent_item(hir_id);
-    let parent_def_id = parent_id.to_def_id();
-
-    is_test_harness_description(tcx, parent_def_id)
+/// Extract the test harness name from the `#[rustc_test_maker]`
+pub fn test_harness_name(tcx: TyCtxt, def_id: DefId) -> String {
+    let attrs = tcx.get_attrs_unchecked(def_id);
+    let marker = tcx.sess.find_by_name(attrs, rustc_span::symbol::sym::rustc_test_marker).unwrap();
+    parse_str_value(&marker).unwrap()
 }
 
 /// Extract all Kani attributes for a given `def_id` if any exists.
@@ -490,6 +471,16 @@ fn parse_key_values(attr: &Attribute) -> Result<BTreeMap<String, String>, String
             )),
         })
         .collect()
+}
+
+/// Extracts the string value argument from the attribute provided.
+///
+/// For attributes with the following format, this will return a string that represents "VALUE".
+/// - `#[attribute = "VALUE"]`
+fn parse_str_value(attr: &Attribute) -> Option<String> {
+    // Vector of meta items , that contain the arguments given the attribute
+    let value = attr.value_str();
+    value.map(|sym| sym.to_string())
 }
 
 /// If the attribute is named `kanitool::name`, this extracts `name`

commit 3224cbd564d2053789971d36253759e4c385f646

Fix MIR dump to emit one MIR per-harness (#2556)
This was a regression introduced by #2439. We were still writing the result of the reachability algorithm to the same file for every harness. Thus, we could only see the MIR for the last harness that was processed.

Use the file name that is specific for the harness instead and generate one MIR file per harness like we do with other files generated by kani-compiler.

kani-compiler/src/codegen_cprover_gotoc/compiler_interface.rs
@@ -86,7 +86,7 @@ impl GotocCodegenBackend {
             || collect_reachable_items(tcx, starting_items),
             "codegen reachability analysis",
         );
-        dump_mir_items(tcx, &items);
+        dump_mir_items(tcx, &items, &symtab_goto.with_extension("kani.mir"));
 
         // Follow rustc naming convention (cx is abbrev for context).
         // https://rustc-dev-guide.rust-lang.org/conventions.html#naming-conventions

kani-compiler/src/kani_middle/mod.rs
@@ -4,6 +4,7 @@
 //! and transformations.
 
 use std::collections::HashSet;
+use std::path::Path;
 
 use crate::kani_queries::QueryDb;
 use rustc_hir::{def::DefKind, def_id::DefId, def_id::LOCAL_CRATE};
@@ -79,7 +80,7 @@ pub fn check_reachable_items(tcx: TyCtxt, queries: &QueryDb, items: &[MonoItem])
 }
 
 /// Print MIR for the reachable items if the `--emit mir` option was provided to rustc.
-pub fn dump_mir_items(tcx: TyCtxt, items: &[MonoItem]) {
+pub fn dump_mir_items(tcx: TyCtxt, items: &[MonoItem], output: &Path) {
     /// Convert MonoItem into a DefId.
     /// Skip stuff that we cannot generate the MIR items.
     fn visible_item<'tcx>(item: &MonoItem<'tcx>) -> Option<(MonoItem<'tcx>, DefId)> {
@@ -96,9 +97,7 @@ pub fn dump_mir_items(tcx: TyCtxt, items: &[MonoItem]) {
 
     if tcx.sess.opts.output_types.contains_key(&OutputType::Mir) {
         // Create output buffer.
-        let outputs = tcx.output_filenames(());
-        let path = outputs.output_path(OutputType::Mir).with_extension("kani.mir");
-        let out_file = File::create(&path).unwrap();
+        let out_file = File::create(output).unwrap();
         let mut writer = BufWriter::new(out_file);
 
         // For each def_id, dump their MIR