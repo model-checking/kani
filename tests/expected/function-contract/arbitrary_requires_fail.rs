bed5def0cd2e155bbfe27d739c138f389dcd0743
Justus Adam <justbldr@amazon.com>
2023-06-28 15:08:34 -0700

    Fixing regression test setup

--- tests/expected/function-contract/arbitrary_requires_fail.rs
+++ tests/expected/function-contract/arbitrary_requires_fail.rs
@@ -1,6 +1,5 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-verify-fail
 
 fn div(dividend: u32, divisor: u32) -> u32 {
     dividend / divisor
--- tests/expected/function-contract/simple_ensures_fail.rs
+++ tests/expected/function-contract/simple_ensures_fail.rs
@@ -1,6 +1,5 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// kani-verify-fail
 
 #[kani::ensures(result == x)]
 fn max(x: u32, y: u32) -> u32 {
