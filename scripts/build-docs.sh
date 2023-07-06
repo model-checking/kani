79b06d3bf77ff722c1236f53ebde6643f8aedd0a
Kareem Khazem <karkhaz@amazon.com>
2023-07-07 00:56:55 +0100

    Rename old visualization generator script
    To make filenames in a future commit less ambiguous.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -62,7 +62,7 @@ fi
 echo "Building user documentation..."
 # Generate benchcomp documentation from source code
 mkdir -p gen_src
-"${SCRIPT_DIR}/gen_benchcomp_schemas.py" gen_src
+"${SCRIPT_DIR}/gen_visualization_schemas.py" gen_src
 
 # Build the book into ./book/
 mkdir -p book
