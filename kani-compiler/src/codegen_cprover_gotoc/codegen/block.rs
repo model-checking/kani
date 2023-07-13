c9ef05ce51e33e7583db8cd8cb881bbb037211ad
Adrian Palacios <accorell@amazon.com>
2023-07-13 22:36:10 +0000

    Add cover before every statement/terminator

--- kani-compiler/src/codegen_cprover_gotoc/codegen/block.rs
+++ kani-compiler/src/codegen_cprover_gotoc/codegen/block.rs
@@ -3,7 +3,8 @@
 
 use crate::codegen_cprover_gotoc::GotocCtx;
 use cbmc::goto_program::{Stmt, Expr};
-use rustc_middle::mir::{BasicBlock, BasicBlockData, Statement, Terminator};
+use rustc_middle::mir::{BasicBlock, BasicBlockData};
+use rustc_span::Span;
 use tracing::debug;
 
 impl<'tcx> GotocCtx<'tcx> {
@@ -22,48 +23,47 @@ impl<'tcx> GotocCtx<'tcx> {
         match bbd.statements.len() {
             0 => {
                 let term = bbd.terminator();
+                let span = term.source_info.span;
+                // push cover stmt onto block with label
+                // remove the next label
+                let cover = self.cover_with_span(span);
+                self.current_fn_mut().push_onto_block(cover.with_label(label));
                 let tcode = self.codegen_terminator(term);
-                let new_tcode = self.add_cover_term(tcode, term);
-                self.current_fn_mut().push_onto_block(new_tcode.with_label(label));
+                self.current_fn_mut().push_onto_block(tcode);
             }
             _ => {
                 let stmt = &bbd.statements[0];
+                
+                let span = stmt.source_info.span;
+                let cover = self.cover_with_span(span);
+                self.current_fn_mut().push_onto_block(cover.with_label(label));
                 let scode = self.codegen_statement(stmt);
-                let new_scode = self.add_cover_stmt(scode, stmt);
-                self.current_fn_mut().push_onto_block(new_scode.with_label(label.clone()));
+                self.current_fn_mut().push_onto_block(scode);
 
                 for s in &bbd.statements[1..] {
+                    let span = s.source_info.span;
+                    // push cover statements
+                    let cover = self.cover_with_span(span);
+                    self.current_fn_mut().push_onto_block(cover);
                     let stmt = self.codegen_statement(s);
                     self.current_fn_mut().push_onto_block(stmt);
                 }
+                // push cover statements
+                // test with division by zero or overflow
+                // that false assumption will be in the middle of the basic block
                 let term = bbd.terminator();
+                let span = term.source_info.span;
+                // push cover stmt onto block with label
+                // remove the next label
+                let cover = self.cover_with_span(span);
+                self.current_fn_mut().push_onto_block(cover);
                 let tcode = self.codegen_terminator(term);
                 self.current_fn_mut().push_onto_block(tcode);
             }
         }
         self.current_fn_mut().reset_current_bb();
     }
-    fn add_cover_term(&mut self, stmt: Stmt, term: &Terminator<'tcx>) -> Stmt {
-            let span = &term.source_info.span;
-            let loc = self.codegen_span(&span);
-            let stmts = vec![stmt];
-            // let body = Stmt::block(stmts, loc);
-            let cover = self.codegen_cover(Expr::c_true(), "cover_experiment", Some(*span));
-            let mut new_stmts = stmts.clone();
-            new_stmts.insert(0, cover);
-            let body = Stmt::block(new_stmts, loc);
-            body
+    fn cover_with_span(&mut self, span: Span) -> Stmt {
+            self.codegen_cover(Expr::c_true(), "cover_experiment", Some(span))
     }
-
-    fn add_cover_stmt(&mut self, stmt: Stmt, s: &Statement<'tcx>) -> Stmt {
-        let span = &s.source_info.span;
-        let loc = self.codegen_span(&span);
-        let stmts = vec![stmt];
-        // let body = Stmt::block(stmts, loc);
-        let cover = self.codegen_cover(Expr::c_true(), "cover_experiment", Some(*span));
-        let mut new_stmts = stmts.clone();
-        new_stmts.insert(0, cover);
-        let body = Stmt::block(new_stmts, loc);
-        body
-}
 }
