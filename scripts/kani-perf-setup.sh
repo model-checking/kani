dae1c83fd8da5e324e43211b784ea93f8eb77305
Celina G. Val <celinval@amazon.com>
2023-07-25 16:58:28 -0700

    Rename perf/ to make changes backward compatible
    The current benchcomp configuration expects the benchmark results to
    be inside `build/tests/perf`. And changing that is a bit painful since
    we run older versions of `kani-perf.sh` when comparing the performance
    with older versions of Kani.

--- scripts/kani-perf-setup.sh
+++ scripts/kani-perf-setup.sh
@@ -9,7 +9,7 @@ set -o nounset
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 KANI_DIR=$SCRIPT_DIR/..
-PERF_DIR="${KANI_DIR}/tests/perf"
+BENCH_DIR="${KANI_DIR}/tests/benchmarks"
 
 RUN_DIR="/tmp/kani_perf_run_$(date +%s)"
 
@@ -18,31 +18,31 @@ build_kani() {
   cargo build-dev -- --release
 }
 
-prep_perf_files() {
-  mkdir ${RUN_DIR} # We want to error out if the folder exists
-  cp -r ${PERF_DIR} ${RUN_DIR}/perf
-  for overlay_dir in ${PERF_DIR}/overlays/*/; do
+prep_bench_files() {
+  mkdir -p "$( dirname "${RUN_DIR}")"
+  cp -r "${BENCH_DIR}" "${RUN_DIR}"
+  for overlay_dir in ${BENCH_DIR}/overlays/*/; do
     orig_dir=$(basename ${overlay_dir})
     echo "Copying overlays for $orig_dir"
-    cp -r -v ${overlay_dir}* ${RUN_DIR}/perf/${orig_dir}/
+    cp -r -v ${overlay_dir}* ${RUN_DIR}/${orig_dir}/
   done
 }
 
-cleanup_perf() {
+cleanup_artifacts() {
   echo "Cleaning up..."
   rm -r ${RUN_DIR}
   rm "$@"
 }
 
 run_benchmarks() {
-  suite=$(basename "${SUITE}")
-  mode="cargo-kani-test"
+  local suite=$(basename "${SUITE}")
+  local mode="cargo-kani-test"
   echo "Check compiletest suite=$suite mode=$mode"
   cargo run -p compiletest -- --suite $suite --mode $mode --no-fail-fast --report-time "$@"
 }
 
 print_result() {
-  exit_code=$1
+  local exit_code=$1
   echo
   if [ $exit_code -eq 0 ]; then
     echo "All Kani perf tests completed successfully."
@@ -54,32 +54,45 @@ print_result() {
 }
 
 benchmark_build() {
+  # Abort if there is any setup error.
+  set -o errexit
   build_kani
   # Prepare for a verification first
-  prep_perf_files
-  SUITE="${KANI_DIR}/tests/bench_build"
-  ln -s "${RUN_DIR}" "${SUITE}"
+  prep_bench_files
 
   # Now override expected files to just expect a successful build
-  expected_files=$(find ${RUN_DIR} -name "*expected")
+  local expected_files=$(find ${RUN_DIR} -name "*expected")
   for expected_file in ${expected_files}; do
     echo "Compiling" > ${expected_file}
     echo "Finished" >> ${expected_file}
   done
 
+  SUITE="${KANI_DIR}/tests/bench_build"
+  rm "${SUITE}"
+  ln -s "${RUN_DIR}" "${SUITE}"
+
+  # Delay execution errors since we need to cleanup.
+  set +o errexit
   run_benchmarks --kani-flag="--only-codegen"
-  exit_code=$?
-  cleanup_perf "${SUITE}"
+  local exit_code=$?
+  cleanup_artifacts "${SUITE}"
   print_result ${exit_code}
 }
 
 benchmark_verification() {
+  # Abort if there is any setup error.
+  set -o errexit
   build_kani
-  prep_perf_files
-  SUITE="${KANI_DIR}/tests/bench_verification"
+  prep_bench_files
+  # Need to use "perf" here, or this change won't be backward compatible.
+  SUITE="${KANI_DIR}/tests/perf"
+  rm "${SUITE}"
   ln -s "${RUN_DIR}" "${SUITE}"
+
+  # Delay execution errors since we need to cleanup.
+  set +o errexit
   run_benchmarks
-  exit_code=$?
-  cleanup_perf
+  local exit_code=$?
+  cleanup_artifacts "${SUITE}"
   print_result ${exit_code}
 }
