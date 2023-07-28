dd54c091d2c84abea720f512ea5aa8cf99211940
Adrian Palacios <accorell@amazon.com>
2023-07-28 18:30:00 +0000

    Comments in one test

--- tests/coverage/reachable/rem-zero/reachable_fail/test.rs
+++ tests/coverage/reachable/rem-zero/reachable_fail/test.rs
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 fn rem(x: u16, y: u16) -> u16 {
-    x % y
-}
+    x % y // PARTIAL: `x % y` is covered but induces a division failure
+} // NONE: Caused by division failure earlier
 
 #[kani::proof]
 fn main() {
     rem(678, 0);
-}
+} // NONE: Caused by division failure earlier
