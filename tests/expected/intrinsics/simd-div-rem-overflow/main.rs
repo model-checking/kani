e1a03360d2c33539ac71c4447abab29e8fd7fe42
Alex Reisner <1683937+reisnera@users.noreply.github.com>
2023-08-12 13:44:05 -0500

    Update tests based on review

--- tests/expected/intrinsics/simd-div-rem-overflow/main.rs
+++ tests/expected/intrinsics/simd-div-rem-overflow/main.rs
@@ -15,24 +15,30 @@ extern "platform-intrinsic" {
     fn simd_rem<T>(x: T, y: T) -> T;
 }
 
+unsafe fn do_simd_div(dividends: i32x2, divisors: i32x2) -> i32x2 {
+    simd_div(dividends, divisors)
+}
+
+unsafe fn do_simd_rem(dividends: i32x2, divisors: i32x2) -> i32x2 {
+    simd_rem(dividends, divisors)
+}
+
 #[kani::proof]
 fn test_simd_div_overflow() {
-    let dividend = kani::any();
-    kani::assume(dividend == i32::MIN);
+    let dividend = i32::MIN;
     let dividends = i32x2(dividend, dividend);
-    let divisor = kani::any();
-    kani::assume(divisor == -1);
+    let divisor = -1;
     let divisors = i32x2(divisor, divisor);
-    let _quotient = unsafe { simd_div(dividends, divisors) };
+    let quotients = unsafe { do_simd_div(dividends, divisors) };
+    assert_eq!(quotients.0, quotients.1);
 }
 
 #[kani::proof]
 fn test_simd_rem_overflow() {
-    let dividend = kani::any();
-    kani::assume(dividend == i32::MIN);
+    let dividend = i32::MIN;
     let dividends = i32x2(dividend, dividend);
-    let divisor = kani::any();
-    kani::assume(divisor == -1);
+    let divisor = -1;
     let divisors = i32x2(divisor, divisor);
-    let _remainder = unsafe { simd_rem(dividends, divisors) };
+    let remainders = unsafe { do_simd_rem(dividends, divisors) };
+    assert_eq!(remainders.0, remainders.1);
 }
--- tests/kani/Intrinsics/SIMD/Operators/remainder_float_fixme.rs
+++ tests/kani/Intrinsics/SIMD/Operators/remainder_float_fixme.rs
@@ -1,7 +1,7 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Checks that the `simd_div` intrinsic returns the expected results for floating point numbers.
+//! Checks that the `simd_rem` intrinsic returns the expected results for floating point numbers.
 #![feature(repr_simd, platform_intrinsics)]
 
 #[repr(simd)]
@@ -14,17 +14,17 @@ impl f32x2 {
         f32x2(f(), f())
     }
 
-    fn non_simd_div(self, divisors: Self) -> Self {
-        f32x2(self.0 / divisors.0, self.1 / divisors.1)
+    fn non_simd_rem(self, divisors: Self) -> Self {
+        f32x2(self.0 % divisors.0, self.1 % divisors.1)
     }
 }
 
 extern "platform-intrinsic" {
-    fn simd_div<T>(x: T, y: T) -> T;
+    fn simd_rem<T>(x: T, y: T) -> T;
 }
 
 #[kani::proof]
-fn test_simd_div() {
+fn test_simd_rem() {
     let dividends = f32x2::new_with(|| {
         let multiplier = kani::any_where(|&n: &i8| n >= -5 && n <= 5);
         0.5 * f32::from(multiplier)
@@ -33,7 +33,7 @@ fn test_simd_div() {
         let multiplier = kani::any_where(|&n: &i8| n != 0 && n >= -5 && n <= 5);
         0.5 * f32::from(multiplier)
     });
-    let normal_results = dividends.non_simd_div(divisors);
-    let simd_results = unsafe { simd_div(dividends, divisors) };
+    let normal_results = dividends.non_simd_rem(divisors);
+    let simd_results = unsafe { simd_rem(dividends, divisors) };
     assert_eq!(normal_results, simd_results);
 }
