6c87e4ef22b12a157cb66fee4f12f951402edde0
Adrian Palacios <accorell@amazon.com>
2023-09-12 19:26:07 +0000

    Fix `clippy`

--- kani-driver/src/concrete_playback/test_generator.rs
+++ kani-driver/src/concrete_playback/test_generator.rs
@@ -387,7 +387,7 @@ mod concrete_vals_extractor {
                     // convert the character into its ASCII value in that case.
                     let interp_val = {
                         let interp_val_str = interp_concrete_val.to_string();
-                        if interp_val_str.starts_with("\'") && interp_val_str.ends_with("\'") {
+                        if interp_val_str.starts_with('\'') && interp_val_str.ends_with('\'') {
                             let interp_num = interp_val_str.chars().nth(1).unwrap() as u8;
                             interp_num.to_string()
                         } else {
@@ -395,7 +395,7 @@ mod concrete_vals_extractor {
                         }
                     };
 
-                    return Some(ConcreteVal { byte_arr: next_num, interp_val: interp_val });
+                    return Some(ConcreteVal { byte_arr: next_num, interp_val });
                 }
             }
         }
