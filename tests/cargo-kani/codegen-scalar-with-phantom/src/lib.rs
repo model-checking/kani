fb8d5cb1db648d1d2204055637723d27e70f9fd7
Adrian Palacios <accorell@amazon.com>
2023-09-28 22:02:37 +0000

    Add a more general test for ZSTs

--- tests/cargo-kani/codegen-scalar-with-phantom/src/lib.rs
+++ tests/cargo-kani/codegen-scalar-with-phantom/src/lib.rs
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //! Check that we can codegen structs with scalar and phantom data.
+//!
+//! Note: Phantom data is represented with ZSTs, which are already covered by
+//! the test `codegen-scalar-with-zsts`, but we include this one as well for
+//! completeness.
 
 use std::marker::PhantomData;
 
@@ -11,7 +15,7 @@ pub struct Foo<R> {
 }
 
 #[kani::proof]
-fn main() {
+fn check_phantom_data() {
     const C: Foo<usize> = Foo { x: 0, _t: PhantomData };
     assert_eq!(C.x, 0);
 }
