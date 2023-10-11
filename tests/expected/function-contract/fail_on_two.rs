5ebfac50ebf5ccd3ae887af450d66e2cd00fd701
Justus Adam <dev@justus.science>
2023-10-10 20:58:40 -0400

    Fix import error, add fail on two test case

---
+++ tests/expected/function-contract/fail_on_two.rs
@@ -0,0 +1,19 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Zfunction-contracts
+
+
+#[kani::ensures(result < 3)]
+fn fail_on_two(i: i32) -> i32 {
+    match i {
+        0 => fail_on_two(i + 1),
+        1 => 2,
+        _ => unreachable!("fail on two"),
+    }
+}
+
+#[kani::proof_for_contract(fail_on_two)]
+fn harness() {
+    let first = fail_on_two(0);
+    let _ = fail_on_two(first);
+}
