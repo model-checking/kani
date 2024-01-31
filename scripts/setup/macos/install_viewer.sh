abfcd2e830a87aa07713da4e0f86749cac4bfd7b
Michael Tautschnig <tautschn@amazon.com>
2024-01-31 19:19:46 +0000

    Revert "Try to install pre-bottled cbmc-viewer"
    This reverts commit ab0a4d603ed4b774d273dc5c7d22983f2a77855d.

--- scripts/setup/macos/install_viewer.sh
+++ scripts/setup/macos/install_viewer.sh
@@ -16,5 +16,7 @@ fi
 
 set -x
 
-brew install aws/tap/cbmc-viewer
+# brew doesn't recognize specific versions of viewer
+# Build from source, since there's only a macos-12 bottle which doesn't seem to work.
+brew install -s aws/tap/cbmc-viewer
 echo "Installed: $(cbmc-viewer --version)"
