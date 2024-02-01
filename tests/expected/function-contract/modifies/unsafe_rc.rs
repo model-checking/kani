52e703a97caef2631749a2f751549fd32c573a67
Justus Adam <justbldr@amazon.com>
2024-01-31 22:56:50 -0500

    Forgot that this now needs formatting

--- tests/expected/function-contract/modifies/unsafe_rc.rs
+++ tests/expected/function-contract/modifies/unsafe_rc.rs
@@ -2,10 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // kani-flags: -Zfunction-contracts
 
-/// Illustrates the problem from https://github.com/model-checking/kani/issues/2907
-
-use std::rc::Rc;
 use std::ops::Deref;
+/// Illustrates the problem from https://github.com/model-checking/kani/issues/2907
+use std::rc::Rc;
 
 #[kani::modifies({
     let intref : &u32  = ptr.deref().deref();
@@ -19,7 +18,7 @@ fn modify(ptr: Rc<&mut u32>) {
 
 #[kani::proof_for_contract(modify)]
 fn main() {
-    let mut i : u32 = kani::any();
+    let mut i: u32 = kani::any();
     let ptr = Rc::new(&mut i);
     modify(ptr.clone());
 }
