0f75d2d21ac1899c4bd0b09a4d47f9bdc33a7c29
Michael Tautschnig <tautschn@amazon.com>
2024-02-06 21:17:32 +0000

    Test for powf32

---
+++ tests/kani/Intrinsics/Math/Arith/powf32.rs
@@ -0,0 +1,10 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#[kani::proof]
+fn verify_pow() {
+    let x: f32 = kani::any();
+    kani::assume(x.is_normal());
+    let x2 = x.powf(2.0);
+    assert!(x2 >= 0);
+}
