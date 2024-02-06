890dca594b51fdae060b733c661b8c94731118a5
Michael Tautschnig <tautschn@amazon.com>
2024-02-06 21:34:47 +0000

    Test exp

---
+++ tests/kani/Intrinsics/Math/Arith/exp.rs
@@ -0,0 +1,20 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#[kani::proof]
+fn verify_exp32() {
+    let two = 2.0_f32;
+    let two_sq = std::f32::consts::E * std::f32::consts::E;
+    let two_exp = two.exp();
+
+    assert!((two_sq - two_exp).abs() <= 1.0);
+}
+
+#[kani::proof]
+fn verify_exp64() {
+    let two = 2.0_f64;
+    let two_sq = std::f64::consts::E * std::f64::consts::E;
+    let two_exp = two.exp();
+
+    assert!((two_sq - two_exp).abs() <= 1.0);
+}
