a044577d2d017b358e126b7aa96cb43da5750da4
Michael Tautschnig <tautschn@amazon.com>
2024-02-06 21:42:04 +0000

    Syntax fixes

--- tests/kani/Intrinsics/Math/Arith/powf32.rs
+++ tests/kani/Intrinsics/Math/Arith/powf32.rs
@@ -6,5 +6,5 @@ fn verify_pow() {
     let x: f32 = kani::any();
     kani::assume(x.is_normal());
     let x2 = x.powf(2.0);
-    assert!(x2 >= 0);
+    assert!(x2 >= 0.0);
 }
--- tests/kani/Intrinsics/Math/Arith/powi.rs
+++ tests/kani/Intrinsics/Math/Arith/powi.rs
@@ -6,7 +6,7 @@ fn verify_powi32() {
     let x: f32 = kani::any();
     kani::assume(x.is_normal());
     let x2 = x.powi(2);
-    assert!(x2 >= 0);
+    assert!(x2 >= 0.0);
 }
 
 #[kani::proof]
@@ -14,5 +14,5 @@ fn verify_powi64() {
     let x: f64 = kani::any();
     kani::assume(x.is_normal());
     let x2 = x.powi(2);
-    assert!(x2 >= 0);
+    assert!(x2 >= 0.0);
 }
