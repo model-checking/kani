f08a3e99296f669055fa61c640ac916a0033fc31
Michael Tautschnig <tautschn@amazon.com>
2024-02-07 19:56:10 +0100

    Enable powf*, exp*, log* intrinsics (#2996)
    CBMC provides approximating implementations of these.
    
    Resolves: #2966

---
+++ tests/kani/Intrinsics/Math/Arith/log.rs
@@ -0,0 +1,22 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// This test will trigger use of the `logf32` and `logf64` intrinsics, which in turn invoke
+// functions modelled in CBMC's math library. These models use approximations as documented in
+// CBMC's source code: https://github.com/diffblue/cbmc/blob/develop/src/ansi-c/library/math.c.
+
+#[kani::proof]
+fn verify_logf32() {
+    let e = std::f32::consts::E;
+    let e_log = e.ln();
+
+    assert!((e_log - 1.0).abs() <= 0.058);
+}
+
+#[kani::proof]
+fn verify_logf64() {
+    let e = std::f64::consts::E;
+    let e_log = e.ln();
+
+    assert!((e_log - 1.0).abs() <= 0.058);
+}
