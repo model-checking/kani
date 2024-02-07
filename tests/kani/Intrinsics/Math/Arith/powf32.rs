9fed6e52ca181b5194806f5c1e7eecff79912f1c
Michael Tautschnig <tautschn@amazon.com>
2024-02-07 10:11:10 +0000

    Fix powf32

--- tests/kani/Intrinsics/Math/Arith/powf32.rs
+++ tests/kani/Intrinsics/Math/Arith/powf32.rs
@@ -5,6 +5,7 @@
 fn verify_pow() {
     let x: f32 = kani::any();
     kani::assume(x.is_normal());
+    kani::assume(x > 1.0 && x < u16::MAX.into());
     let x2 = x.powf(2.0);
     assert!(x2 >= 0.0);
 }
