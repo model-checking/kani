b60abc63f8240cf21abdfef5eda480384734daab
Michael Tautschnig <tautschn@amazon.com>
2024-02-07 09:57:05 +0000

    Log test

---
+++ tests/kani/Intrinsics/Math/Arith/log.rs
@@ -0,0 +1,18 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#[kani::proof]
+fn verify_logf32() {
+    let e = std::f32::consts::E;
+    let e_log = two.log();
+
+    assert!((e_log - 1.0).abs() <= 0.1);
+}
+
+#[kani::proof]
+fn verify_logf64() {
+    let e = std::f64::consts::E;
+    let e_log = two.log();
+
+    assert!((e_log - 1.0).abs() <= 0.1);
+}
