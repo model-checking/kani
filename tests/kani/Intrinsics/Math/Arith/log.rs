e0de5b583f1fa0c46734778d312f08a9e49a9650
Michael Tautschnig <tautschn@amazon.com>
2024-02-07 10:01:37 +0000

    Fixup

--- tests/kani/Intrinsics/Math/Arith/log.rs
+++ tests/kani/Intrinsics/Math/Arith/log.rs
@@ -4,7 +4,7 @@
 #[kani::proof]
 fn verify_logf32() {
     let e = std::f32::consts::E;
-    let e_log = two.log();
+    let e_log = e.ln();
 
     assert!((e_log - 1.0).abs() <= 0.1);
 }
@@ -12,7 +12,7 @@ fn verify_logf32() {
 #[kani::proof]
 fn verify_logf64() {
     let e = std::f64::consts::E;
-    let e_log = two.log();
+    let e_log = e.ln();
 
     assert!((e_log - 1.0).abs() <= 0.1);
 }
