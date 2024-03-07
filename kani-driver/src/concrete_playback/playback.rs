45caad49ab8f8cec9c3a5b695261908ffec63f8d
Jaisurya Nanduri <91620234+jaisnan@users.noreply.github.com>
2024-03-07 13:15:51 -0500

    Add `--use-local-toolchain` to Kani setup (#3056)
    Adds `--use-local-toolchain` to Kani's setup flow, which accepts a local
    toolchain and then uses that to finish the Kani setup.
    
    Some notes:
    
    1. Why? This is mainly for installing GPG verified toolchains. 
    2. This is missing some cleanup and refactoring work, like ensuring that
    the user defined toolchain matches the one that Kani was built against
    etc. Marked as Todo, for later.
    
    Resolves [#3058](https://github.com/model-checking/kani/issues/3058)
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

--- kani-driver/src/concrete_playback/playback.rs
+++ kani-driver/src/concrete_playback/playback.rs
@@ -7,7 +7,7 @@ use crate::args::common::Verbosity;
 use crate::args::playback_args::{CargoPlaybackArgs, KaniPlaybackArgs, MessageFormat};
 use crate::call_cargo::cargo_config_args;
 use crate::call_single_file::base_rustc_flags;
-use crate::session::{lib_playback_folder, InstallType};
+use crate::session::{lib_playback_folder, setup_cargo_command, InstallType};
 use crate::{session, util};
 use anyhow::Result;
 use std::ffi::OsString;
@@ -17,8 +17,7 @@ use std::process::Command;
 use tracing::debug;
 
 pub fn playback_cargo(args: CargoPlaybackArgs) -> Result<()> {
-    let install = InstallType::new()?;
-    cargo_test(&install, args)
+    cargo_test(args)
 }
 
 pub fn playback_standalone(args: KaniPlaybackArgs) -> Result<()> {
@@ -93,9 +92,10 @@ fn build_test(install: &InstallType, args: &KaniPlaybackArgs) -> Result<PathBuf>
 }
 
 /// Invokes cargo test using Kani compiler and the provided arguments.
-/// TODO: This should likely be inside KaniSession, but KaniSession requires `VerificationArgs` today.
-/// For now, we just use InstallType directly.
-fn cargo_test(install: &InstallType, args: CargoPlaybackArgs) -> Result<()> {
+fn cargo_test(args: CargoPlaybackArgs) -> Result<()> {
+    let install = InstallType::new()?;
+    let mut cmd = setup_cargo_command()?;
+
     let rustc_args = base_rustc_flags(lib_playback_folder()?);
     let mut cargo_args: Vec<OsString> = vec!["test".into()];
 
@@ -123,9 +123,7 @@ fn cargo_test(install: &InstallType, args: CargoPlaybackArgs) -> Result<()> {
     }
 
     // Arguments that will only be passed to the target package.
-    let mut cmd = Command::new("cargo");
-    cmd.arg(session::toolchain_shorthand())
-        .args(&cargo_args)
+    cmd.args(&cargo_args)
         .env("RUSTC", &install.kani_compiler()?)
         // Use CARGO_ENCODED_RUSTFLAGS instead of RUSTFLAGS is preferred. See
         // https://doc.rust-lang.org/cargo/reference/environment-variables.html
--- kani-driver/src/session.rs
+++ kani-driver/src/session.rs
@@ -379,3 +379,25 @@ fn init_logger(args: &VerificationArgs) {
     );
     tracing::subscriber::set_global_default(subscriber).unwrap();
 }
+
+// Setup the default version of cargo being run, based on the type/mode of installation for kani
+// If kani is being run in developer mode, then we use the one provided by rustup as we can assume that the developer will have rustup installed
+// For release versions of Kani, we use a version of cargo that's in the toolchain that's been symlinked during `cargo-kani` setup. This will allow
+// Kani to remove the runtime dependency on rustup later on.
+pub fn setup_cargo_command() -> Result<Command> {
+    let install_type = InstallType::new()?;
+
+    let cmd = match install_type {
+        InstallType::DevRepo(_) => {
+            let mut cmd = Command::new("cargo");
+            cmd.arg(self::toolchain_shorthand());
+            cmd
+        }
+        InstallType::Release(kani_dir) => {
+            let cargo_path = kani_dir.join("toolchain").join("bin").join("cargo");
+            Command::new(cargo_path)
+        }
+    };
+
+    Ok(cmd)
+}
