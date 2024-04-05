d4abc03ed9219b4bbc97bf75440d59e06cb4d11a
Michael Tautschnig <tautschn@amazon.com>
2024-04-05 09:55:43 +0000

    Add test for typed_swap

---
+++ tests/kani/Intrinsics/typed_swap.rs
@@ -0,0 +1,19 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// Check that `typed_swap` yields the expected results.
+// https://doc.rust-lang.org/nightly/std/intrinsics/fn.typed_swap.html
+
+#![feature(core_intrinsics)]
+#![allow(internal_features)]
+
+#[kani::proof]
+fn test_typed_swap_u32() {
+    let mut a: u32 = kani::any();
+    let a_before = a;
+    let mut b: u32 = kani::any();
+    let b_before = b;
+    unsafe { std::intrinsics::typed_swap(&mut a, &mut b); }
+    assert!(b == a_before);
+    assert!(a == b_before);
+}
