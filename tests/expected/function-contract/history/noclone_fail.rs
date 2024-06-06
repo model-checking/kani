ed9bb8b46d542a2f503cf44945cce08977b1e471
Matias Scharager <mscharag@amazon.com>
2024-06-06 15:08:37 -0400

    interesting test

---
+++ tests/expected/function-contract/history/noclone_fail.rs
@@ -0,0 +1,24 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Zfunction-contracts
+
+struct NoCopy<T>(T);
+
+impl<T: kani::Arbitrary> kani::Arbitrary for NoCopy<T> {
+    fn any() -> Self {
+        Self(kani::any())
+    }
+}
+
+#[kani::ensures(|result| old(ptr).0 + 1 == ptr.0)]
+#[kani::requires(ptr.0 < 100)]
+#[kani::modifies(&mut ptr.0)]
+fn modify(ptr: &mut NoCopy<u32>) {
+    ptr.0 += 1;
+}
+
+#[kani::proof_for_contract(modify)]
+fn main() {
+    let mut i = kani::any();
+    modify(&mut i);
+}
