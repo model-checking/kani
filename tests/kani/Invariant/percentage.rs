e1915feb3d8a6c0d6580c308cb29b10992bd623c
Adrian Palacios <accorell@amazon.com>
2024-06-06 18:13:30 +0000

    Extend `percentage` test

--- tests/kani/Invariant/percentage.rs
+++ tests/kani/Invariant/percentage.rs
@@ -12,6 +12,25 @@ use kani::Invariant;
 #[derive(kani::Arbitrary)]
 struct Percentage(u8);
 
+impl Percentage {
+    pub fn try_new(val: u8) -> Result<Self, String> {
+        if val <= 100 {
+            Ok(Self(val))
+        } else {
+            Err(String::from("error: invalid percentage value"))
+        }
+    }
+
+    pub fn value(&self) -> u8 {
+        self.0
+    }
+
+    pub fn increase(&self, other: u8) -> Percentage {
+        let amount = self.0 + other;
+        Percentage::try_new(amount.min(100)).unwrap()
+    }
+}
+
 impl kani::Invariant for Percentage {
     fn is_safe(&self) -> bool {
         self.0 <= 100
@@ -22,7 +41,7 @@ impl kani::Invariant for Percentage {
 fn check_assume_safe() {
     let percentage: Percentage = kani::any();
     kani::assume(percentage.is_safe());
-    assert!(percentage.0 <= 100);
+    assert!(percentage.value() <= 100);
 }
 
 #[kani::proof]
@@ -30,4 +49,14 @@ fn check_assume_safe() {
 fn check_assert_safe() {
     let percentage: Percentage = kani::any();
     assert!(percentage.is_safe());
+}
+
+#[kani::proof]
+fn check_increase_safe() {
+    let percentage: Percentage = kani::any();
+    kani::assume(percentage.is_safe());
+    let amount = kani::any();
+    kani::assume(amount <= 100);
+    let new_percentage = percentage.increase(amount);
+    assert!(new_percentage.is_safe());
 }
