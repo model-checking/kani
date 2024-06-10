60572fcbffadd0cdfbec62dd05decb6ec0252e15
Michael Tautschnig <tautschn@amazon.com>
2024-06-10 20:49:08 +0000

    Expand test

--- tests/kani/FatPointers/metadata.rs
+++ tests/kani/FatPointers/metadata.rs
@@ -3,7 +3,22 @@
 
 #![feature(ptr_metadata)]
 
+struct S {
+    x: i32,
+}
+
+trait T {}
+
+impl T for S {}
+
 #[kani::proof]
 fn ptr_metadata() {
     assert_eq!(std::ptr::metadata("foo"), 3_usize);
+
+    let s = S { x: 42 };
+    let p: &dyn T = &s;
+    assert_eq!(std::ptr::metadata(p).size_of(), 4_usize);
+
+    let c: char = 'c';
+    assert_eq!(std::ptr::metadata(&c), ());
 }
