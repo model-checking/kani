bb6aed8d429bca06fa81882667ef8df5829bf1a9
Matias Scharager <mscharag@amazon.com>
2024-06-11 11:29:31 -0400

    format

--- tests/expected/function-contract/history/side_effect.rs
+++ tests/expected/function-contract/history/side_effect.rs
@@ -6,7 +6,7 @@
 #[kani::requires(*ptr < 100)]
 #[kani::requires(*ptr == _val)]
 #[kani::modifies(ptr)]
-fn modify(ptr: &mut u32, _val : u32) {
+fn modify(ptr: &mut u32, _val: u32) {
     *ptr += 1;
 }
 
