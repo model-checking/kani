35b41fc150cd08ba1c4f1e8fb7cac7157c1ddfd8
Adrian Palacios <accorell@amazon.com>
2024-06-17 20:45:58 +0000

    Fix comments in all test cases

--- tests/ui/attr-invariant/invalid-pred-error/invalid-pred-error.rs
+++ tests/ui/attr-invariant/invalid-pred-error/invalid-pred-error.rs
@@ -1,11 +1,14 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Check that Kani can automatically derive `Invariant` for structs with named fields.
+//! Check that there is a compilation error when the predicate passed to
+//! `kani::invariant` attribute would result in a compiler error.
 
 extern crate kani;
 use kani::Invariant;
 
+// Note: The `x.is_safe() && y.is_safe()` requires `self` before each struct
+// field to be evaluated in the `is_safe` function body.
 #[derive(kani::Arbitrary)]
 #[kani::invariant(x.is_safe() && y.is_safe())]
 struct Point {
