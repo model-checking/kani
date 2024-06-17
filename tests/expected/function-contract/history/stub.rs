801f0dffd078ababcf2f0bcabd001d40fbcfb169
Matias Scharager <mscharag@amazon.com>
2024-06-17 15:04:02 -0400

    stub_verified

--- tests/expected/function-contract/history/stub.rs
+++ tests/expected/function-contract/history/stub.rs
@@ -2,25 +2,30 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // kani-flags: -Zfunction-contracts
 
-fn double1(ptr: &mut u32) {
-    *ptr += *ptr;
-}
-
-fn double2(ptr: &mut u32) {
-    *ptr = *ptr + *ptr;
-}
-
 #[kani::ensures(|result| old(*ptr + *ptr) == *ptr)]
 #[kani::requires(*ptr < 100)]
 #[kani::modifies(ptr)]
-fn modify(ptr: &mut u32) {
-    double2(ptr)
+fn double(ptr: &mut u32) {
+    *ptr += *ptr;
 }
 
-/// This tests using `stub` within a test that uses `old`
-#[kani::proof_for_contract(modify)]
-#[kani::stub(double2, double1)]
-fn main() {
+#[kani::proof_for_contract(double)]
+fn double_harness() {
     let mut i = kani::any();
-    modify(&mut i);
+    double(&mut i);
+}
+
+#[kani::ensures(|result| old(*ptr + *ptr + *ptr + *ptr) == *ptr)]
+#[kani::requires(*ptr < 100)]
+#[kani::modifies(ptr)]
+#[kani::stub_verified(double)]
+fn quadruple(ptr: &mut u32) {
+    double(ptr);
+    double(ptr)
+}
+
+#[kani::proof_for_contract(quadruple)]
+fn quadruple_harness() {
+    let mut i = kani::any();
+    quadruple(&mut i);
 }
