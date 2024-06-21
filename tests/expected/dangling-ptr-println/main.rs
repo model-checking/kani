4be38b9e1fc0e755937063d0b106c61df4b90a77
Artem Agvanian <nartagva@amazon.com>
2024-06-21 09:19:08 -0700

    Add tests checking if Kani detects dangling pointer dereference inside println macro (#3235).

---
+++ tests/expected/dangling-ptr-println/main.rs
@@ -0,0 +1,30 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Z ptr-to-ref-cast-checks
+
+// These tests check that Kani correctly detects dangling pointer dereference inside println macro.
+
+fn reference_dies() -> *mut i32 {
+    let mut x: i32 = 2;
+    &mut x as *mut i32
+}
+
+#[kani::proof]
+fn local_unsafe() {
+    let x = reference_dies();
+    println!("My pointer, {}", unsafe { *x });
+}
+
+#[kani::proof]
+unsafe fn general_unsafe() {
+    let x = reference_dies();
+    println!("My pointer, {}", *x);
+}
+
+#[kani::proof]
+fn unsafe_block() {
+    let x = reference_dies();
+    unsafe {
+        println!("{}", *x);
+    }
+}
