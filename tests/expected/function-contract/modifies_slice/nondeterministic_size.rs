e04f299eebc98817bc344a6608f03ae0cd4d0ec7
Matias Scharager <mscharag@amazon.com>
2024-06-24 16:21:53 -0400

    nondeterminstics_size test

---
+++ tests/expected/function-contract/modifies_slice/nondeterministic_size.rs
@@ -0,0 +1,17 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Zfunction-contracts
+
+// Test that modifies a slice of nondeterministic size
+
+#[kani::modifies_slice(x)]
+#[kani::ensures(|_| x.into_iter().map(|v| *v == 0).fold(true,|a,b|a&b))]
+fn zero(x: &mut [u8]) {
+    x.fill(0)
+}
+
+#[kani::proof_for_contract(zero)]
+fn main() {
+    let mut x = [0..kani::any()].map(|_|kani::any());
+    zero(&mut x);
+}
