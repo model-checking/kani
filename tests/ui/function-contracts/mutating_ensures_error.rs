34359f550a52ce4132910f4357d92a8b335d008f
Matias Scharager <mscharag@amazon.com>
2024-06-28 17:24:55 -0400

    forgot to copy paste test in

--- tests/ui/function-contracts/mutating_ensures_error.rs
+++ tests/ui/function-contracts/mutating_ensures_error.rs
@@ -3,12 +3,10 @@
 // kani-flags: -Zfunction-contracts
 
 #[kani::ensures(|_| {*_x += 1; true})]
-fn unit(_x: &mut u32) {
-}
+fn unit(_x: &mut u32) {}
 
 #[kani::proof_for_contract(id)]
 fn harness() {
     let mut x = kani::any();
     unit(&mut x);
 }
-
