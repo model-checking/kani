84f0e27bbcde53e4fb00caf5172fbb4c442dc660
Celina G. Val <celinval@amazon.com>
2024-06-28 16:41:04 -0700

    Update tests/cargo-ui/stubbing-flag/src/main.rs
    Co-authored-by: Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>

--- tests/cargo-ui/stubbing-flag/src/main.rs
+++ tests/cargo-ui/stubbing-flag/src/main.rs
@@ -1,7 +1,7 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-//! This tests that the --enabling stubbing and using `--harness` arguments flow from
+//! This tests that enabling stubbing and using `--harness` arguments flow from
 //! `kani-driver` to `kani-compiler`.
 
 #[kani::proof]
