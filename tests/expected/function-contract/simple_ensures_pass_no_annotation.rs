6f0c0b5b97c6861a3113af85c5d94bc17ee3e306
Matias Scharager <matias.scharager@gmail.com>
2024-07-01 12:39:32 -0400

    Function Contracts: Closure Type Inference (#3307)
    The rust type inference for closures doesn't work in the particular use
    case we are using it for ensures clauses. By creating a helper function,
    we change the path the rust type inference takes and lets the type of
    the closure be identified properly. This means type annotations are no
    longer required within ensures clauses.
    
    Resolves #3304
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

---
+++ tests/expected/function-contract/simple_ensures_pass_no_annotation.rs
@@ -0,0 +1,14 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Zfunction-contracts
+
+#[kani::ensures(|result| (*result == x) | (*result == y))]
+fn max(x: u32, y: u32) -> u32 {
+    if x > y { x } else { y }
+}
+
+#[kani::proof_for_contract(max)]
+fn max_harness() {
+    let _ = Box::new(9_usize);
+    max(7, 6);
+}
