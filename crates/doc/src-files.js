325d5c8cf9c27f3b398801f03143df52d0efb4fb
tautschnig <mt@debian.org>
2024-07-02 09:30:18 +0000

    Deploying to gh-pages from @ model-checking/kani@2fb97d28b457bf7eb86086ea44a3c5db23e41e38 🚀

---
+++ crates/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["kani",["",[["models",[],["mod.rs"]]],["arbitrary.rs","contracts.rs","futures.rs","internal.rs","invariant.rs","lib.rs","mem.rs","shadow.rs","slice.rs","tuple.rs","vec.rs"]]]\
+]'));
+createSrcSidebar();
