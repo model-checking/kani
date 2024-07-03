f6d33854ca95e25cbd52d52f3ca842b7eb07793f
Celina G. Val <celinval@amazon.com>
2024-07-02 22:44:22 -0700

    Remove deprecated `--enable-stubbing` (#3309)
    Part of #2279 
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.
    
    ---------
    
    Co-authored-by: Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>

--- tests/expected/function-stubbing-no-harness/main.rs
+++ tests/expected/function-stubbing-no-harness/main.rs
@@ -1,7 +1,7 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-// kani-flags: --harness foo --enable-unstable --enable-stubbing
+// kani-flags: --harness foo -Z stubbing
 //
 //! This tests whether we detect missing harnesses during stubbing.
 
