098d8789e76ac9e1457cad6c3575681ade4b8dc5
Artem Agvanian <nartagva@amazon.com>
2024-07-08 14:36:38 -0700

    Format code

--- tests/expected/uninit/delayed-ub/delayed-ub.rs
+++ tests/expected/uninit/delayed-ub/delayed-ub.rs
@@ -4,7 +4,7 @@ fn invalid_value() {
     unsafe {
         let mut value: u128 = 0;
         let ptr = &mut value as *mut _ as *mut (u8, u32, u64);
-        *ptr = (4, 4, 4);   // This assignment itself does not cause UB...
+        *ptr = (4, 4, 4); // This assignment itself does not cause UB...
         let c: u128 = value; // ...but this reads a padding value! ⚠️
     }
 }
