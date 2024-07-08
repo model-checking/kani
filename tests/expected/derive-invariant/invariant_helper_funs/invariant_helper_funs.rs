09e3ee53e853fad7c43b4305076977f1958b3b13
Adrian Palacios <accorell@amazon.com>
2024-07-08 21:40:32 +0000

    Add test with functions

---
+++ tests/expected/derive-invariant/invariant_helper_funs/invariant_helper_funs.rs
@@ -0,0 +1,47 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Check that functions can be called in the invariant attribute helpers.
+//! This is like the `invariant_helper` test but using a function instead
+//! of passing in a predicate.
+
+extern crate kani;
+use kani::Invariant;
+
+#[derive(kani::Arbitrary)]
+#[derive(kani::Invariant)]
+struct PositivePoint {
+    #[invariant(is_coordinate_safe(x))]
+    x: i32,
+    #[invariant(is_coordinate_safe(y))]
+    y: i32,
+}
+
+fn is_coordinate_safe(val: &i32) -> bool {
+    *val >= 0
+}
+
+#[kani::proof]
+fn check_invariant_helper_ok() {
+    let pos_point: PositivePoint = kani::any();
+    assert!(pos_point.is_safe());
+}
+
+#[kani::proof]
+#[kani::should_panic]
+fn check_invariant_helper_fail() {
+    // In this case, `kani::any()` returns unconstrained values that don't
+    // respect the invariants.
+    let pos_point: PositivePoint = PositivePoint { x: kani::any(), y: kani::any() };
+    assert!(pos_point.is_safe());
+}
+
+#[kani::proof]
+fn check_invariant_helper_ok_manual() {
+    // In this case, `kani::any()` returns unconstrained values that don't
+    // respect the invariants. However, we manually constrain them later.
+    let pos_point: PositivePoint = PositivePoint { x: kani::any(), y: kani::any() };
+    kani::assume(pos_point.x >= 0);
+    kani::assume(pos_point.y >= 0);
+    assert!(pos_point.is_safe());
+}
