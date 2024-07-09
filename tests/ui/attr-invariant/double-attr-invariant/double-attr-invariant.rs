631b103d99278c8021c525a6f49abc3aa3f6d48a
Adrian Palacios <accorell@amazon.com>
2024-07-09 21:08:14 +0000

    Add double-attr-invariant test

---
+++ tests/ui/attr-invariant/double-attr-invariant/double-attr-invariant.rs
@@ -0,0 +1,17 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Check that there is a compilation error when the predicate passed to
+//! `kani::invariant` attribute would result in a compiler error.
+
+extern crate kani;
+use kani::Invariant;
+
+// Note: The struct fields `x` and `y` are references in this context, we should
+// refer to `*x` and `*y` instead.
+#[kani::invariant(*x >= 0)]
+#[kani::invariant(*y >= 0)]
+struct Point {
+    x: i32,
+    y: i32,
+}
