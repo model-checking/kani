7bd56347a4c9b3535b3debd6cc4a7e46c677089c
Matias Scharager <mscharag@amazon.com>
2024-07-16 15:45:13 -0400

    new test

--- tests/expected/function-contract/modifies_fat_pointer/slice_of_array.rs
+++ tests/expected/function-contract/modifies_fat_pointer/slice_of_array.rs
@@ -4,7 +4,7 @@
 
 // Test that modifies a slice containing u32 size data
 
-#[kani::modifies(x[0..3])]
+#[kani::modifies(&x[0..3])]
 #[kani::ensures(|_| x[0..3].iter().map(|v| *v == 0).fold(true,|a,b|a&b))]
 fn zero(x: &mut [u32; 6]) {
     x[0..3].fill(0)
@@ -12,6 +12,6 @@ fn zero(x: &mut [u32; 6]) {
 
 #[kani::proof_for_contract(zero)]
 fn harness() {
-    let mut x = [kani::any(), kani::any(), kani::any()];
+    let mut x = [kani::any(), kani::any(), kani::any(), kani::any(), kani::any(), kani::any()];
     zero(&mut x);
 }
