35a9320307758919849122ca006046bbd41359d7
Matias Scharager <mscharag@amazon.com>
2024-07-17 19:27:50 -0400

    preserves memory

--- tests/expected/function-contract/interior-mutability/unsafecell.rs
+++ tests/expected/function-contract/interior-mutability/unsafecell.rs
@@ -2,45 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // kani-flags: -Zfunction-contracts
 
-use std::cell::{Cell, RefCell, UnsafeCell};
+use std::cell::{Cell, UnsafeCell};
 use std::mem::transmute;
-use std::panic;
 
-#[allow(dead_code)]
-pub struct UnsafeCellHack<T: ?Sized> {
-    value: T,
-}
-
-#[allow(dead_code)]
-pub struct CellHack<T: ?Sized> {
-    value: UnsafeCellHack<T>,
-}
-
-#[allow(dead_code)]
-pub struct RefCellHack<T: ?Sized> {
-    borrow: CellHack<isize>,
-    borrowed_at: CellHack<Option<&'static crate::panic::Location<'static>>>,
-    value: UnsafeCellHack<T>,
-}
-
-trait ToHack<T> {
+trait ToHack<T : ?Sized> {
     unsafe fn hack(&self) -> &T;
 }
 
-impl<T> ToHack<UnsafeCellHack<T>> for UnsafeCell<T> {
-    unsafe fn hack(&self) -> &UnsafeCellHack<T> {
+impl<T : ?Sized> ToHack<T> for UnsafeCell<T> {
+    unsafe fn hack(&self) -> &T {
         transmute(self)
     }
 }
 
-impl<T> ToHack<CellHack<T>> for Cell<T> {
-    unsafe fn hack(&self) -> &CellHack<T> {
-        transmute(self)
-    }
-}
-
-impl<T> ToHack<RefCellHack<T>> for RefCell<T> {
-    unsafe fn hack(&self) -> &RefCellHack<T> {
+impl<T : ?Sized> ToHack<T> for Cell<T> {
+    unsafe fn hack(&self) -> &T {
         transmute(self)
     }
 }
@@ -51,9 +27,9 @@ struct InteriorMutability {
     x: UnsafeCell<u32>,
 }
 
-#[kani::requires(unsafe{x.x.hack()}.value < 100)]
-#[kani::modifies(&x.x.hack().value)]
-#[kani::ensures(|_| unsafe{x.x.hack()}.value < 101)]
+#[kani::requires(*unsafe{x.x.hack()} < 100)]
+#[kani::modifies(x.x.hack())]
+#[kani::ensures(|_| *unsafe{x.x.hack()} < 101)]
 fn modify(x: &InteriorMutability) {
     unsafe { *x.x.get() += 1 }
 }
