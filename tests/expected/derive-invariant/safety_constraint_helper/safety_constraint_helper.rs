477d9bf33c9f89bb635753e3a6bfd9d4702f8b81
Adrian Palacios <accorell@amazon.com>
2024-07-22 18:50:13 +0000

    Update comments so they're clear.

--- tests/expected/derive-invariant/safety_constraint_helper/safety_constraint_helper.rs
+++ tests/expected/derive-invariant/safety_constraint_helper/safety_constraint_helper.rs
@@ -25,16 +25,17 @@ fn check_invariant_helper_ok() {
 #[kani::proof]
 #[kani::should_panic]
 fn check_invariant_helper_fail() {
-    // In this case, `kani::any()` returns unconstrained values that don't
-    // respect the invariants.
+    // In this case, we build the struct from unconstrained arbitrary values
+    // that do not respect `PositivePoint`'s safety constraints.
     let pos_point: PositivePoint = PositivePoint { x: kani::any(), y: kani::any() };
     assert!(pos_point.is_safe());
 }
 
 #[kani::proof]
 fn check_invariant_helper_ok_manual() {
-    // In this case, `kani::any()` returns unconstrained values that don't
-    // respect the invariants. However, we manually constrain them later.
+    // In this case, we build the struct from unconstrained arbitrary values
+    // that do not respect `PositivePoint`'s safety constraints. However, we
+    // manually constrain them later.
     let pos_point: PositivePoint = PositivePoint { x: kani::any(), y: kani::any() };
     kani::assume(pos_point.x >= 0);
     kani::assume(pos_point.y >= 0);
