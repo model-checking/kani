dddc5568460e67070c95b0c0155205efb17db914
Adrian Palacios <accorell@amazon.com>
2024-07-26 20:02:30 +0000

    Fix attribute tests (still need to be moved)

--- tests/expected/attr-invariant/check_arbitrary/check_arbitrary.rs
+++ tests/expected/attr-invariant/check_arbitrary/check_arbitrary.rs
@@ -1,14 +1,16 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Check that the `kani::invariant` attribute automatically generates the
-//! `Arbitrary` implementation, and also that the values it generates respect
-//! the type invariant.
+//! Check that the `#[safety_constraint(...)]` attribute automatically generates
+//! the `Arbitrary` implementation, and also that the values it generates
+//! respect the type invariant.
 
 extern crate kani;
 use kani::Invariant;
 
-#[kani::invariant(*x >= 0 && *y >= 0)]
+#[derive(kani::Arbitrary)]
+#[derive(kani::Invariant)]
+#[safety_constraint(*x >= 0 && *y >= 0)]
 struct Point {
     x: i32,
     y: i32,
--- tests/expected/attr-invariant/check_invariant/check_invariant.rs
+++ tests/expected/attr-invariant/check_invariant/check_invariant.rs
@@ -1,13 +1,15 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Check that the `kani::invariant` attribute automatically generates the
-//! `Arbitrary` and `Invariant` implementations for the annotated type.
+//! Check that the `#[safety_constraint(...)]` attribute automatically generates
+//! the `Arbitrary` and `Invariant` implementations for the annotated type.
 
 extern crate kani;
 use kani::Invariant;
 
-#[kani::invariant(x.is_safe() && y.is_safe())]
+#[derive(kani::Arbitrary)]
+#[derive(kani::Invariant)]
+#[safety_constraint(true)]
 struct Point {
     x: i32,
     y: i32,
