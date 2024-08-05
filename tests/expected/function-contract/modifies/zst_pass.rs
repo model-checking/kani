b04a47abe126570e1cca91d5a82fd6332b55fe14
Carolyn Zech <cmzech@amazon.com>
2024-08-05 09:26:56 -0400

    filter out zsts for cmbc assigns for contracts

--- tests/expected/function-contract/modifies/zst_pass.rs
+++ tests/expected/function-contract/modifies/zst_pass.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // kani-flags: -Zfunction-contracts
 
-// #[kani::requires(assert_valid_ptr(dst) && has_valid_value(dst))]
-#[kani::requires(true)]
 #[kani::modifies(dst)]
 pub unsafe fn replace<T>(dst: *mut T, src: T) -> T {
     std::ptr::replace(dst, src)
