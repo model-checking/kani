9857cc8724fc51dc19e2511a29954875b8c0167c
Carolyn Zech <cmzech@amazon.com>
2024-08-06 18:50:37 -0400

    add test case similar to bolero issue

--- tests/expected/cover-or-fail/cover-unreachable-outside-harness-fail/main.rs
+++ tests/expected/cover-or-fail/cover-unreachable-outside-harness-fail/main.rs
@@ -15,3 +15,17 @@ fn proof() {
     // but the kani::cover_or_fail!() call *may or may not be* reachable, verification fails
     my_function(false);
 }
+
+// Simplified version of the .filter(|_| false).for_each(|_| {} pattern 
+// in https://github.com/model-checking/kani/issues/2792
+fn test() {
+    let my_vec: Vec<u8> = vec![1, 2, 3];
+    my_vec.into_iter().filter(|_| false).for_each(|_| {
+        kani::cover_or_fail!();
+    });
+}
+
+#[kani::proof]
+fn bolero_use_case() {
+    test();
+}
