3fb3a73d6d563e92eb447b32ec963ea77853ec00
Artem Agvanian <nartagva@amazon.com>
2024-08-07 13:34:46 -0700

    Stabilize pointer-to-reference cast validity checks (#3426)
    This PR stabilizes pointer-to-reference cast validity checks, so that
    they are run by default.
    
    Resolves #3425 
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

--- kani_metadata/src/unstable.rs
+++ kani_metadata/src/unstable.rs
@@ -89,8 +89,6 @@ pub enum UnstableFeature {
     ValidValueChecks,
     /// Ghost state and shadow memory APIs.
     GhostState,
-    /// Automatically check that pointers are valid when casting them to references.
-    PtrToRefCastChecks,
     /// Automatically check that uninitialized memory is not used.
     UninitChecks,
     /// Enable an unstable option or subcommand.
