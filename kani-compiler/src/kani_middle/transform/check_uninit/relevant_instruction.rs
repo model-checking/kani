b909c8be6de7a3afc7473bd20edbc3f1f82aeab8
Artem Agvanian <nartagva@amazon.com>
2024-08-14 08:48:29 -0700

    Avoid corner-cases by grouping instrumentation into basic blocks

--- kani-compiler/src/kani_middle/transform/check_uninit/relevant_instruction.rs
+++ kani-compiler/src/kani_middle/transform/check_uninit/relevant_instruction.rs
@@ -5,7 +5,10 @@
 //! character of instrumentation needed.
 
 use crate::kani_middle::transform::body::{InsertPosition, MutableBody, SourceInstruction};
-use stable_mir::mir::{Mutability, Operand, Place, Rvalue};
+use stable_mir::{
+    mir::{Mutability, Operand, Place, Rvalue, Statement, StatementKind},
+    ty::Ty,
+};
 use strum_macros::AsRefStr;
 
 /// Memory initialization operations: set or get memory initialization state for a given pointer.
@@ -39,27 +42,57 @@ impl MemoryInitOp {
     /// Produce an operand for the relevant memory initialization related operation. This is mostly
     /// required so that the analysis can create a new local to take a reference in
     /// `MemoryInitOp::SetRef`.
-    pub fn mk_operand(&self, body: &mut MutableBody, source: &mut SourceInstruction) -> Operand {
+    pub fn mk_operand(
+        &self,
+        body: &mut MutableBody,
+        statements: &mut Vec<Statement>,
+        source: &mut SourceInstruction,
+    ) -> Operand {
         match self {
             MemoryInitOp::Check { operand, .. }
             | MemoryInitOp::Set { operand, .. }
             | MemoryInitOp::CheckSliceChunk { operand, .. }
             | MemoryInitOp::SetSliceChunk { operand, .. } => operand.clone(),
             MemoryInitOp::CheckRef { operand, .. } | MemoryInitOp::SetRef { operand, .. } => {
-                Operand::Copy(Place {
-                    local: {
-                        let place = match operand {
-                            Operand::Copy(place) | Operand::Move(place) => place,
-                            Operand::Constant(_) => unreachable!(),
-                        };
-                        body.insert_assignment(
-                            Rvalue::AddressOf(Mutability::Not, place.clone()),
-                            source,
-                            self.position(),
-                        )
-                    },
-                    projection: vec![],
-                })
+                let span = source.span(body.blocks());
+
+                let ref_local = {
+                    let place = match operand {
+                        Operand::Copy(place) | Operand::Move(place) => place,
+                        Operand::Constant(_) => unreachable!(),
+                    };
+                    let rvalue = Rvalue::AddressOf(Mutability::Not, place.clone());
+                    let ret_ty = rvalue.ty(body.locals()).unwrap();
+                    let result = body.new_local(ret_ty, span, Mutability::Not);
+                    let stmt = Statement {
+                        kind: StatementKind::Assign(Place::from(result), rvalue),
+                        span,
+                    };
+                    statements.push(stmt);
+                    result
+                };
+
+                Operand::Copy(Place { local: ref_local, projection: vec![] })
+            }
+            MemoryInitOp::Unsupported { .. } | MemoryInitOp::TriviallyUnsafe { .. } => {
+                unreachable!()
+            }
+        }
+    }
+
+    pub fn operand_ty(&self, body: &MutableBody) -> Ty {
+        match self {
+            MemoryInitOp::Check { operand, .. }
+            | MemoryInitOp::Set { operand, .. }
+            | MemoryInitOp::CheckSliceChunk { operand, .. }
+            | MemoryInitOp::SetSliceChunk { operand, .. } => operand.ty(body.locals()).unwrap(),
+            MemoryInitOp::SetRef { operand, .. } | MemoryInitOp::CheckRef { operand, .. } => {
+                let place = match operand {
+                    Operand::Copy(place) | Operand::Move(place) => place,
+                    Operand::Constant(_) => unreachable!(),
+                };
+                let rvalue = Rvalue::AddressOf(Mutability::Not, place.clone());
+                rvalue.ty(body.locals()).unwrap()
             }
             MemoryInitOp::Unsupported { .. } | MemoryInitOp::TriviallyUnsafe { .. } => {
                 unreachable!()
