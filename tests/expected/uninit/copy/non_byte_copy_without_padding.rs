e6f8a62d689d0b0ebcbabe3661be6273c5ab9be8
Artem Agvanian <nartagva@amazon.com>
2024-08-15 10:31:09 -0700

    Implement memory initialization state copy functionality  (#3350)
    This PR adds support of copying memory initialization state without
    checks in-between. Every time a copy is performed, the tracked byte is
    non-deterministically switched.
    
    Resolves #3347
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

---
+++ tests/expected/uninit/copy/non_byte_copy_without_padding.rs
@@ -0,0 +1,23 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+// kani-flags: -Z uninit-checks
+
+#[repr(C)]
+#[derive(kani::Arbitrary)]
+struct S(u32, u8); // 5 bytes of data + 3 bytes of padding.
+
+#[kani::proof]
+/// This checks that reading copied initialized bytes after a multi-byte copy verifies correctly.
+unsafe fn non_byte_copy_without_padding() {
+    let from: S = kani::any();
+    let mut to: u64 = kani::any();
+
+    let from_ptr = &from as *const S;
+    let to_ptr = &mut to as *mut u64;
+
+    // This should not cause UB since `copy` is untyped.
+    std::ptr::copy(from_ptr as *const u32, to_ptr as *mut u32, 1);
+
+    // Since the previous copy only copied 4 bytes, no padding was copied, so no padding is read.
+    let data: u64 = std::ptr::read(to_ptr);
+}
