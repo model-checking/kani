88ba7e563b62f5d312707db0f32c7d8c40d8f9d0
Adrian Palacios <accorell@amazon.com>
2024-08-20 22:06:33 +0000

    Paste link to issue

--- tests/coverage/unreachable/assume_assert/main.rs
+++ tests/coverage/unreachable/assume_assert/main.rs
@@ -4,8 +4,9 @@
 //! This test should check that the region after `kani::assume(false)` is
 //! `UNCOVERED`. However, due to a technical limitation in `rustc`'s coverage
 //! instrumentation, only one `COVERED` region is reported for the whole
-//! function.
-//! TODO: Create issue and paste here.
+//! function. More details in
+//! <https://github.com/model-checking/kani/issues/3441>.
+
 #[kani::proof]
 fn check_assume_assert() {
     let a: u8 = kani::any();
