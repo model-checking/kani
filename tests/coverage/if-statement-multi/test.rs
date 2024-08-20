047f24db5244906d21780c2ac9e8e170a424f3a9
Adrian Palacios <accorell@amazon.com>
2024-08-20 22:04:35 +0000

    Fix test description

--- tests/coverage/if-statement-multi/test.rs
+++ tests/coverage/if-statement-multi/test.rs
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 // kani-flags: --coverage -Zsource-coverage
 
-//! Checks that we print a line which points the user to the path where coverage
-//! results have been saved. The line should look like:
-//! ```
-//! [info] Coverage results saved to /path/to/outdir/kanicov_YYYY-MM-DD_hh-mm
-//! ```
+//! Checks that we are covering all regions except
+//!  * the `val == 42` condition
+//!  * the `false` branch
+//!
+//! No coverage information is shown for `_other_function` because it's sliced
+//! off: <https://github.com/model-checking/kani/issues/3445>
 
 fn _other_function() {
     println!("Hello, world!");
