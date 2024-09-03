d59632980b2d0951a0cb820aa4407bc54e5899d8
Carolyn Zech <cmzech@amazon.com>
2024-09-03 10:29:50 -0400

    add copyright

--- tests/expected/issue-2239/issue_2239.rs
+++ tests/expected/issue-2239/issue_2239.rs
@@ -1,3 +1,6 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 #![feature(trivial_bounds)]
 #![allow(unused, trivial_bounds)]
 
--- tests/expected/issue-2732/issue_2732.rs
+++ tests/expected/issue-2732/issue_2732.rs
@@ -1,3 +1,6 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 const C: [u32; 5] = [0; 5];
 
 #[allow(unconditional_panic)]
