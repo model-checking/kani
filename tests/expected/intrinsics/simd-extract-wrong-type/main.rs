d2051b77437a0032120f0513e0e9c3c4766d8562
Qinheping Hu <qinhh@amazon.com>
2024-09-18 02:54:13 -0500

    Upgrade toolchain to 2024-09-12 (#3524)
    Relevant upstream PR:
    
    https://github.com/rust-lang/rust/commit/d2309c2a9d Ban non-array SIMD
    
    Resolves https://github.com/model-checking/kani/issues/3521
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

--- tests/expected/intrinsics/simd-extract-wrong-type/main.rs
+++ tests/expected/intrinsics/simd-extract-wrong-type/main.rs
@@ -10,11 +10,11 @@ use std::intrinsics::simd::simd_extract;
 #[repr(simd)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct i64x2(i64, i64);
+pub struct i64x2([i64; 2]);
 
 #[kani::proof]
 fn main() {
-    let y = i64x2(0, 1);
+    let y = i64x2([0, 1]);
     let res: i32 = unsafe { simd_extract(y, 1) };
     // ^^^^ The code above fails to type-check in Rust with the error:
     // ```
