d1ab70455d1f25a91b48bc24e43f87a0b135c363
Qinheping Hu <qinhh@amazon.com>
2024-09-17 23:49:23 -0500

    Fix more tests

--- tests/expected/intrinsics/simd-div-div-zero/main.rs
+++ tests/expected/intrinsics/simd-div-div-zero/main.rs
@@ -8,13 +8,13 @@ use std::intrinsics::simd::simd_div;
 #[repr(simd)]
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct i32x2(i32, i32);
+pub struct i32x2([i32; 2]);
 
 #[kani::proof]
 fn test_simd_div() {
     let dividend = kani::any();
-    let dividends = i32x2(dividend, dividend);
+    let dividends = i32x2([dividend, dividend]);
     let divisor = 0;
-    let divisors = i32x2(divisor, divisor);
+    let divisors = i32x2([divisor, divisor]);
     let _ = unsafe { simd_div(dividends, divisors) };
 }
