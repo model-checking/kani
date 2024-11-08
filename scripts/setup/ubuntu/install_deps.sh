207dabd6e6d0eb32e91b8be8e89b7716f41d66a8
Zyad Hassan <zyadh@amazon.com>
2024-11-08 14:43:00 -0800

    More cleanup

--- scripts/setup/ubuntu/install_deps.sh
+++ scripts/setup/ubuntu/install_deps.sh
@@ -57,6 +57,5 @@ fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ${SCRIPT_DIR}/install_cbmc.sh
-${SCRIPT_DIR}/install_viewer.sh
 # The Kissat installation script is platform-independent, so is placed one level up
 ${SCRIPT_DIR}/../install_kissat.sh
