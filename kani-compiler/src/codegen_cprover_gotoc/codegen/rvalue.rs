bb7d7099ebe4a07f66487c68a1894b566ab66333
Carolyn Zech <cmzech@amazon.com>
2024-11-11 16:48:55 -0500

    traverse type

--- kani-compiler/src/codegen_cprover_gotoc/codegen/rvalue.rs
+++ kani-compiler/src/codegen_cprover_gotoc/codegen/rvalue.rs
@@ -709,7 +709,7 @@ impl GotocCtx<'_> {
                         let meta = self.codegen_operand_stable(&operands[1]);
                         slice_fat_ptr(typ, data_cast, meta, &self.symbol_table)
                     }
-                    TyKind::RigidTy(RigidTy::Adt(..)) => {
+                    TyKind::RigidTy(RigidTy::Adt(_, generic_args)) => {
                         let pointee_goto_typ = self.codegen_ty_stable(pointee_ty);
                         let data_cast =
                             data.cast_to(Type::Pointer { typ: Box::new(pointee_goto_typ) });
@@ -719,10 +719,27 @@ impl GotocCtx<'_> {
                             data_cast
                         // This is a wide raw pointer; `pointee_ty` is a DST and has non-zero-sized metadata.
                         // An ADT can be a DST if it is a struct and its last field is a DST.
-                        // The only way to construct such a DST is by making the type generic and having the last field be a slice,
                         // c.f. https://doc.rust-lang.org/nomicon/exotic-sizes.html#dynamically-sized-types-dsts
                         } else {
-                            slice_fat_ptr(typ, data_cast, meta, &self.symbol_table)
+                            let mut dst_field = *generic_args.0.first().expect("To construct an ADT that is a DST, there must be a generic argument").expect_ty();
+                            while let TyKind::RigidTy(RigidTy::Adt(_, generic_args)) =
+                                dst_field.kind()
+                            {
+                                dst_field = *generic_args.0.first().unwrap().expect_ty();
+                            }
+
+                            if dst_field.kind().is_slice() {
+                                slice_fat_ptr(typ, data_cast, meta, &self.symbol_table)
+                            } else {
+                                let vtable_expr = meta
+                                    .member("_vtable_ptr", &self.symbol_table)
+                                    .member("pointer", &self.symbol_table)
+                                    .cast_to(
+                                        typ.lookup_field_type("vtable", &self.symbol_table)
+                                            .unwrap(),
+                                    );
+                                dynamic_fat_ptr(typ, data_cast, vtable_expr, &self.symbol_table)
+                            }
                         }
                     }
                     TyKind::RigidTy(RigidTy::Dynamic(..)) => {
