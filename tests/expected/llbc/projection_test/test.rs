28395c5c4f52386c78913834a646afd7a5f9dbc7
thanhnguyen-aws <ntson@amazon.com>
2024-11-18 15:21:44 -0800

    re-run kani and fix the expected file

--- tests/expected/llbc/projection_test/test.rs
+++ tests/expected/llbc/projection_test/test.rs
@@ -9,21 +9,30 @@ struct MyStruct {
     b: i32,
 }
 
-enum MyEnum {
+enum MyEnum0 {
     A(MyStruct, i32),
     B,
 }
 
+enum MyEnum {
+    A(MyStruct, MyEnum0),
+    B((i32, i32)),
+}
+
 fn enum_match(e: MyEnum) -> i32 {
     match e {
-        MyEnum::A(s, i) => s.a + i,
-        MyEnum::B => 0,
+        MyEnum::A(s, e0) => match e0 {
+            MyEnum0::A(s1, b) => s1.a + b,
+            MyEnum0::B => s.a + s.b,
+        },
+        MyEnum::B((a, b)) => a + b,
     }
 }
 
 #[kani::proof]
 fn main() {
     let s = MyStruct { a: 1, b: 2 };
-    let e = MyEnum::A(s, 1);
+    let s0 = MyStruct { a: 1, b: 2 };
+    let e = MyEnum::A(s, MyEnum0::A(s0, 1));
     let i = enum_match(e);
 }
