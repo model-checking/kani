4107f81c7d2b0d6112865d1c2647608ecceafdf6
thanhnguyen-aws <ntson@amazon.com>
2024-11-20 14:43:01 -0800

    fixed format

--- kani-compiler/src/codegen_aeneas_llbc/mir_to_ullbc/mod.rs
+++ kani-compiler/src/codegen_aeneas_llbc/mir_to_ullbc/mod.rs
@@ -245,33 +245,30 @@ impl<'a, 'tcx> Context<'a, 'tcx> {
                     }
                     _ => panic!("generic_params_from_adtdef: not a param type"),
                 },
-                GenericArgKind::Const(tc) => {
-                    match tc.kind() {
-                        TyConstKind::Param(paramtc) => {
-                            let def_id_internal =
-                                rustc_internal::internal(self.tcx, adtdef.def_id());
-                            let paramenv = self.tcx.param_env(def_id_internal);
-                            let pc_internal = rustc_middle::ty::ParamConst {
-                                index: paramtc.index,
-                                name: rustc_span::Symbol::intern(&paramtc.name),
-                            };
-                            let ty_internal = pc_internal.find_ty_from_env(paramenv);
-                            let ty_stable = rustc_internal::stable(ty_internal);
-                            let trans_ty = self.translate_ty(ty_stable);
-                            let lit_ty = match trans_ty.kind() {
-                                CharonTyKind::Literal(lit) => *lit,
-                                _ => panic!("generic_params_from_adtdef: not a literal type"),
-                            };
-                            let c_constgeneric = CharonConstGenericVar {
-                                index: CharonConstGenericVarId::from_usize(paramtc.index as usize),
-                                name: paramtc.name.clone(),
-                                ty: lit_ty,
-                            };
-                            c_const_generics.push(c_constgeneric);
-                        }
-                        _ => panic!("generic_params_from_adtdef: not a param const"),
+                GenericArgKind::Const(tc) => match tc.kind() {
+                    TyConstKind::Param(paramtc) => {
+                        let def_id_internal = rustc_internal::internal(self.tcx, adtdef.def_id());
+                        let paramenv = self.tcx.param_env(def_id_internal);
+                        let pc_internal = rustc_middle::ty::ParamConst {
+                            index: paramtc.index,
+                            name: rustc_span::Symbol::intern(&paramtc.name),
+                        };
+                        let ty_internal = pc_internal.find_ty_from_env(paramenv);
+                        let ty_stable = rustc_internal::stable(ty_internal);
+                        let trans_ty = self.translate_ty(ty_stable);
+                        let lit_ty = match trans_ty.kind() {
+                            CharonTyKind::Literal(lit) => *lit,
+                            _ => panic!("generic_params_from_adtdef: not a literal type"),
+                        };
+                        let c_constgeneric = CharonConstGenericVar {
+                            index: CharonConstGenericVarId::from_usize(paramtc.index as usize),
+                            name: paramtc.name.clone(),
+                            ty: lit_ty,
+                        };
+                        c_const_generics.push(c_constgeneric);
                     }
-                }
+                    _ => panic!("generic_params_from_adtdef: not a param const"),
+                },
             }
         }
         CharonGenericParams {
@@ -801,7 +798,9 @@ impl<'a, 'tcx> Context<'a, 'tcx> {
                 let c_raw_constexpr = self.translate_allocation(alloc, *ty);
                 translate_constant_expr_to_const_generic(c_raw_constexpr).unwrap()
             }
-            TyConstKind::Param(paramc) => CharonConstGeneric::Var(CharonConstGenericVarId::from_usize(paramc.index as usize)),
+            TyConstKind::Param(paramc) => {
+                CharonConstGeneric::Var(CharonConstGenericVarId::from_usize(paramc.index as usize))
+            }
             _ => todo!(),
         }
     }
