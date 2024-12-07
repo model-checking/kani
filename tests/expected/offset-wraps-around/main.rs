fa0d1ec33129eaef95eca68158b964c23dc3c4d6
Celina G. Val <celinval@amazon.com>
2024-12-06 18:15:15 -0800

    A few tests adjustments including a new fixme test

--- tests/expected/offset-wraps-around/main.rs
+++ tests/expected/offset-wraps-around/main.rs
@@ -5,8 +5,8 @@
 //! the add operation wraps.
 //!
 //! Note that CBMC offset logic will wrap around the object bits, not the entire address space, when
-//! computing the offset between pointers. Doing that is UB in Rust, so we should be OK
-//! as long as Kani can detect UB in that case.
+//! computing the offset between pointers.
+//! See <https://github.com/model-checking/kani/issues/1150> for more details.
 
 use std::convert::TryInto;
 
@@ -15,15 +15,14 @@ fn original_harness() {
     let v: &[u128] = &[0; 10];
     let v_0: *const u128 = &v[0];
     let high_offset = usize::MAX / (std::mem::size_of::<u128>() * 4);
-    unsafe {
-        let v_wrap: *const u128 = v_0.wrapping_add(high_offset);
-        // This should trigger UB!!
-        let wrapped_offset = unsafe { v_wrap.offset_from(v_0) };
-        // Without UB detection, the offsets are the same, but CBMC pointer arithmetic
-        // would "wrap around" making this incorrect
-        // https://github.com/model-checking/kani/issues/1150
-        assert!(high_offset == wrapped_offset.try_into().unwrap());
-    }
+    let v_wrap: *const u128 = v_0.wrapping_add(high_offset);
+    // FIX-ME: This should trigger UB!!
+    // https://github.com/model-checking/kani/issues/3756
+    let wrapped_offset = unsafe { v_wrap.offset_from(v_0) };
+    // Without UB detection, the offsets are the same, but CBMC pointer arithmetic
+    // would "wrap around" making this incorrect
+    // https://github.com/model-checking/kani/issues/1150
+    assert!(high_offset == wrapped_offset.try_into().unwrap());
 }
 
 /// This harness is similar to the `original_harness`, but we replace the `offset_from` with
@@ -33,27 +32,18 @@ fn harness_without_ub() {
     let v: &[u128] = &[0; 10];
     let v_0: *const u128 = &v[0];
     let high_offset = usize::MAX / (size_of::<u128>() * 4);
-    unsafe {
-        let v_wrap: *const u128 = v_0.wrapping_add(high_offset);
-        // The only way to compute offset of pointers out of bounds is to convert them to integers.
-        let wrapped_offset = (v_wrap.addr() - v_0.addr()) / size_of::<u128>();
-        // Now this should work
-        assert_eq!(high_offset, wrapped_offset);
-    }
+    let v_wrap: *const u128 = v_0.wrapping_add(high_offset);
+    // The only way to compute offset of pointers out of bounds is to convert them to integers.
+    let wrapped_offset = (v_wrap.addr() - v_0.addr()) / size_of::<u128>();
+    // Now this should work
+    assert_eq!(high_offset, wrapped_offset);
 }
 
+/// Check that wrapping offset by `usize::MAX + 1` bytes will result in the same base pointer.
 #[kani::proof]
-fn check_wrap_ptr_max() {
+fn check_wrap_around_ptr() {
     let v: &[u128] = &[0; 10];
     let orig_ptr: *const u128 = &v[0];
     let new_ptr: *const u128 = orig_ptr.wrapping_byte_add(usize::MAX).wrapping_byte_add(1);
     assert_eq!(orig_ptr as usize, new_ptr as usize);
-}
-
-#[kani::proof]
-fn check_wrap_ptr_10_bits() {
-    let v: &[u128] = &[0; 10];
-    let orig_ptr: *const u128 = &v[0];
-    let new_ptr: *const u128 = orig_ptr.wrapping_byte_add(1 << 63);
-    assert_ne!(orig_ptr as usize, new_ptr as usize);
 }
