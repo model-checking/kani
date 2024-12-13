dab2d6592b54e50a6eb0d171717e86bad1644548
Michael Tautschnig <tautschn@amazon.com>
2024-12-13 10:11:10 +0000

    Revert "Fix tests after enabling offset bound checks"
    This reverts commit 1632b67680defe00051ee8ea27a4b0870e45ae5c.

--- tests/expected/intrinsics/copy-nonoverlapping/copy-unreadable-src/main.rs
+++ tests/expected/intrinsics/copy-nonoverlapping/copy-unreadable-src/main.rs
@@ -7,10 +7,10 @@ fn test_copy_nonoverlapping_invalid() {
     let arr: [i32; 3] = [0, 1, 0];
     let src: *const i32 = arr.as_ptr();
 
-    // Get an invalid pointer with a negative offset
-    let src_invalid = src.wrapping_sub(1) as *const i32;
-    let dst = src.wrapping_add(1) as *mut i32;
     unsafe {
+        // Get an invalid pointer with a negative offset
+        let src_invalid = unsafe { src.sub(1) as *const i32 };
+        let dst = src.add(1) as *mut i32;
         core::intrinsics::copy_nonoverlapping(src_invalid, dst, 1);
     }
 }
--- tests/expected/pointer-overflow/main.rs
+++ tests/expected/pointer-overflow/main.rs
@@ -1,6 +1,7 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
+// kani-flags: --enable-unstable --extra-pointer-checks
 // Checks that overflows for pointer arithmetic are reported
 
 #[kani::proof]
