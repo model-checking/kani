63f483df24c5368e0b3608d59251eae091428220
Qinheping Hu <qinhh@amazon.com>
2025-01-10 11:52:56 -0600

    Update tests/script-based-pre/cargo_manifest_test/manifest_test.sh
    Co-authored-by: Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>

--- tests/script-based-pre/cargo_manifest_test/manifest_test.sh
+++ tests/script-based-pre/cargo_manifest_test/manifest_test.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
 # Test if Kani can correctly check if package in the workspace when
-# manifest-path presents.
+# manifest-path present.
 
 cargo kani --manifest-path=add/Cargo.toml --package add --debug
