2a44655615762f0f45a101adffc6b07ccb494e96
Rajath Kotyal <rjkotyal@amazon.com>
2025-02-04 16:24:05 -0800

    Update check_message_overflow.rs

--- tests/ui/cbmc_checks/float-overflow/check_message_overflow.rs
+++ tests/ui/cbmc_checks/float-overflow/check_message_overflow.rs
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
 // Check we don't print temporary variables as part of CBMC messages.
+// This test verifies that Kani does not report floating-point overflow by default
+// for operations that result in +/-Infinity.
 extern crate kani;
-
-use kani::any;
 
 // Use the result so rustc doesn't optimize them away.
 fn dummy(result: f32) -> f32 {
@@ -13,8 +13,8 @@ fn dummy(result: f32) -> f32 {
 
 #[kani::proof]
 fn main() {
-    let a = kani::any_where(|&x: &f32| x.is_finite() && x.abs() < 1e20);
-    let b = kani::any_where(|&x: &f32| x.is_finite() && x.abs() < 1e20);
+    let a = kani::any_where(|&x: &f32| x.is_finite());
+    let b = kani::any_where(|&x: &f32| x.is_finite());
 
     dummy(a + b);
     dummy(a - b);
