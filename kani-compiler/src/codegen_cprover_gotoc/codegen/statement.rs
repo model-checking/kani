81e9aa3063251cbe2474f18281c7292ebd41e667
Carolyn Zech <cmzech@amazon.com>
2025-02-11 13:21:26 -0500

    Upgrade toolchain to 2/10 (#3883)
    Upgrade toolchain to 2/10.
    
    I **highly recommend** reviewing this PR commit-by-commit. The
    description in each commit message links to the upstream PRs that
    prompted those particular changes.
    
    ## Callouts
    - 2/1 had a lot of formatting changes. I split the commits for that day
    into formatting changes and functionality changes accordingly.
    - 2/5 introduced a regression in our delayed UB instrumentation, so I
    made a new fixme test. See #3881 for details.
    
    
    ## Culprit PRs:
    https://github.com/rust-lang/rust/pull/134424 
    https://github.com/rust-lang/rust/pull/130514
    https://github.com/rust-lang/rust/pull/135748
    https://github.com/rust-lang/rust/pull/136590
    https://github.com/rust-lang/rust/pull/135318
    https://github.com/rust-lang/rust/pull/135265
    
    https://github.com/rust-lang/rust/pull/128045/commits/bcb8565f301b579dee60fffe87d5a329cc69fefa
    https://github.com/rust-lang/rust/pull/136471
    https://github.com/rust-lang/rust/pull/136645
    
    Resolves #3863
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

--- kani-compiler/src/codegen_cprover_gotoc/codegen/statement.rs
+++ kani-compiler/src/codegen_cprover_gotoc/codegen/statement.rs
@@ -7,10 +7,10 @@ use crate::codegen_cprover_gotoc::codegen::function::rustc_smir::region_from_cov
 use crate::codegen_cprover_gotoc::{GotocCtx, VtableCtx};
 use crate::unwrap_or_return_codegen_unimplemented_stmt;
 use cbmc::goto_program::{Expr, Location, Stmt, Type};
+use rustc_abi::{FieldsShape, Primitive, TagEncoding, Variants};
 use rustc_middle::ty::layout::LayoutOf;
 use rustc_middle::ty::{List, TypingEnv};
 use rustc_smir::rustc_internal;
-use rustc_target::abi::{FieldsShape, Primitive, TagEncoding, Variants};
 use stable_mir::abi::{ArgAbi, FnAbi, PassMode};
 use stable_mir::mir::mono::{Instance, InstanceKind};
 use stable_mir::mir::{
@@ -552,7 +552,7 @@ impl GotocCtx<'_> {
                 let ty = self.operand_ty_stable(op);
                 if ty.kind().is_bool() {
                     Some(self.codegen_operand_stable(op).cast_to(Type::c_bool()))
-                } else if arg_abi.map_or(true, |abi| abi.mode != PassMode::Ignore) {
+                } else if arg_abi.is_none_or(|abi| abi.mode != PassMode::Ignore) {
                     Some(self.codegen_operand_stable(op))
                 } else {
                     None
