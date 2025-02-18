4eea90fdb1e55082dd78e1f3684dbef4ec0b175d
Carolyn Zech <cmzech@amazon.com>
2025-02-18 10:09:36 -0500

    Apply suggestions from code review
    Co-authored-by: Michael Tautschnig <mt@debian.org>

--- tests/script-based-pre/playback_array/playback_array.sh
+++ tests/script-based-pre/playback_array/playback_array.sh
@@ -2,8 +2,15 @@
 # Copyright Kani Contributors
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 
+set -e
 set -o pipefail
 set -o nounset
+
+cleanup()
+{
+  rm ${RS_FILE}
+}
+trap cleanup EXIT
 
 RS_FILE="modified.rs"
 cp array.rs ${RS_FILE}
@@ -13,6 +20,3 @@ kani ${RS_FILE} -Z concrete-playback --concrete-playback=inplace
 
 echo "[TEST] Run test..."
 kani playback -Z concrete-playback ${RS_FILE}
-
-# Cleanup
-rm ${RS_FILE}
