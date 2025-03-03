1e18b2a7da8f7bd2338642b9c1443ea42ef5a333
Michael Tautschnig <tautschn@amazon.com>
2025-03-03 17:41:43 +0000

    Retrieve toolchain version

--- scripts/setup/install_rustup.sh
+++ scripts/setup/install_rustup.sh
@@ -7,4 +7,7 @@ set -eux
 # Install Rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
   && source  ~/.cargo/env
-rustup toolchain install
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd ${SCRIPT_DIR}/../../
+rustup toolchain install $(grep 'channel = ' rust-toolchain.toml | cut -d '"' -f 2)
+popd
