e151363aaa04b42899743bab8d915fe3e00b4cfc
Zyad Hassan <zyadh@amazon.com>
2025-03-03 11:52:00 -0800

    Add components

--- scripts/setup/install_rustup.sh
+++ scripts/setup/install_rustup.sh
@@ -9,5 +9,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
   && source  ~/.cargo/env
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd ${SCRIPT_DIR}/../../
-rustup toolchain install
+toolchain_version=$(grep 'channel = ' rust-toolchain.toml | cut -d '"' -f 2)
+toolchain_components=$(grep 'components = ' rust-toolchain.toml | cut -d '=' -f 2- | sed 's/[",]//g' | sed 's/\[//' | sed 's/\]//')
+rustup toolchain install $toolchain_version --component $toolchain_components
 popd
