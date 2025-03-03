ff42100c652c0fb8d1cd0cedd60c347b3586b93f
Zyad Hassan <zyadh@amazon.com>
2025-03-03 11:44:59 -0800

    Remove argument to rustup toolchain install

--- scripts/setup/install_rustup.sh
+++ scripts/setup/install_rustup.sh
@@ -9,5 +9,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
   && source  ~/.cargo/env
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd ${SCRIPT_DIR}/../../
-rustup toolchain install $(grep 'channel = ' rust-toolchain.toml | cut -d '"' -f 2)
+rustup toolchain install
 popd
