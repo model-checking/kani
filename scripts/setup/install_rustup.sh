38bee3cd6b1ab1dce48cf609eec93997ecf01133
Michael Tautschnig <tautschn@amazon.com>
2025-03-03 20:20:20 +0000

    DEBUG

--- scripts/setup/install_rustup.sh
+++ scripts/setup/install_rustup.sh
@@ -7,9 +7,12 @@ set -eux
 # Install Rust toolchain
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y \
   && source  ~/.cargo/env
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pushd ${SCRIPT_DIR}/../../
-toolchain_version=$(grep 'channel = ' rust-toolchain.toml | cut -d '"' -f 2)
-toolchain_components=$(grep 'components = ' rust-toolchain.toml | cut -d '=' -f 2- | sed 's/[" ]//g' | sed 's/\[//' | sed 's/\]//')
-rustup toolchain install $toolchain_version --component $toolchain_components
-popd
+rustup --version
+cat rust-toolchain.toml
+rustup toolchain install
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# pushd ${SCRIPT_DIR}/../../
+# toolchain_version=$(grep 'channel = ' rust-toolchain.toml | cut -d '"' -f 2)
+# toolchain_components=$(grep 'components = ' rust-toolchain.toml | cut -d '=' -f 2- | sed 's/[" ]//g' | sed 's/\[//' | sed 's/\]//')
+# rustup toolchain install $toolchain_version --component $toolchain_components
+# popd
