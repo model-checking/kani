7520bc3b68ecb178341283cee6a6b34d0d27a028
thanhnguyen-aws <ntson@amazon.com>
2025-03-19 13:55:43 -0700

    add expected

--- library/kani_macros/src/sysroot/loop_contracts/mod.rs
+++ library/kani_macros/src/sysroot/loop_contracts/mod.rs
@@ -7,11 +7,10 @@
 use proc_macro::TokenStream;
 use proc_macro_error2::abort_call_site;
 use quote::{format_ident, quote};
+use syn::parse_macro_input;
 use syn::spanned::Spanned;
 use syn::token::AndAnd;
-use syn::{BinOp, Expr, ExprBinary, Stmt, Block, visit_mut::VisitMut, Ident};
-use syn::parse_macro_input;
-
+use syn::{BinOp, Block, Expr, ExprBinary, Ident, Stmt, visit_mut::VisitMut};
 
 struct TransformationResult {
     transformed_expr: Expr,
@@ -23,7 +22,7 @@ struct CallReplacer {
     old_name: String,
     replacements: Vec<(Expr, proc_macro2::Ident)>,
     counter: usize,
-    var_prefix: String
+    var_prefix: String,
 }
 
 impl CallReplacer {
@@ -32,7 +31,7 @@ impl CallReplacer {
             old_name: old_name.to_string(),
             replacements: Vec::new(),
             counter: 0,
-            var_prefix: var_prefix
+            var_prefix: var_prefix,
         }
     }
 
@@ -49,13 +48,15 @@ impl CallReplacer {
                 return true;
             }
         }
-        
-        let full_path = expr_path.path.segments
+
+        let full_path = expr_path
+            .path
+            .segments
             .iter()
             .map(|seg| seg.ident.to_string())
             .collect::<Vec<_>>()
             .join("::");
-        
+
         full_path.ends_with(&self.old_name)
     }
 }
@@ -64,7 +65,7 @@ impl VisitMut for CallReplacer {
     fn visit_expr_mut(&mut self, expr: &mut Expr) {
         // Visit nested expressions first
         syn::visit_mut::visit_expr_mut(self, expr);
-        
+
         if let Expr::Call(call) = expr {
             if let Expr::Path(expr_path) = &*call.func {
                 if self.should_replace(expr_path) {
@@ -77,12 +78,16 @@ impl VisitMut for CallReplacer {
     }
 }
 
-fn transform_function_calls(expr: Expr, function_name: &str, var_prefix: String) -> TransformationResult {
+fn transform_function_calls(
+    expr: Expr,
+    function_name: &str,
+    var_prefix: String,
+) -> TransformationResult {
     let mut replacer = CallReplacer::new(function_name, var_prefix);
     let mut transformed_expr = expr;
     replacer.visit_expr_mut(&mut transformed_expr);
-    
-    let mut newreplace : Vec<(Expr, Ident)> = Vec::new();
+
+    let mut newreplace: Vec<(Expr, Ident)> = Vec::new();
     for (call, var) in replacer.replacements {
         match call {
             Expr::Call(call_expr) => {
@@ -94,33 +99,20 @@ fn transform_function_calls(expr: Expr, function_name: &str, var_prefix: String)
     }
 
     // Generate declarations block
-    let declarations: Vec<Stmt> = newreplace
-        .iter()
-        .map(|(call, var)| {
-            syn::parse_quote!(let mut #var = #call;)
-        })
-        .collect();
+    let declarations: Vec<Stmt> =
+        newreplace.iter().map(|(call, var)| syn::parse_quote!(let mut #var = #call;)).collect();
     let declarations_block: Block = syn::parse_quote!({
         #(#declarations)*
     });
 
-
     // Generate assignments block
-    let assignments: Vec<Stmt> = newreplace
-        .into_iter()
-        .map(|(call, var)| {
-            syn::parse_quote!(#var = #call;)
-        })
-        .collect();
+    let assignments: Vec<Stmt> =
+        newreplace.into_iter().map(|(call, var)| syn::parse_quote!(#var = #call;)).collect();
     let assignments_block: Block = syn::parse_quote!({
         #(#assignments)*
     });
 
-    TransformationResult {
-        transformed_expr,
-        declarations_block,
-        assignments_block,
-    }
+    TransformationResult { transformed_expr, declarations_block, assignments_block }
 }
 
 struct BreakContinueReplacer;
@@ -129,7 +121,7 @@ impl VisitMut for BreakContinueReplacer {
     fn visit_expr_mut(&mut self, expr: &mut Expr) {
         // Visit nested expressions first
         syn::visit_mut::visit_expr_mut(self, expr);
-        
+
         // Replace the expression
         *expr = match expr {
             Expr::Break(_) => {
@@ -138,7 +130,7 @@ impl VisitMut for BreakContinueReplacer {
             Expr::Continue(_) => {
                 syn::parse_quote!(return)
             }
-            _ => return
+            _ => return,
         };
     }
 }
@@ -168,7 +160,7 @@ fn transform_break_continue(block: &mut Block) {
 ///  }
 /// ```
 pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
-    // parse the stmt of the loop    
+    // parse the stmt of the loop
     let mut loop_stmt: Stmt = syn::parse(item.clone()).unwrap();
 
     // name of the loop invariant as closure of the form
@@ -179,19 +171,20 @@ pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     // expr of the loop invariant
     let mut inv_expr: Expr = syn::parse(attr).unwrap();
-    // adding remember variables 
+    // adding remember variables
     let mut var_prefix: String = "__kani_remember_var".to_owned();
     var_prefix.push_str(&loop_id);
-    let transform_inv: TransformationResult = transform_function_calls(inv_expr.clone(), "old", var_prefix);
+    let transform_inv: TransformationResult =
+        transform_function_calls(inv_expr.clone(), "old", var_prefix);
     let has_old = !transform_inv.declarations_block.stmts.is_empty();
     let decl_stms = transform_inv.declarations_block.stmts.clone();
     let mut assign_stms = transform_inv.assignments_block.stmts.clone();
     let (mut loop_body, loop_guard) = match loop_stmt {
         Stmt::Expr(ref mut e, _) => match e {
             Expr::While(ew) => (ew.body.clone(), ew.cond.clone()),
-            _ => panic!()
+            _ => panic!(),
         },
-        _ => panic!()
+        _ => panic!(),
     };
     let loop_body_stms = loop_body.stmts.clone();
     assign_stms.extend(loop_body_stms);
@@ -204,9 +197,9 @@ pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
         match loop_stmt {
             Stmt::Expr(ref mut e, _) => match e {
                 Expr::While(ew) => ew.body.stmts = assign_stms.clone(),
-                _ => panic!()
+                _ => panic!(),
             },
-            _ => panic!()
+            _ => panic!(),
         };
     }
 
@@ -241,9 +234,9 @@ pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
             note = "for now, loop contracts is only supported for while-loops.";
         ),
     }
-    
+
     if has_old {
-    quote!(
+        quote!(
         {
         assert!(#loop_guard);
         #(#decl_stms)*
@@ -260,9 +253,9 @@ pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
         #loop_stmt
         })
-    .into()
+        .into()
     } else {
-    quote!(
+        quote!(
         {
         // Dummy function used to force the compiler to capture the environment.
         // We cannot call closures inside constant functions.
@@ -274,7 +267,7 @@ pub fn loop_invariant(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
         #loop_stmt
         })
-    .into()
+        .into()
     }
 }
 
