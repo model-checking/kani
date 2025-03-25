9f0c04d228e4d9f26ef1fec0b2806657ec3b2f52
Rajath Kotyal <rjkotyal@amazon.com>
2025-03-24 18:32:19 -0700

    clean up code

--- tests/expected/MemPredicates/ptr_size_validity.rs
+++ tests/expected/MemPredicates/ptr_size_validity.rs
@@ -9,16 +9,6 @@ mod size {
     use super::*;
 
     #[kani::proof]
-    fn verify_checked_size_of_raw_exceeds_isize_max() {
-        let len_exceeding_isize_max = (isize::MAX as usize) + 1;
-        let data_ptr: *const [u8] = core::ptr::from_raw_parts(core::ptr::null::<u8>(), len_exceeding_isize_max);
-
-        let size = kani::mem::checked_size_of_raw(data_ptr);
-
-        assert!(size.is_none());
-    }
-
-    #[kani::proof]
     fn verify_is_inbounds() {
         let val = 42u8;
         let ptr = &val as *const u8;
