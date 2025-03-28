d8d43d9b48f8953c8ae5774bb8679e5e8075bae3
Michael Tautschnig <mt@debian.org>
2025-03-28 09:16:53 +0100

    Update kani-compiler/src/main.rs
    Co-authored-by: Carolyn Zech <cmzech@amazon.com>

--- kani-compiler/src/main.rs
+++ kani-compiler/src/main.rs
@@ -17,6 +17,7 @@
 #![feature(f16)]
 #![feature(non_exhaustive_omitted_patterns_lint)]
 #![feature(cfg_version)]
+// Once the `stable` branch is at 1.86 or later, remove this line, since float_next_up_down is stabilized
 #![cfg_attr(not(version("1.86")), feature(float_next_up_down))]
 #![feature(try_blocks)]
 extern crate rustc_abi;
