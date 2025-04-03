af12d6a870c3648fd6b4d44cfead220d19200ec4
Carolyn Zech <cmzech@amazon.com>
2025-04-03 13:05:01 -0700

    rephrase "crate" to "package"

--- kani-driver/src/call_cargo.rs
+++ kani-driver/src/call_cargo.rs
@@ -182,13 +182,13 @@ crate-type = ["lib"]
             cargo_args.push("-v".into());
         }
 
-        // Arguments that will only be passed to the target crate (the crate under verification)
+        // Arguments that will only be passed to the target package (the package under verification)
         // and not its dependencies, c.f. https://doc.rust-lang.org/cargo/commands/cargo-rustc.html.
         // The difference between pkg_args and rustc_args is that rustc_args are also provided when
-        // we invoke rustc on the target crate's dependencies.
+        // we invoke rustc on the target package's dependencies.
         // We do not provide the `--reachability` argument to dependencies so that it has the default value `None`
         // (c.f. kani-compiler::args::ReachabilityType) and we skip codegen for the dependency.
-        // This is the desired behavior because we only want to construct `CodegenUnits` for the target crate;
+        // This is the desired behavior because we only want to construct `CodegenUnits` for the target package;
         // i.e., if some dependency has harnesses, we don't want to run them.
 
         // If you are adding a new `kani-compiler` argument, you likely want to put it `kani_compiler_flags()` instead,
