d463882592b5770a541131381a3e59541f8b466a
Qinheping Hu <qinhh@amazon.com>
2025-04-11 15:02:38 +0000

    Make tests depending on new CBMC fixme

--- tests/kani/Quantifiers/no_array_fixme.rs
+++ tests/kani/Quantifiers/no_array_fixme.rs
@@ -18,9 +18,34 @@ fn forall_assume_harness() {
     kani::assert(j > 4, "");
 }
 
+fn comp(x: isize, y: isize) -> bool {
+    x > y
+}
+
+#[kani::proof]
+fn forall_function_harness() {
+    let j = kani::any();
+    kani::assume(j > 5);
+    kani::assert(kani::forall!(|i in (2,5)| comp(j, i) ), "");
+}
+
 #[kani::proof]
 fn exists_assert_harness() {
     let j = kani::any();
     kani::assume(j > 2);
     kani::assert(kani::exists!(|i in (2,5)| i < j ), "");
+}
+
+#[kani::proof]
+fn exists_assume_harness() {
+    let j = kani::any();
+    kani::assume(kani::exists!(|i in (2,4)| i == j));
+    kani::assert(j == 3 || j == 2, "");
+}
+
+#[kani::proof]
+fn exists_function_harness() {
+    let j = kani::any();
+    kani::assume(j > 2);
+    kani::assert(kani::exists!(|i in (2,5)| comp(j, i) ), "");
 }
