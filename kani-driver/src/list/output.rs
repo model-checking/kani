b932ca216682c6740b5dbf65a6c444b11d30433a
Carolyn Zech <cmzech@amazon.com>
2025-04-16 23:44:50 -0400

    include crate name in Markdown and printed output for list subcommand
    don't put it in JSON since that already includes the file which is more specific anyway

--- kani-driver/src/list/output.rs
+++ kani-driver/src/list/output.rs
@@ -3,13 +3,18 @@
 //! This module handles outputting the result for the list subcommand
 
 use std::{
+    collections::BTreeSet,
     fmt::Display,
     fs::File,
     io::{BufWriter, Write},
     path::Path,
 };
 
-use crate::{args::list_args::Format, list::ListMetadata, version::KANI_VERSION};
+use crate::{
+    args::list_args::Format,
+    list::{ListMetadata, merge_list_metadata},
+    version::KANI_VERSION,
+};
 use anyhow::Result;
 use comfy_table::Table as PrettyTable;
 use serde_json::json;
@@ -21,7 +26,11 @@ const FILE_VERSION: &str = "0.1";
 const OUTPUT_FILENAME: &str = "kani-list";
 
 /// Output the results of the list subcommand.
-pub fn output_list_results(list_metadata: ListMetadata, format: Format, quiet: bool) -> Result<()> {
+pub fn output_list_results(
+    list_metadata: BTreeSet<ListMetadata>,
+    format: Format,
+    quiet: bool,
+) -> Result<()> {
     match format {
         Format::Pretty => pretty(list_metadata),
         Format::Markdown => markdown(list_metadata, quiet),
@@ -29,36 +38,64 @@ pub fn output_list_results(list_metadata: ListMetadata, format: Format, quiet: b
     }
 }
 
-/// Print results to the terminal.
-fn pretty(list_metadata: ListMetadata) -> Result<()> {
-    let table = if list_metadata.contracted_functions.is_empty() {
-        None
-    } else {
-        let (header, rows) = construct_contracts_table(&list_metadata);
-        let mut t = PrettyTable::new();
-        t.set_header(header).add_rows(rows);
-        Some(t)
+fn pretty_constructor(header: Vec<String>, rows: Vec<Vec<String>>) -> Result<PrettyTable> {
+    let mut t = PrettyTable::new();
+    t.set_header(header).add_rows(rows);
+    Ok(t)
+}
+
+fn markdown_constructor(header: Vec<String>, rows: Vec<Vec<String>>) -> Result<MarkdownTable> {
+    Ok(MarkdownTable::new(Some(header), rows)?)
+}
+
+/// Construct the "Contracts" and "Standard Harnesses" tables.
+/// `table_constructor` is a function that, given the header and rows for the tables, creates a particular kind of table.
+fn construct_output<T: Display>(
+    list_metadata: BTreeSet<ListMetadata>,
+    table_constructor: fn(Vec<String>, Vec<Vec<String>>) -> Result<T>,
+) -> Result<(String, String)> {
+    let contract_output = {
+        const CONTRACTS_SECTION: &str = "Contracts:";
+        const NO_CONTRACTS_MSG: &str = "No contracts or contract harnesses found.";
+        let contract_table = if list_metadata.iter().all(|md| md.contracted_functions.is_empty()) {
+            None
+        } else {
+            let (header, rows) = construct_contracts_table(&list_metadata);
+            let t = table_constructor(header, rows)?;
+            Some(t)
+        };
+        format_results(contract_table, CONTRACTS_SECTION.to_string(), NO_CONTRACTS_MSG.to_string())
     };
-    let output = format_results(table, &list_metadata);
-    println!("{}", output);
+    let standard_output = {
+        const HARNESSES_SECTION: &str = "Standard Harnesses (#[kani::proof]):";
+        const NO_HARNESSES_MSG: &str = "No standard harnesses found.";
+        let standard_table = {
+            let (header, rows) = construct_standard_table(&list_metadata);
+            let t = table_constructor(header, rows)?;
+            Some(t)
+        };
+        format_results(standard_table, HARNESSES_SECTION.to_string(), NO_HARNESSES_MSG.to_string())
+    };
+    Ok((contract_output, standard_output))
+}
+
+/// Print results to the terminal.
+fn pretty(list_metadata: BTreeSet<ListMetadata>) -> Result<()> {
+    let (contract_output, standard_output) = construct_output(list_metadata, pretty_constructor)?;
+    println!("{}", contract_output);
+    println!("{}", standard_output);
 
     Ok(())
 }
 
 /// Output results to a Markdown file.
-fn markdown(list_metadata: ListMetadata, quiet: bool) -> Result<()> {
-    let table = if list_metadata.contracted_functions.is_empty() {
-        None
-    } else {
-        let (header, rows) = construct_contracts_table(&list_metadata);
-        Some(MarkdownTable::new(Some(header), rows)?)
-    };
-
-    let output = format_results(table, &list_metadata);
+fn markdown(list_metadata: BTreeSet<ListMetadata>, quiet: bool) -> Result<()> {
+    let (contract_output, standard_output) = construct_output(list_metadata, markdown_constructor)?;
 
     let out_path = Path::new(OUTPUT_FILENAME).with_extension("md");
     let mut out_file = File::create(&out_path).unwrap();
-    out_file.write_all(output.as_bytes()).unwrap();
+    out_file.write_all(contract_output.as_bytes()).unwrap();
+    out_file.write_all(standard_output.as_bytes()).unwrap();
     if !quiet {
         println!("Wrote list results to {}", std::fs::canonicalize(&out_path)?.display());
     }
@@ -66,21 +103,23 @@ fn markdown(list_metadata: ListMetadata, quiet: bool) -> Result<()> {
 }
 
 /// Output results as a JSON file.
-fn json(list_metadata: ListMetadata, quiet: bool) -> Result<()> {
+fn json(list_metadata: BTreeSet<ListMetadata>, quiet: bool) -> Result<()> {
     let out_path = Path::new(OUTPUT_FILENAME).with_extension("json");
     let out_file = File::create(&out_path).unwrap();
     let writer = BufWriter::new(out_file);
 
+    let combined_md = merge_list_metadata(list_metadata);
+
     let json_obj = json!({
         "kani-version": KANI_VERSION,
         "file-version": FILE_VERSION,
-        "standard-harnesses": &list_metadata.standard_harnesses,
-        "contract-harnesses": &list_metadata.contract_harnesses,
-        "contracts": &list_metadata.contracted_functions,
+        "standard-harnesses": combined_md.standard_harnesses,
+        "contract-harnesses": combined_md.contract_harnesses,
+        "contracts": combined_md.contracted_functions,
         "totals": {
-            "standard-harnesses": list_metadata.standard_harnesses_count,
-            "contract-harnesses": list_metadata.contract_harnesses_count,
-            "functions-under-contract": list_metadata.contracted_functions.len(),
+            "standard-harnesses": combined_md.standard_harnesses_count,
+            "contract-harnesses": combined_md.contract_harnesses_count,
+            "functions-under-contract": combined_md.contracted_functions.len(),
         }
     });
 
@@ -95,65 +134,95 @@ fn json(list_metadata: ListMetadata, quiet: bool) -> Result<()> {
 
 /// Construct the rows for the table of contracts information.
 /// Returns a tuple of the table header and the rows.
-fn construct_contracts_table(list_metadata: &ListMetadata) -> (Vec<String>, Vec<Vec<String>>) {
+fn construct_contracts_table(
+    list_metadata: &BTreeSet<ListMetadata>,
+) -> (Vec<String>, Vec<Vec<String>>) {
     const NO_HARNESSES_MSG: &str = "NONE";
+    const CRATE_NAME: &str = "Crate";
     const FUNCTION_HEADER: &str = "Function";
     const CONTRACT_HARNESSES_HEADER: &str = "Contract Harnesses (#[kani::proof_for_contract])";
     const TOTALS_HEADER: &str = "Total";
 
-    let header =
-        vec![String::new(), FUNCTION_HEADER.to_string(), CONTRACT_HARNESSES_HEADER.to_string()];
+    let header = vec![
+        String::new(),
+        CRATE_NAME.to_string(),
+        FUNCTION_HEADER.to_string(),
+        CONTRACT_HARNESSES_HEADER.to_string(),
+    ];
 
     let mut rows: Vec<Vec<String>> = vec![];
+    let mut functions_under_contract_total = 0;
+    let mut contract_harnesses_total = 0;
 
-    for cf in &list_metadata.contracted_functions {
-        let mut row = vec![String::new(), cf.function.clone()];
-        if cf.harnesses.is_empty() {
-            row.push(NO_HARNESSES_MSG.to_string());
-        } else {
-            row.push(cf.harnesses.join(", "));
+    for crate_md in list_metadata {
+        for cf in &crate_md.contracted_functions {
+            let mut row = vec![String::new(), crate_md.crate_name.to_string(), cf.function.clone()];
+            if cf.harnesses.is_empty() {
+                row.push(NO_HARNESSES_MSG.to_string());
+            } else {
+                row.push(cf.harnesses.join(", "));
+            }
+            rows.push(row);
         }
-        rows.push(row);
+        functions_under_contract_total += crate_md.contracted_functions.len();
+        contract_harnesses_total += crate_md.contract_harnesses_count;
     }
 
     let totals_row = vec![
         TOTALS_HEADER.to_string(),
-        list_metadata.contracted_functions.len().to_string(),
-        list_metadata.contract_harnesses_count.to_string(),
+        String::new(),
+        functions_under_contract_total.to_string(),
+        contract_harnesses_total.to_string(),
     ];
     rows.push(totals_row);
 
     (header, rows)
 }
 
-/// Format results as a String
-fn format_results<T: Display>(table: Option<T>, list_metadata: &ListMetadata) -> String {
-    const CONTRACTS_SECTION: &str = "Contracts:";
-    const HARNESSES_SECTION: &str = "Standard Harnesses (#[kani::proof]):";
-    const NO_CONTRACTS_MSG: &str = "No contracts or contract harnesses found.";
-    const NO_HARNESSES_MSG: &str = "No standard harnesses found.";
+fn construct_standard_table(
+    list_metadata: &BTreeSet<ListMetadata>,
+) -> (Vec<String>, Vec<Vec<String>>) {
+    const CRATE_NAME: &str = "Crate";
+    const HARNESS_HEADER: &str = "Harness";
+    const TOTALS_HEADER: &str = "Total";
 
+    let header = vec![String::new(), CRATE_NAME.to_string(), HARNESS_HEADER.to_string()];
+
+    let mut rows: Vec<Vec<String>> = vec![];
+
+    let mut total = 0;
+
+    for crate_md in list_metadata {
+        for harnesses in crate_md.standard_harnesses.values() {
+            for harness in harnesses {
+                rows.push(vec![
+                    String::new(),
+                    crate_md.crate_name.to_string(),
+                    harness.to_string(),
+                ]);
+            }
+            total += harnesses.len();
+        }
+    }
+
+    let totals_row = vec![TOTALS_HEADER.to_string(), String::new(), total.to_string()];
+    rows.push(totals_row);
+
+    (header, rows)
+}
+
+fn format_results<T: Display>(
+    table: Option<T>,
+    section_name: String,
+    absent_name: String,
+) -> String {
     let mut output: Vec<String> = vec![];
-    output.push(format!("\n{CONTRACTS_SECTION}"));
+    output.push(format!("\n{section_name}"));
 
     if let Some(table) = table {
         output.push(format!("{table}"));
     } else {
-        output.push(NO_CONTRACTS_MSG.to_string());
-    }
-
-    output.push(format!("\n{HARNESSES_SECTION}"));
-    if list_metadata.standard_harnesses.is_empty() {
-        output.push(NO_HARNESSES_MSG.to_string());
-    }
-
-    let mut std_harness_index = 0;
-
-    for harnesses in (&list_metadata.standard_harnesses).values() {
-        for harness in harnesses {
-            output.push(format!("{}. {harness}", std_harness_index + 1));
-            std_harness_index += 1;
-        }
+        output.push(absent_name);
     }
 
     output.join("\n")
