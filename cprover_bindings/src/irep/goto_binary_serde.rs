6ca757521a27a392c0e971ef1e6e4482a810a94d
AlexanderPortland <alexanderportland1@gmail.com>
2025-06-09 10:19:47 -0700

    switch IrepId strings to be copy-on-write

--- cprover_bindings/src/irep/goto_binary_serde.rs
+++ cprover_bindings/src/irep/goto_binary_serde.rs
@@ -283,7 +283,7 @@ impl IrepNumbering {
     /// Turns a [IrepId] to a [NumberedString]. The [IrepId] gets the number of its
     /// string representation.
     fn number_irep_id(&mut self, irep_id: &IrepId) -> NumberedString {
-        self.number_string(&irep_id.to_string().intern())
+        self.number_string(&irep_id.to_string_cow().intern())
     }
 
     /// Turns an [Irep] into a [NumberedIrep]. The [Irep] is recursively traversed
