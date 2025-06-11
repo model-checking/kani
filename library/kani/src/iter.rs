4bf94883d8f7881aeae16c0f3ece17067617c2c6
thanhnguyen-aws <ntson@amazon.com>
2025-06-11 11:36:05 -0700

    add copyright

--- library/kani/src/iter.rs
+++ library/kani/src/iter.rs
@@ -1,3 +1,10 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! This macro generates implementations of the `KaniIntoIter` trait for various common types that are used in for loop.
+//! We use this trait to overwrite the Rust IntoIter trait to reduce call stacks and avoid complicated loop invariant specifications,
+//! while maintaining the semantic of the loop.
+
 use crate::{KaniIntoIter, KaniSingleIter};
 
 impl<T: Copy> KaniIntoIter for Vec<T> {
