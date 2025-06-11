f625f85b3fa99c8a015667bb0aede64f21501195
thanhnguyen-aws <ntson@amazon.com>
2025-06-11 14:18:36 -0700

    fix format

--- tests/expected/loop-contract/for_loop_tuple.rs
+++ tests/expected/loop-contract/for_loop_tuple.rs
@@ -6,16 +6,15 @@
 //! Check for-loop invariant for when pat is a tuple.
 
 #![feature(proc_macro_hygiene)]
-#![feature(stmt_expr_attributes)] 
+#![feature(stmt_expr_attributes)]
 
 #[kani::proof]
-fn forloop(){
+fn forloop() {
     let mut sum: u32 = 0;
-    let a : [(u8, u8);10] = kani::any();
+    let a: [(u8, u8); 10] = kani::any();
 
     #[kani::loop_invariant( kaniindex <= 10 && sum <= (kaniindex as u32 * u8::MAX as u32 * 2) )]
-    for (i,j) in a
-    {   
+    for (i, j) in a {
         sum = sum + (i as u32) + (j as u32);
     }
     assert!(sum <= 20 * u8::MAX as u32);
--- tests/expected/loop-contract/for_loop_tuple_quantifier.rs
+++ tests/expected/loop-contract/for_loop_tuple_quantifier.rs
@@ -6,20 +6,19 @@
 //! Check for-loop invariant for when pat is a tuple.
 
 #![feature(proc_macro_hygiene)]
-#![feature(stmt_expr_attributes)] 
+#![feature(stmt_expr_attributes)]
 
-fn sum_pair ((x, y): (u8,u8) ) -> u32 {
+fn sum_pair((x, y): (u8, u8)) -> u32 {
     x as u32 + y as u32
 }
 
 #[kani::proof]
-fn forloop(){
+fn forloop() {
     let mut sum: u32 = 0;
-    let a : [(u8, u8);10] = kani::any();
+    let a: [(u8, u8); 10] = kani::any();
     kani::assume(kani::forall!(|i in (0,10)| sum_pair(a[i]) <= 10));
     #[kani::loop_invariant( kaniindex <= 10 && sum <= (kaniindex as u32 * 10 as u32) )]
-    for (i,j) in a.iter()
-    {   
+    for (i, j) in a.iter() {
         sum = sum + (i as u32) + (j as u32);
     }
     assert!(sum <= 100);
