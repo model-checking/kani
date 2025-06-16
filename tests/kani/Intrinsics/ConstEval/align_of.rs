a1cad6d6182a26804cee48e3ea1190b31ef90f35
Michael Tautschnig <tautschn@amazon.com>
2025-06-16 14:43:41 +0000

    Simplify test

--- tests/kani/Intrinsics/ConstEval/align_of.rs
+++ tests/kani/Intrinsics/ConstEval/align_of.rs
@@ -12,34 +12,9 @@ enum MyEnum {}
 
 #[kani::proof]
 fn main() {
-    #[cfg(target_arch = "x86_64")]
-    {
-        // Scalar types
-        assert!(align_of::<i8>() == 1);
-        assert!(align_of::<i16>() == 2);
-        assert!(align_of::<i32>() == 4);
-        assert!(align_of::<i64>() == 8);
-        assert!(align_of::<i128>() == 16);
-        assert!(align_of::<isize>() == 8);
-        assert!(align_of::<u8>() == 1);
-        assert!(align_of::<u16>() == 2);
-        assert!(align_of::<u32>() == 4);
-        assert!(align_of::<u64>() == 8);
-        assert!(align_of::<u128>() == 16);
-        assert!(align_of::<usize>() == 8);
-        assert!(align_of::<f32>() == 4);
-        assert!(align_of::<f64>() == 8);
-        assert!(align_of::<bool>() == 1);
-        assert!(align_of::<char>() == 4);
-        // Compound types (tuple and array)
-        assert!(align_of::<(i32, i32)>() == 4);
-        assert!(align_of::<[i32; 5]>() == 4);
-        // Custom data types (struct and enum)
-        assert!(align_of::<MyStruct>() == 1);
-        assert!(align_of::<MyEnum>() == 1);
-    }
-
-    #[cfg(target_arch = "aarch64")]
+    // for the following types x86_64 and aarch64 agree on the alignment; see
+    // AlignOfVal/align_of_fat_ptr.rs for some example of where they don't agree
+    #[cfg(any(target_arch = "x86_64" | target_arch = "aarch64"))]
     {
         // Scalar types
         assert!(align_of::<i8>() == 1);
