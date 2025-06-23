ec0e5bb29c3fc8f09ab243bfc085ac2613695249
thanhnguyen-aws <ntson@amazon.com>
2025-06-23 08:39:54 -0700

    add expected tests

---
+++ tests/expected/loop-contract/loop_assigns_for_fat_ptr_fail.rs
@@ -0,0 +1,23 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// kani-flags: -Z loop-contracts
+
+//! Check the use of loop_assigns for Rust's Fat ptr
+
+#![feature(proc_macro_hygiene)]
+#![feature(stmt_expr_attributes)]
+
+use std::ptr::slice_from_raw_parts;
+
+#[kani::proof]
+fn main() {
+    let mut i = 0;
+    let mut a: [u8; 100] = kani::any();
+    #[kani::loop_invariant(i <= 20)]
+    #[kani::loop_assigns(&i , slice_from_raw_parts(a.as_ptr(), 18))]
+    while i < 20 {
+        a[i] = 1;
+        i = i + 1;
+    }
+}
