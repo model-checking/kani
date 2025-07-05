8db052c530cd36d8fda04253215ee9a3f48e1514
Michael Tautschnig <tautschn@amazon.com>
2025-07-05 00:15:39 +0000

    Copyright and comment

--- tests/expected/debug-assert/main.rs
+++ tests/expected/debug-assert/main.rs
@@ -1,3 +1,9 @@
+// Copyright Kani Contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// This test checks that execution continues beyond a failing debug_assert (but does not in case of
+// a failing assert).
+
 #[kani::proof]
 fn foo() {
     std::debug_assert!(false, "will fail");
