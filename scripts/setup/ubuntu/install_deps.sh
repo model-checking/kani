586c5859fbda20468c4562aa3b2c64f81793cfe8
Michael Tautschnig <tautschn@amazon.com>
2025-07-15 18:42:09 +0000

    Revert "Build from source on Linux"
    This reverts commit 961084b4980f068bc1d01ecc1715a3bc60a0885f.

--- scripts/setup/ubuntu/install_deps.sh
+++ scripts/setup/ubuntu/install_deps.sh
@@ -38,20 +38,9 @@ sudo unzip -o -j -d /usr/local/bin cvc5-Linux-${ARCH}-static.zip cvc5-Linux-${AR
 rm cvc5-Linux-${ARCH}-static.zip
 cvc5 --version
 
-# It would be nice to just use their binaries as follows, but turns out their
-# "static" binaries aren't statically linked, and Ubuntu 22.04 doesn't have
-# sufficiently recent glibc.
-# curl -L --remote-name https://github.com/bitwuzla/bitwuzla/releases/download/0.8.1/Bitwuzla-Linux-${ARCH}-static.zip
-# sudo unzip -o -j -d /usr/local/bin Bitwuzla-Linux-${ARCH}-static.zip Bitwuzla-Linux-${ARCH}-static/bin/bitwuzla
-# rm Bitwuzla-Linux-${ARCH}-static.zip
-curl -L --remote-name https://github.com/bitwuzla/bitwuzla/archive/refs/tags/0.8.1.tar.gz
-tar xzf 0.8.1.tar.gz
-cd bitwuzla-0.8.1
-./configure.py
-ninja -C build
-sudo cp build/src/main/bitwuzla /usr/local/bin/
-cd ..
-rm -r 0.8.1.tar.gz bitwuzla-0.8.1
+curl -L --remote-name https://github.com/bitwuzla/bitwuzla/releases/download/0.8.1/Bitwuzla-Linux-${ARCH}-static.zip
+sudo unzip -o -j -d /usr/local/bin Bitwuzla-Linux-${ARCH}-static.zip Bitwuzla-Linux-${ARCH}-static/bin/bitwuzla
+rm Bitwuzla-Linux-${ARCH}-static.zip
 bitwuzla --version
 
 # Get the directory containing this script
