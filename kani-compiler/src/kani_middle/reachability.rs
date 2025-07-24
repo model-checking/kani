0a606ecd47a4b96538f38fd415665b58611db141
Carolyn Zech <cmzech@amazon.com>
2025-07-24 16:24:26 -0400

    remove ICE workaround

--- kani-compiler/src/kani_middle/reachability.rs
+++ kani-compiler/src/kani_middle/reachability.rs
@@ -84,14 +84,6 @@ where
     crate_items
         .iter()
         .filter_map(|item| {
-            // avoid stable MIR panic
-            // https://github.com/model-checking/kani/issues/3919
-            if let Ok(instance) = Instance::try_from(*item) {
-                let int_def_id = rustc_internal::internal(tcx, instance.def.def_id());
-                if matches!(tcx.def_kind(int_def_id), rustc_hir::def::DefKind::GlobalAsm) {
-                    return None;
-                }
-            };
             // Only collect monomorphic items.
             matches!(item.kind(), ItemKind::Fn)
                 .then(|| {
