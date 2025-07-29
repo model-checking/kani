f241bb9882e1e67b239674a5f59d2aa870bff8ac
Carolyn Zech <cmzech@amazon.com>
2025-07-29 19:11:09 -0400

    more tests

--- tests/ui/function-stubbing-error/resolution_errors.rs
+++ tests/ui/function-stubbing-error/resolution_errors.rs
@@ -21,11 +21,48 @@ pub trait Foo {
 
 impl Foo for Bar {}
 
+// types don't impl Foo
 #[kani::proof]
 #[kani::stub(u8::foo, stub_foo)]
 #[kani::stub(<(i32, i32)>::foo, stub_foo)]
 #[kani::stub(<[u32]>::foo, stub_foo)]
 #[kani::stub(str::foo, stub_foo)]
 #[kani::stub(<[char; 10]>::foo, stub_foo)]
+fn missing_impls() {}
+
+// type impls Foo, but fn bar doesn't exist
+#[kani::proof]
 #[kani::stub(<Bar as Foo>::bar, stub_foo)]
 fn invalid_methods() {}
+
+trait A {
+    fn bar(&self) -> u32;
+}
+
+trait B {
+    fn bar(&self) -> u32;
+}
+
+struct X {}
+
+impl A for X {
+    fn bar(&self) -> u32 {
+        200
+    }
+}
+
+impl B for X {
+    fn bar(&self) -> u32 {
+        300
+    }
+}
+
+// X::bar is ambiguous; two impls could match
+#[kani::proof]
+#[kani::stub(X::bar, A::bar)]
+fn ambiguous_stub() {}
+
+// B::bar doesn't have a body
+#[kani::proof]
+#[kani::stub(<X as A>::bar, B::bar)]
+fn missing_body() {}
