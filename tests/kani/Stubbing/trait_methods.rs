669c94c02210f59e142bd9442acf922a37dba9e0
Carolyn Zech <cmzech@amazon.com>
2025-07-29 21:28:07 -0400

    add test for issue 2524

--- tests/kani/Stubbing/trait_methods.rs
+++ tests/kani/Stubbing/trait_methods.rs
@@ -35,3 +35,29 @@ pub fn stub_foo() -> bool {
 #[kani::stub(<(i32, i32) as Foo>::foo, stub_foo)]
 #[kani::stub(<[u32] as Foo>::foo, stub_foo)]
 fn stub_trait_methods() {}
+
+// https://github.com/model-checking/kani/issues/2524
+mod issue_2524 {
+
+    trait Foo {
+        fn foo() -> usize;
+    }
+
+    struct Bar;
+
+    impl Foo for Bar {
+        fn foo() -> usize {
+            1
+        }
+    }
+
+    fn foo_stub() -> usize {
+        2
+    }
+
+    #[kani::proof]
+    #[kani::stub(Bar::foo, foo_stub)]
+    fn my_proof() {
+        assert_eq!(Bar::foo(), 2)
+    }
+}
