aee5da3278dbc43ab8a56aa6e0fbd12479898356
Michael Tautschnig <tautschn@amazon.com>
2025-07-30 08:14:54 +0000

    Additional test

--- tests/kani/Panic/prove_safety_only.rs
+++ tests/kani/Panic/prove_safety_only.rs
@@ -10,3 +10,13 @@ fn div0() -> i32 {
     let y: i32 = kani::any();
     x / y
 }
+
+#[kani::proof]
+fn assert_hides_ub() {
+    let arr: [u8; 5] = kani::any();
+    let mut bytes = kani::slice::any_slice_of_array(&arr);
+    let slice_offset = unsafe { bytes.as_ptr().offset_from(&arr as *const u8) };
+    let offset: usize = kani::any();
+    assert!(offset <= 4 && (slice_offset as usize) + offset <= 4);
+    let _ = unsafe { *bytes.as_ptr().add(offset) };
+}
