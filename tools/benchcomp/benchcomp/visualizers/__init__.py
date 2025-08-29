7b5915725e1fc289738f424a1bd8c29284c4b0ec
Zyad Hassan <88045115+zhassan-aws@users.noreply.github.com>
2025-08-29 11:39:10 -0700

    Add import select_autoescape (#4327)
    Minor refactor in benchcomp
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 and MIT licenses.

--- tools/benchcomp/benchcomp/visualizers/__init__.py
+++ tools/benchcomp/benchcomp/visualizers/__init__.py
@@ -11,6 +11,8 @@ import yaml
 import benchcomp
 import benchcomp.visualizers.utils as viz_utils
 
+from jinja2 import Environment, select_autoescape
+
 
 # TODO The doc comment should appear in the help output, which should list all
 # available checks.
@@ -229,8 +231,8 @@ class dump_markdown_results_table:
             "variants": self._get_variants(metrics),
         }
 
-        env = jinja2.Environment(
-            loader=jinja2.BaseLoader, autoescape=jinja2.select_autoescape(
+        env = Environment(
+            loader=jinja2.BaseLoader, autoescape=select_autoescape(
                 enabled_extensions=("html"),
                 default_for_string=True))
         template = env.from_string(self._get_template())
