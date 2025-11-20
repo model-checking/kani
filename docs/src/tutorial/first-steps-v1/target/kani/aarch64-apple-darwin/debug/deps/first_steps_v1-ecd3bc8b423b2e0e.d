fb10f355996d4044d5444394c8e484acf084c855
ConnorJKY <keyoujiang2023@gmail.com>
2025-11-19 19:06:07 -0800

    Comments fixed: delete irrelevant files

--- docs/src/tutorial/first-steps-v1/target/kani/aarch64-apple-darwin/debug/deps/first_steps_v1-ecd3bc8b423b2e0e.d
+++
@@ -1,7 +0,0 @@
-/Users/jingfeixu/kani-output/docs/src/tutorial/first-steps-v1/target/kani/aarch64-apple-darwin/debug/deps/first_steps_v1-ecd3bc8b423b2e0e.d: src/lib.rs
-
-/Users/jingfeixu/kani-output/docs/src/tutorial/first-steps-v1/target/kani/aarch64-apple-darwin/debug/deps/libfirst_steps_v1-ecd3bc8b423b2e0e.rlib: src/lib.rs
-
-/Users/jingfeixu/kani-output/docs/src/tutorial/first-steps-v1/target/kani/aarch64-apple-darwin/debug/deps/libfirst_steps_v1-ecd3bc8b423b2e0e.rmeta: src/lib.rs
-
-src/lib.rs:
