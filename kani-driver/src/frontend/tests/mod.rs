f8f1a461ae527a5db65ed7c401587f22f809ab26
edison <yimingy9@illinois.edu>
2025-11-24 11:39:56 -0800

    ci: fix copyright and clippy check

--- kani-driver/src/frontend/tests/mod.rs
+++ kani-driver/src/frontend/tests/mod.rs
@@ -1,9 +1,9 @@
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-/// Tests for the frontend module
-/// This module contains tests for the schema_utils module
-/// and the json_handler module
+//! Tests for the frontend module
+//! This module contains tests for the schema_utils module
+//! and the json_handler module
 
 #[cfg(test)]
 mod schema_utils_test;
